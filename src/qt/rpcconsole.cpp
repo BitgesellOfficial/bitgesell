24541950f23fbe202b0f8afe1098a8c9837af7fd
Russell Yanofsky <russ@yanofsky.org>
2017-12-05 15:57:12 -0500

    refactor: Rename Node::disconnect methods
    Avoid overloading method name to work more easily with IPC framework

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -1193,7 +1193,7 @@ void RPCConsole::disconnectSelectedNode()
         // Get currently selected peer address
         NodeId id = nodes.at(i).data().toLongLong();
         // Find the node, disconnect it and clear the selected node
-        if(m_node.disconnect(id))
+        if(m_node.disconnectById(id))
             clearSelectedNode();
     }
 }
@@ -1218,7 +1218,7 @@ void RPCConsole::banSelectedNode(int bantime)
         const CNodeCombinedStats *stats = clientModel->getPeerTableModel()->getNodeStats(detailNodeRow);
         if (stats) {
             m_node.ban(stats->nodeStats.addr, BanReasonManuallyAdded, bantime);
-            m_node.disconnect(stats->nodeStats.addr);
+            m_node.disconnectByAddress(stats->nodeStats.addr);
         }
     }
     clearSelectedNode();
