d8689a3ed0d2a6e31298e7b02c8f31dd5074023f
Andrew Chow <achow101-github@achow101.com>
2019-07-08 17:05:05 -0400

    Implement IsHDEnabled in DescriptorScriptPubKeyMan

--- src/wallet/scriptpubkeyman.cpp
+++ src/wallet/scriptpubkeyman.cpp
@@ -1823,7 +1823,8 @@ bool DescriptorScriptPubKeyMan::SetupDescriptorGeneration(const CExtKey& master_
 
 bool DescriptorScriptPubKeyMan::IsHDEnabled() const
 {
-    return false;
+    LOCK(cs_desc_man);
+    return m_wallet_descriptor.descriptor->IsRange();
 }
 
 bool DescriptorScriptPubKeyMan::CanGetAddresses(bool internal) const
