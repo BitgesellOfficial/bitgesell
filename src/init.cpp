42508dca326f36241ae4df80bc72f6fcae213af3
James O'Beirne <james.obeirne@gmail.com>
2019-09-16 15:01:12 -0400

    Add CChainState::ResizeCoinsCaches
    Also adds CCoinsViewCache::ReallocateCache() to attempt to free
    memory that the cacheCoins's allocator may be hanging onto when
    downsizing the cache.
    
    Adds `CChainState::m_coins{tip,db}_cache_size_bytes` data members
    so that we can reference cache size on a per-chainstate basis for
    flushing.

--- src/init.cpp
+++ src/init.cpp
@@ -1523,7 +1523,7 @@ bool AppInitMain(const util::Ref& context, NodeContext& node)
     int64_t nCoinDBCache = std::min(nTotalCache / 2, (nTotalCache / 4) + (1 << 23)); // use 25%-50% of the remainder for disk cache
     nCoinDBCache = std::min(nCoinDBCache, nMaxCoinsDBCache << 20); // cap total coins db cache
     nTotalCache -= nCoinDBCache;
-    nCoinCacheUsage = nTotalCache; // the rest goes to in-memory cache
+    int64_t nCoinCacheUsage = nTotalCache; // the rest goes to in-memory cache
     int64_t nMempoolSizeMax = gArgs.GetArg("-maxmempool", DEFAULT_MAX_MEMPOOL_SIZE) * 1000000;
     LogPrintf("Cache configuration:\n");
     LogPrintf("* Using %.1f MiB for block index database\n", nBlockTreeDBCache * (1.0 / 1024 / 1024));
@@ -1636,9 +1636,9 @@ bool AppInitMain(const util::Ref& context, NodeContext& node)
                     break;
                 }
 
-                // The on-disk coinsdb is now in a good state, create the cache
-                ::ChainstateActive().InitCoinsCache();
-                assert(::ChainstateActive().CanFlushToDisk());
+                    // The on-disk coinsdb is now in a good state, create the cache
+                    chainstate->InitCoinsCache(nCoinCacheUsage);
+                    assert(chainstate->CanFlushToDisk());
 
                 is_coinsview_empty = fReset || fReindexChainState ||
                     ::ChainstateActive().CoinsTip().GetBestBlock().IsNull();
