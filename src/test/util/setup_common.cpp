bee8b33c33d72ad4bd208d396973ea9ecc6e9f0a
James O'Beirne <james.obeirne@gmail.com>
2019-12-12 14:11:03 -0500

    test: add basic tests for ChainstateManager
    Feedback incorporated from Russell Yanofsky.
    
    Co-authored-by: MarcoFalke <falke.marco@gmail.com>

--- src/test/util/setup_common.cpp
+++ src/test/util/setup_common.cpp
@@ -113,7 +113,8 @@ TestingSetup::TestingSetup(const std::string& chainName) : BasicTestingSetup(cha
     GetMainSignals().RegisterBackgroundSignalScheduler(*g_rpc_node->scheduler);
 
     pblocktree.reset(new CBlockTreeDB(1 << 20, true));
-    g_chainstate = MakeUnique<CChainState>();
+
+    g_chainman.InitializeChainstate();
     ::ChainstateActive().InitCoinsDB(
         /* cache_size_bytes */ 1 << 23, /* in_memory */ true, /* should_wipe */ false);
     assert(!::ChainstateActive().CanFlushToDisk());
