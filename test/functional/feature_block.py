6d7cf97951acb946dc4d302e22802fa04ce14d3b
Samer Afach <info@afach.de>
2020-03-14 14:44:56 +0100

    Fix mining to an invalid target + ensure that a new block has the correct hash internally in Python tests

--- test/functional/feature_block.py
+++ test/functional/feature_block.py
@@ -632,7 +632,7 @@ class FullBlockTest(BGLTestFramework):
         self.move_tip(44)
         b47 = self.next_block(47, solve=False)
         target = uint256_from_compact(b47.nBits)
-        while b47.sha256 < target:
+        while b47.sha256 <= target:
             b47.nNonce += 1
             b47.rehash()
         self.send_blocks([b47], False, force_send=True, reject_reason='high-hash', reconnect=True)
@@ -1345,6 +1345,8 @@ class FullBlockTest(BGLTestFramework):
             block.hashMerkleRoot = block.calc_merkle_root()
         if solve:
             block.solve()
+        else:
+            block.rehash()
         self.tip = block
         self.block_heights[block.sha256] = height
         assert number not in self.blocks
