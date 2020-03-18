4a778bfc8e2b2e8f9bd2eabc4c63729ad9c380b5
Pieter Wuille <pieter.wuille@gmail.com>
2020-03-18 00:33:43 -0700

    Simplify bn2vch using int.to_bytes

--- test/functional/test_framework/script.py
+++ test/functional/test_framework/script.py
@@ -21,6 +21,16 @@ OPCODE_NAMES = {}
 def hash160(s):
     return hashlib.new('ripemd160', sha256(s)).digest()
 
+def bn2vch(v):
+    """Convert number to bitcoin-specific little endian format."""
+    # We need v.bit_length() bits, plus a sign bit for every nonzero number.
+    n_bits = v.bit_length() + (v != 0)
+    # The number of bytes for that is:
+    n_bytes = (n_bits + 7) // 8
+    # Convert number to absolute value + sign in top bit.
+    encoded_v = 0 if v == 0 else abs(v) | ((v < 0) << (n_bytes * 8 - 1))
+    # Serialize to bytes
+    return encoded_v.to_bytes(n_bytes, 'little')
 
 _opcode_instances = []
 class CScriptOp(int):
