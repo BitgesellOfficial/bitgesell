5c09d163a73a5e6c5e36bbdf99c97ccc4694f026
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2020-04-25 13:44:44 +0200

    test: replace inv type magic numbers by constants

--- test/functional/p2p_invalid_messages.py
+++ test/functional/p2p_invalid_messages.py
@@ -215,10 +215,10 @@ class InvalidMessagesTest(BGLTestFramework):
     def test_large_inv(self):
         conn = self.nodes[0].add_p2p_connection(P2PInterface())
         with self.nodes[0].assert_debug_log(['Misbehaving', 'peer=4 (0 -> 20): message inv size() = 50001']):
-            msg = msg_inv([CInv(MSG_TX, 1)] * 50001)
+            msg = messages.msg_inv([messages.CInv(messages.MSG_TX, 1)] * 50001)
             conn.send_and_ping(msg)
         with self.nodes[0].assert_debug_log(['Misbehaving', 'peer=4 (20 -> 40): message getdata size() = 50001']):
-            msg = msg_getdata([CInv(MSG_TX, 1)] * 50001)
+            msg = messages.msg_getdata([messages.CInv(messages.MSG_TX, 1)] * 50001)
             conn.send_and_ping(msg)
         with self.nodes[0].assert_debug_log(['Misbehaving', 'peer=4 (40 -> 60): headers message size = 2001']):
             msg = msg_headers([CBlockHeader()] * 2001)
