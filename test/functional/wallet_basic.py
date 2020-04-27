2cd390f6b3379002fbc12622331a92878de06fe1
Harris <brakmic@gmail.com>
2020-04-27 10:45:03 +0200

    rpc: return block hash & height in getbalances, gettransaction & getwalletinfo JSONs
    Co-authored-by: Aurèle Oulès <aurele@oules.com>

--- test/functional/wallet_basic.py
+++ test/functional/wallet_basic.py
@@ -684,7 +684,7 @@ class WalletTest(BGLTestFramework):
                                  "category": baz["category"],
                                  "vout":     baz["vout"]}
         expected_fields = frozenset({'amount', 'bip125-replaceable', 'confirmations', 'details', 'fee',
-                                     'hex', 'time', 'timereceived', 'trusted', 'txid', 'wtxid', 'walletconflicts'})
+                                     'hex', 'lastprocessedblock', 'time', 'timereceived', 'trusted', 'txid', 'wtxid', 'walletconflicts'})
         verbose_field = "decoded"
         expected_verbose_fields = expected_fields | {verbose_field}
 
