2954d195258aa6b4004cfd261880f70ac41addd7
Daniel Kraft <d@domob.eu>
2020-04-28 09:26:09 +0200

    Testcase for wallet issue with orphaned rewards.
    This adds a new test case demonstrating the wallet issue when block
    rewards are orphaned (https://github.com/bitcoin/bitcoin/issues/14148).

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -283,6 +283,8 @@ BASE_SCRIPTS = [
     'rpc_scantxoutset.py',
     'feature_logging.py',
     'feature_anchors.py',
+    'feature_coinstatsindex.py',
+    'wallet_orphanedreward.py',
     'p2p_node_network_limited.py',
     'p2p_permissions.py',
     'feature_blocksdir.py',
