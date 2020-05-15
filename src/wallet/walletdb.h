252bf73b4ebcd25e1fde09b5c4f31471cced1b57
Andrew Chow <achow101-github@achow101.com>
2020-05-15 19:15:50 -0400

    Expose a version of ReadKeyValue and use it in RecoverKeysOnlyFilter
    We need this exposed for BerkeleyBatch::Recover to be moved out.

--- src/wallet/walletdb.h
+++ src/wallet/walletdb.h
@@ -1,5 +1,5 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2020 The Bitcoin Core developers
+// Copyright (c) 2009-2020 The BGL Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -291,5 +291,8 @@ private:
 
 //! Compacts BDB state so that wallet.dat is self-contained (if there are changes)
 void MaybeCompactWalletDB();
+
+//! Unserialize a given Key-Value pair and load it into the wallet
+bool ReadKeyValue(CWallet* pwallet, CDataStream& ssKey, CDataStream& ssValue, std::string& strType, std::string& strErr);
 
 #endif // BGL_WALLET_WALLETDB_H
