38f4f00a2812329f6d69f42c56eb3df43ca1a757
Andrew Chow <achow101-github@achow101.com>
2020-06-15 15:31:02 -0400

    walletdb: Move Rewrite into BerkeleyDatabase
    Make Rewrite actually a member of BerkeleyDatabase instead of a static
    function in BerkeleyBatch

--- src/wallet/bdb.h
+++ src/wallet/bdb.h
@@ -276,8 +276,6 @@ public:
     bool TxnBegin();
     bool TxnCommit();
     bool TxnAbort();
-
-    bool static Rewrite(BerkeleyDatabase& database, const char* pszSkip = nullptr);
 };
 
 std::string BerkeleyDatabaseVersion();
