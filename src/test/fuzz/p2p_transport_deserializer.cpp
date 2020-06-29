945fa46e9ff08abf0d930284e70f31e43b475ad3
Troy Giorshev <troygiorshev@gmail.com>
2020-06-29 14:15:06 -0400

    Move checksum check from net_processing to net
    This removes the m_valid_checksum member from CNetMessage.  Instead,
    GetMessage() returns an Optional.
    
    Additionally, GetMessage() has been given an out parameter to be used to
    hold error information.  For now it is specifically a uint32_t used to
    hold the raw size of the corrupt message.
    
    The checksum check is now done in GetMessage.

--- src/test/fuzz/p2p_transport_deserializer.cpp
+++ src/test/fuzz/p2p_transport_deserializer.cpp
@@ -33,7 +33,7 @@ void test_one_input(const std::vector<uint8_t>& buffer)
         if (deserializer.Complete()) {
             const std::chrono::microseconds m_time{std::numeric_limits<int64_t>::max()};
             uint32_t out_err_raw_size{0};
-            Optional<CNetMessage> result{deserializer.GetMessage(m_time, out_err_raw_size)};
+            Optional<CNetMessage> result{deserializer.GetMessage(Params().MessageStart(), m_time, out_err_raw_size)};
             if (result) {
                 assert(result->m_command.size() <= CMessageHeader::COMMAND_SIZE);
                 assert(result->m_raw_message_size <= buffer.size());
