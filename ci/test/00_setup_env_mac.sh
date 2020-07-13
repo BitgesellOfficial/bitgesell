3f99aea85987edc094806198c08d03f0f7010ff5
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-07-14 01:13:25 +0300

    ci: Fix configure options for macOS builds

--- ci/test/00_setup_env_mac.sh
+++ ci/test/00_setup_env_mac.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright (c) 2019-2020 The Bitcoin Core developers
+# Copyright (c) 2019-2020 The BGL Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -14,4 +14,4 @@ export XCODE_BUILD_ID=11C505
 export RUN_UNIT_TESTS=false
 export RUN_FUNCTIONAL_TESTS=false
 export GOAL="deploy"
-export BGL_CONFIG="--enable-gui --enable-reduce-exports --enable-werror"
+export BGL_CONFIG="--with-gui --enable-reduce-exports --enable-werror"
