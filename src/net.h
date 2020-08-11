74302b1c412d2baea0662d204cd95430f94038e9
Amiti Uttarwar <amiti@uttarwar.org>
2020-08-11 10:36:42 -0700

    [trivial] Small style updates

--- src/net.h
+++ src/net.h
@@ -830,7 +830,7 @@ public:
     std::atomic_bool fPauseSend{false};
 
     bool IsOutboundOrBlockRelayConn() const {
-        switch(m_conn_type) {
+        switch (m_conn_type) {
             case ConnectionType::OUTBOUND_FULL_RELAY:
             case ConnectionType::BLOCK_RELAY:
                 return true;
@@ -869,7 +869,7 @@ public:
     }
 
     bool ExpectServicesFromConn() const {
-        switch(m_conn_type) {
+        switch (m_conn_type) {
             case ConnectionType::INBOUND:
             case ConnectionType::MANUAL:
             case ConnectionType::FEELER:
@@ -893,7 +893,7 @@ public:
 
     // flood relay
     std::vector<CAddress> vAddrToSend;
-    std::unique_ptr<CRollingBloomFilter> m_addr_known = nullptr;
+    std::unique_ptr<CRollingBloomFilter> m_addr_known{nullptr};
     bool fGetAddr{false};
     int64_t nNextAddrSend GUARDED_BY(cs_sendProcessing){0};
     int64_t nNextLocalAddrSend GUARDED_BY(cs_sendProcessing){0};
