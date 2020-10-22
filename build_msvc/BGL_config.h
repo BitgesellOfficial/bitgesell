205f4b4fd4164696d2aaba4b4da9b049a596c513
Andrew Chow <achow101-github@achow101.com>
2020-10-22 13:59:18 -0400

    Drop the leading 0 from the version number
    Removes the leading 0 from the version number. The minor version, which
    we had been using as the major version, is now the major version. The
    revision, which we had been using as the minor version, is now the minor
    version. The revision number is dropped. The build number is promoted to
    being part of the version number. This also avoids issues where it was
    accidentally not included in the version number.
    
    The CLIENT_VERSION remains the same format as previous as previously,
    the Major version was 0 so that was never a factor in CLIENT_VERSION.

--- build_msvc/BGL_config.h
+++ build_msvc/BGL_config.h
@@ -15,13 +15,10 @@
 #define CLIENT_VERSION_IS_RELEASE true
 
 /* Major version */
-#define CLIENT_VERSION_MAJOR 0
+#define CLIENT_VERSION_MAJOR 21
 
 /* Minor version */
-#define CLIENT_VERSION_MINOR 21
-
-/* Build revision */
-#define CLIENT_VERSION_REVISION 1
+#define CLIENT_VERSION_MINOR 99
 
 /* Copyright holder(s) before %s replacement */
 #define COPYRIGHT_HOLDERS "The %s developers"
@@ -259,7 +256,7 @@
 #define PACKAGE_NAME "BGL Core"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "BGL Core 0.29.99"
+#define PACKAGE_STRING "BGL Core 21.99.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "BGL"
@@ -268,7 +265,7 @@
 #define PACKAGE_URL "https://BGLcore.org/"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.21.99"
+#define PACKAGE_VERSION "21.99.0"
 
 /* Define to necessary symbol if this constant uses a non-standard name on
    your system. */
