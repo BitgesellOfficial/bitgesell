6c909dc0ebc5a227bf1ff810d7e46f8e8a4d344e
John Newbery <john@johnnewbery.com>
2020-10-23 10:20:48 +0100

    [net] remove CConnman::MarkAddressGood
    It just forwards calls to CAddrMan::Good.

--- src/net.cpp
+++ src/net.cpp
@@ -2667,11 +2667,6 @@ CConnman::~CConnman()
     Stop();
 }
 
-void CConnman::MarkAddressGood(const CAddress& addr)
-{
-    addrman.Good(addr);
-}
-
 bool CConnman::AddNewAddresses(const std::vector<CAddress>& vAddr, const CAddress& addrFrom, int64_t nTimePenalty)
 {
     return addrman.Add(vAddr, addrFrom, nTimePenalty);
