1a3f0cf2f7f490b816cb66060f61826d64bf8786
fanquake <fanquake@gmail.com>
2020-10-24 16:24:07 +0800

    net: remove SetMaxOutboundTarget
    This has been unused since f3552da81393a8e78ce3e2afed0b9c9d1ff5cee0.

--- src/net.cpp
+++ src/net.cpp
@@ -2857,12 +2857,6 @@ void CConnman::RecordBytesSent(uint64_t bytes)
     nMaxOutboundTotalBytesSentInCycle += bytes;
 }
 
-void CConnman::SetMaxOutboundTarget(uint64_t limit)
-{
-    LOCK(cs_totalBytesSent);
-    nMaxOutboundLimit = limit;
-}
-
 uint64_t CConnman::GetMaxOutboundTarget()
 {
     LOCK(cs_totalBytesSent);
