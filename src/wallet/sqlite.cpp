a0794dcc6309f8b884f8c49d22884052f6140817
Russell Yanofsky <russ@yanofsky.org>
2020-10-30 16:11:28 -0400

    MOVEONLY: Move IsBDBFile, IsSQLiteFile, and ListWalletDir
    This commit does not change to any code and behavior. It it is easily reviewed
    with the --color-moved=dimmed_zebra git diff option.
    
    Motivation for this change is to:
    
    - Consolidate redundant functions
      IsBDBFile /ExistsBerkeleyDatabase / SplitWalletPath, and
      IsSQLiteFile / ExistsSQLiteDatabase in the next commits
    
    - Detect SQLite wallets consistently regardless whether bitcoin is built with
      SQLite support in the next commits
    
    - Avoid attempting to open SQLite databases with the BDB library when bitcoin
      is built without SQLite support in the next commits

--- src/wallet/sqlite.cpp
+++ src/wallet/sqlite.cpp
@@ -558,30 +558,3 @@ std::string SQLiteDatabaseVersion()
 {
     return std::string(sqlite3_libversion());
 }
-
-bool IsSQLiteFile(const fs::path& path)
-{
-    if (!fs::exists(path)) return false;
-
-    // A SQLite Database file is at least 512 bytes.
-    boost::system::error_code ec;
-    auto size = fs::file_size(path, ec);
-    if (ec) LogPrintf("%s: %s %s\n", __func__, ec.message(), path.string());
-    if (size < 512) return false;
-
-    fsbridge::ifstream file(path, std::ios::binary);
-    if (!file.is_open()) return false;
-
-    // Magic is at beginning and is 16 bytes long
-    char magic[16];
-    file.read(magic, 16);
-    file.close();
-
-    // Check the magic, see https://sqlite.org/fileformat2.html
-    std::string magic_str(magic, 16);
-    if (magic_str != std::string("SQLite format 3", 16)) {
-        return false;
-    }
-
-    return magic_str == std::string("SQLite format 3");
-}
