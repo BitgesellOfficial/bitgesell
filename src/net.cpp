4349a7ba489172053364f2f4b04a7ab0c70bc02b
practicalswift <practicalswift@users.noreply.github.com>
2020-11-04 12:22:06 +0000

    Make it easier to reason about node eviction by removing unused NodeEvictionCandidate::addr (CAddress)

--- src/net.cpp
+++ src/net.cpp
@@ -858,7 +858,6 @@ struct NodeEvictionCandidate
     bool fRelevantServices;
     bool fRelayTxes;
     bool fBloomFilter;
-    CAddress addr;
     uint64_t nKeyedNetGroup;
     bool prefer_evict;
     bool m_is_local;
@@ -950,7 +949,7 @@ bool CConnman::AttemptToEvictConnection()
             NodeEvictionCandidate candidate = {node->GetId(), node->nTimeConnected, node->nMinPingUsecTime,
                                                node->nLastBlockTime, node->nLastTXTime,
                                                HasAllDesirableServiceFlags(node->nServices),
-                                               peer_relay_txes, peer_filter_not_null, node->addr, node->nKeyedNetGroup,
+                                               peer_relay_txes, peer_filter_not_null, node->nKeyedNetGroup,
                                                node->m_prefer_evict, node->addr.IsLocal()};
             vEvictionCandidates.push_back(candidate);
         }
