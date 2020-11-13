hebasto/bitcoin-qtw

avoided os-dependant path
The current code fails on windows because of the forward slashes; using os.path.join solves the problem and it is in general more robust

test/functional/test_framework/test_framework.py
@@ -185,8 +185,18 @@ class BitcoinTestFramework(metaclass=BitcoinTestMetaClass):
         config = configparser.ConfigParser()
         config.read_file(open(self.options.configfile))
         self.config = config
-        self.options.bitcoind = os.getenv("BITCOIND", default=config["environment"]["BUILDDIR"] + '/src/bitcoind' + config["environment"]["EXEEXT"])
-        self.options.bitcoincli = os.getenv("BITCOINCLI", default=config["environment"]["BUILDDIR"] + '/src/bitcoin-cli' + config["environment"]["EXEEXT"])
+        fname_bitcoind = os.path.join(
+            config["environment"]["BUILDDIR"],
+            "src",
+            "bitcoind" + config["environment"]["EXEEXT"]
+        )
+        fname_bitcoincli = os.path.join(
+            config["environment"]["BUILDDIR"],
+            "src",
+            "bitcoin-cli" + config["environment"]["EXEEXT"]
+        )
+        self.options.bitcoind = os.getenv("BITCOIND", default=fname_bitcoind)
+        self.options.bitcoincli = os.getenv("BITCOINCLI", default=fname_bitcoincli)
 
         self.options.previous_releases_path = os.getenv("PREVIOUS_RELEASES_DIR") or os.getcwd() + "/releases"
 

BitgesellOfficial/bitgesell

test/functional/test_framework/test_framework.py
@@ -185,8 +185,18 @@ class BGLTestFramework(metaclass=BGLTestMetaClass):
         config = configparser.ConfigParser()
         config.read_file(open(self.options.configfile))
         self.config = config
-        self.options.BGLd = os.getenv("BGLD", default=config["environment"]["BUILDDIR"] + '/src/BGLd' + config["environment"]["EXEEXT"])
-        self.options.BGLcli = os.getenv("BGLCLI", default=config["environment"]["BUILDDIR"] + '/src/BGL-cli' + config["environment"]["EXEEXT"])
+        fname_BGLd = os.path.join(
+            config["environment"]["BUILDDIR"],
+            "src",
+            "BGLd" + config["environment"]["EXEEXT"]
+        )
+        fname_BGLcli = os.path.join(
+            config["environment"]["BUILDDIR"],
+            "src",
+            "BGL-cli" + config["environment"]["EXEEXT"]
+        )
+        self.options.BGLd = os.getenv("BGLD", default=fname_BGLd)
+        self.options.BGLcli = os.getenv("BGLCLI", default=fname_BGLcli)
 
         self.options.previous_releases_path = os.getenv("PREVIOUS_RELEASES_DIR") or os.getcwd() + "/releases"
 