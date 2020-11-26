953fb9dd2eb7ec052fe8a2878792fc69ea46f90e
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-11-26 19:01:19 +0200

    ci: Build with --enable-werror by default, and document exceptions

--- ci/test/06_script_a.sh
+++ ci/test/06_script_a.sh
@@ -4,7 +4,6 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-echo "START SCRIPT A"
 export LC_ALL=C.UTF-8
 
 BGL_CONFIG_ALL="--enable-suppress-external-warnings --disable-dependency-tracking --prefix=$DEPENDS_DIR/$HOST --bindir=$BASE_OUTDIR/bin --libdir=$BASE_OUTDIR/lib"
@@ -13,38 +12,49 @@ if [ -z "$NO_WERROR" ]; then
 fi
 DOCKER_EXEC "ccache --zero-stats --max-size=$CCACHE_SIZE"
 
-
+BEGIN_FOLD autogen
 if [ -n "$CONFIG_SHELL" ]; then
   DOCKER_EXEC "$CONFIG_SHELL" -c "./autogen.sh"
 else
   DOCKER_EXEC ./autogen.sh
 fi
+END_FOLD
 
 DOCKER_EXEC mkdir -p "${BASE_BUILD_DIR}"
+export P_CI_DIR="${BASE_BUILD_DIR}"
 
-CONFIG_EVENT_LIBS="EVENT_LIBS='${EVENT_LIBS}'"
+BEGIN_FOLD configure
+DOCKER_EXEC "${BASE_ROOT_DIR}/configure" --cache-file=config.cache $BGL_CONFIG_ALL $BGL_CONFIG || ( (DOCKER_EXEC cat config.log) && false)
+END_FOLD
 
-DOCKER_EXEC "${CONFIG_EVENT_LIBS} ${BASE_ROOT_DIR}/configure" --cache-file=config.cache $BGL_CONFIG_ALL $BGL_CONFIG || ( (DOCKER_EXEC cat config.log) && false)
-
+BEGIN_FOLD distdir
 DOCKER_EXEC make distdir VERSION=$HOST
+END_FOLD
 
+export P_CI_DIR="${BASE_BUILD_DIR}/BGL-$HOST"
 
-DOCKER_EXEC "${CONFIG_EVENT_LIBS} ./configure --cache-file=../config.cache $BGL_CONFIG_ALL $BGL_CONFIG || ( (DOCKER_EXEC cat config.log) && false)"
+BEGIN_FOLD configure
+DOCKER_EXEC ./configure --cache-file=../config.cache $BGL_CONFIG_ALL $BGL_CONFIG || ( (DOCKER_EXEC cat config.log) && false)
+END_FOLD
 
 set -o errtrace
 trap 'DOCKER_EXEC "cat ${BASE_SCRATCH_DIR}/sanitizer-output/* 2> /dev/null"' ERR
 
-#if [[ ${USE_MEMORY_SANITIZER} == "true" ]]; then
-#  # MemorySanitizer (MSAN) does not support tracking memory initialization done by
-#  # using the Linux getrandom syscall. Avoid using getrandom by undefining
-#  # HAVE_SYS_GETRANDOM. See https://github.com/google/sanitizers/issues/852 for
-#  # details.
-#  DOCKER_EXEC 'grep -v HAVE_SYS_GETRANDOM src/config/bgl-config.h > src/config/bgl-config.h.tmp && mv src/config/bgl-config.h.tmp src/config/bgl-config.h'
-#fi
+if [[ ${USE_MEMORY_SANITIZER} == "true" ]]; then
+  # MemorySanitizer (MSAN) does not support tracking memory initialization done by
+  # using the Linux getrandom syscall. Avoid using getrandom by undefining
+  # HAVE_SYS_GETRANDOM. See https://github.com/google/sanitizers/issues/852 for
+  # details.
+  DOCKER_EXEC 'grep -v HAVE_SYS_GETRANDOM src/config/BGL-config.h > src/config/BGL-config.h.tmp && mv src/config/BGL-config.h.tmp src/config/BGL-config.h'
+fi
 
-
+BEGIN_FOLD build
 DOCKER_EXEC make $MAKEJOBS $GOAL || ( echo "Build failure. Verbose build follows." && DOCKER_EXEC make $GOAL V=1 ; false )
+END_FOLD
 
+BEGIN_FOLD cache_stats
 DOCKER_EXEC "ccache --version | head -n 1 && ccache --show-stats"
 DOCKER_EXEC du -sh "${DEPENDS_DIR}"/*/
 DOCKER_EXEC du -sh "${PREVIOUS_RELEASES_DIR}"
+END_FOLD
+
