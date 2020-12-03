hebasto/bitcoin-qtw

test: resort imports

test/functional/mempool_persist.py
@@ -39,8 +39,8 @@ from decimal import Decimal
 import os
 import time
 
-from test_framework.test_framework import BitcoinTestFramework
 from test_framework.p2p import P2PTxInvStore
+from test_framework.test_framework import BitcoinTestFramework
 from test_framework.util import (
     assert_equal,
     assert_greater_than_or_equal,

test/functional/rpc_blockchain.py
@@ -22,16 +22,6 @@ from decimal import Decimal
 import http.client
 import subprocess
 
-from test_framework.test_framework import BitcoinTestFramework
-from test_framework.util import (
-    assert_equal,
-    assert_greater_than,
-    assert_greater_than_or_equal,
-    assert_raises,
-    assert_raises_rpc_error,
-    assert_is_hex_string,
-    assert_is_hash_string,
-)
 from test_framework.blocktools import (
     create_block,
     create_coinbase,
@@ -42,8 +32,16 @@ from test_framework.messages import (
     FromHex,
     msg_block,
 )
-from test_framework.p2p import (
-    P2PInterface,
+from test_framework.p2p import P2PInterface
+from test_framework.test_framework import BitcoinTestFramework
+from test_framework.util import (
+    assert_equal,
+    assert_greater_than,
+    assert_greater_than_or_equal,
+    assert_raises,
+    assert_raises_rpc_error,
+    assert_is_hex_string,
+    assert_is_hash_string,
 )
 
 

BitgesellOfficial/bitgesell

test/functional/mempool_persist.py
@@ -39,9 +39,8 @@ from decimal import Decimal
 import os
 import time
 
-
-from test_framework.test_framework import BGLTestFramework
 from test_framework.p2p import P2PTxInvStore
+from test_framework.test_framework import BGLTestFramework
 from test_framework.util import (
     assert_equal,
     assert_greater_than_or_equal,

test/functional/rpc_blockchain.py
@@ -22,16 +22,6 @@ from decimal import Decimal
 import http.client
 import subprocess
 
-from test_framework.test_framework import BGLTestFramework
-from test_framework.util import (
-    assert_equal,
-    assert_greater_than,
-    assert_greater_than_or_equal,
-    assert_raises,
-    assert_raises_rpc_error,
-    assert_is_hex_string,
-    assert_is_hash_string,
-)
 from test_framework.blocktools import (
     create_block,
     create_coinbase,
@@ -42,8 +32,16 @@ from test_framework.messages import (
     FromHex,
     msg_block,
 )
-from test_framework.p2p import (
-    P2PInterface,
+from test_framework.p2p import P2PInterface
+from test_framework.test_framework import BGLTestFramework
+from test_framework.util import (
+    assert_equal,
+    assert_greater_than,
+    assert_greater_than_or_equal,
+    assert_raises,
+    assert_raises_rpc_error,
+    assert_is_hex_string,
+    assert_is_hash_string,
 )
 
 