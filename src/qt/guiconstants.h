kallewoof/bitcoin

qt: update QT to support signet network

src/qt/guiconstants.h
@@ -51,6 +51,7 @@ static const int MAX_URI_LENGTH = 255;
 #define QAPP_APP_NAME_DEFAULT "Bitcoin-Qt"
 #define QAPP_APP_NAME_TESTNET "Bitcoin-Qt-testnet"
 #define QAPP_APP_NAME_REGTEST "Bitcoin-Qt-regtest"
+#define QAPP_APP_NAME_SIGNET "Bitcoin-Qt-signet"
 
 /* One gigabyte (GB) in bytes */
 static constexpr uint64_t GB_BYTES{1000000000};

BitgesellOfficial/bitgesell

src/qt/guiconstants.h
@@ -46,6 +46,7 @@ static const int TOOLTIP_WRAP_THRESHOLD = 80;
 #define QAPP_ORG_DOMAIN "BGL.org"
 #define QAPP_APP_NAME_DEFAULT "BGL-Qt"
 #define QAPP_APP_NAME_TESTNET "BGL-Qt-testnet"
+#define QAPP_APP_NAME_SIGNET "BGL-Qt-signet"
 #define QAPP_APP_NAME_REGTEST "BGL-Qt-regtest"
 
 /* One gigabyte (GB) in bytes */