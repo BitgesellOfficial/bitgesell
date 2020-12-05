bitcoin-core-review-club/bitcoin

signet: hard-coded parameters for Signet Global Network VI (2020-09-07)
Co-authored-by: Anthony Towns <aj@erisian.com.au>

src/chainparams.cpp
@@ -265,18 +265,24 @@ public:
         vSeeds.clear();
 
         if (!args.IsArgSet("-signet_challenge")) {
-            throw std::runtime_error(strprintf("%s: -signet_challenge is mandatory for signet networks", __func__));
+            LogPrintf("Using default signet network\n");
+            bin = ParseHex("512103ad5e0edad18cb1f0fc0d28a3d4f1f3e445640337489abb10404f2d1e086be430210359ef5021964fe22d6f8e05b2463c9540ce96883fe3b278760f048f5189f2e6c452ae");
+            vSeeds.emplace_back("178.128.221.177");
+            vSeeds.emplace_back("2a01:7c8:d005:390::5");
+            vSeeds.emplace_back("ntv3mtqw5wt63red.onion:38333");
+        } else {
+            const auto signet_challenge = args.GetArgs("-signet_challenge");
+            if (signet_challenge.size() != 1) {
+                throw std::runtime_error(strprintf("%s: -signet_challenge cannot be multiple values.", __func__));
+            }
+            bin = ParseHex(signet_challenge[0]);
+
+            LogPrintf("Signet with challenge %s\n", signet_challenge[0]);
         }
-        const auto signet_challenge = args.GetArgs("-signet_challenge");
-        if (signet_challenge.size() != 1) {
-            throw std::runtime_error(strprintf("%s: -signet_challenge cannot be multiple values.", __func__));
-        }
-        bin = ParseHex(signet_challenge[0]);
+
         if (args.IsArgSet("-signet_seednode")) {
             vSeeds = args.GetArgs("-signet_seednode");
         }
-
-        LogPrintf("Signet with challenge %s\n", signet_challenge[0]);
 
         strNetworkID = CBaseChainParams::SIGNET;
         consensus.signet_blocks = true;

BitgesellOfficial/bitgesell

src/chainparams.cpp
@@ -254,18 +254,24 @@ public:
         vSeeds.clear();
 
         if (!args.IsArgSet("-signetchallenge")) {
-            throw std::runtime_error(strprintf("%s: -signetchallenge is mandatory for signet networks", __func__));
+            LogPrintf("Using default signet network\n");
+            bin = ParseHex("512103ad5e0edad18cb1f0fc0d28a3d4f1f3e445640337489abb10404f2d1e086be430210359ef5021964fe22d6f8e05b2463c9540ce96883fe3b278760f048f5189f2e6c452ae");
+            vSeeds.emplace_back("178.128.221.177");
+            vSeeds.emplace_back("2a01:7c8:d005:390::5");
+            vSeeds.emplace_back("ntv3mtqw5wt63red.onion:38333");
+        } else {
+            const auto signet_challenge = args.GetArgs("-signetchallenge");
+            if (signet_challenge.size() != 1) {
+                throw std::runtime_error(strprintf("%s: -signetchallenge cannot be multiple values.", __func__));
+            }
+            bin = ParseHex(signet_challenge[0]);
+
+            LogPrintf("Signet with challenge %s\n", signet_challenge[0]);
         }
-        const auto signet_challenge = args.GetArgs("-signetchallenge");
-        if (signet_challenge.size() != 1) {
-            throw std::runtime_error(strprintf("%s: -signetchallenge cannot be multiple values.", __func__));
-        }
-        bin = ParseHex(signet_challenge[0]);
+
         if (args.IsArgSet("-signetseednode")) {
             vSeeds = args.GetArgs("-signetseednode");
         }
-
-        LogPrintf("Signet with challenge %s\n", signet_challenge[0]);
 
         strNetworkID = CBaseChainParams::SIGNET;
         consensus.signet_blocks = true;