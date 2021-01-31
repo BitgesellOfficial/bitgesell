285c99b61cc10640b2d3518e766891cc58348128
practicalswift <practicalswift@users.noreply.github.com>
2021-01-31 21:03:05 +0000

    net: Avoid UBSan warning in ProcessMessage(...)

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -2518,6 +2518,9 @@ void PeerManagerImpl::ProcessMessage(CNode& pfrom, const std::string& msg_type, 
         bool fRelay = true;
 
         vRecv >> nVersion >> nServiceInt >> nTime >> addrMe;
+        if (nTime < 0) {
+            nTime = 0;
+        }
         nServices = ServiceFlags(nServiceInt);
         if (!pfrom.IsInboundConn())
         {
