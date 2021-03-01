59a37192c8980b78dd4e1ccca571b76a6ea5e63f
VanOrton <>
2021-03-01 14:50:04 +0100

    File fix for dummywallet

--- src/dummywallet.cpp
+++ src/dummywallet.cpp
@@ -52,31 +52,6 @@ void DummyWalletInit::AddWalletOptions(ArgsManager& argsman) const
 
 const WalletInitInterface& g_wallet_init_interface = DummyWalletInit();
 
-fs::path GetWalletDir()
-{
-    throw std::logic_error("Wallet function called in non-wallet build.");
-}
-
-std::vector<fs::path> ListWalletDir()
-{
-    throw std::logic_error("Wallet function called in non-wallet build.");
-}
-
-std::vector<std::shared_ptr<CWallet>> GetWallets()
-{
-    throw std::logic_error("Wallet function called in non-wallet build.");
-}
-
-std::shared_ptr<CWallet> LoadWallet(interfaces::Chain& chain, const std::string& name, bilingual_str& error, std::vector<bilingual_str>& warnings)
-{
-    throw std::logic_error("Wallet function called in non-wallet build.");
-}
-
-WalletCreationStatus CreateWallet(interfaces::Chain& chain, const SecureString& passphrase, uint64_t wallet_creation_flags, const std::string& name, bilingual_str& error, std::vector<bilingual_str>& warnings, std::shared_ptr<CWallet>& result)
-{
-    throw std::logic_error("Wallet function called in non-wallet build.");
-}
-
 namespace interfaces {
 
 class Wallet;
