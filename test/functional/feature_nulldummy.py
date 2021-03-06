62362972340ff52e0454e2b421b6dbe07ba8e73a
Jon Atack <jon@atack.com>
2021-03-06 12:01:45 +0100

    test: use 327 fewer blocks in feature_nulldummy
    by generating 100 blocks for coinbase maturity instead of 427.
    
    This speeds up the test and should help avoid timeout errors.

--- test/functional/feature_nulldummy.py
+++ test/functional/feature_nulldummy.py
@@ -29,7 +29,7 @@ from test_framework.util import (
     assert_raises_rpc_error,
 )
 
-COINBASE_MATURITY = 427
+COINBASE_MATURITY = 100
 NULLDUMMY_ERROR = "non-mandatory-script-verify-flag (Dummy CHECKMULTISIG argument must be zero)"
 
 
@@ -48,7 +48,8 @@ def trueDummy(tx):
 
 class NULLDUMMYTest(BGLTestFramework):
     def set_test_params(self):
-        self.num_nodes = 1
+        # Need two nodes so GBT (getblocktemplate) doesn't complain that it's not connected.
+        self.num_nodes = 2
         self.setup_clean_chain = True
         # This script tests NULLDUMMY activation, which is part of the 'segwit' deployment, so we go through
         # normal segwit activation here (and don't use the default always-on behaviour).
