hebasto/bitcoin-qtw

Have feature_filelock.py test both bdb and sqlite, depending on compiled

test/functional/feature_filelock.py
@@ -4,6 +4,8 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Check that it's not possible to start a second bitcoind instance using the same datadir or wallet."""
 import os
+import random
+import string
 
 from test_framework.test_framework import BitcoinTestFramework
 from test_framework.test_node import ErrorMatch
@@ -27,11 +29,21 @@ class FilelockTest(BitcoinTestFramework):
         self.nodes[1].assert_start_raises_init_error(extra_args=['-datadir={}'.format(self.nodes[0].datadir), '-noserver'], expected_msg=expected_msg)
 
         if self.is_wallet_compiled():
-            self.nodes[0].createwallet(self.default_wallet_name)
-            wallet_dir = os.path.join(datadir, 'wallets')
-            self.log.info("Check that we can't start a second bitcoind instance using the same wallet")
-            expected_msg = "Error: Error initializing wallet database environment"
-            self.nodes[1].assert_start_raises_init_error(extra_args=['-walletdir={}'.format(wallet_dir), '-wallet=' + self.default_wallet_name, '-noserver'], expected_msg=expected_msg, match=ErrorMatch.PARTIAL_REGEX)
+            def check_wallet_filelock(descriptors):
+                wallet_name = ''.join([random.choice(string.ascii_lowercase) for _ in range(6)])
+                self.nodes[0].createwallet(wallet_name=wallet_name, descriptors=descriptors)
+                wallet_dir = os.path.join(datadir, 'wallets')
+                self.log.info("Check that we can't start a second bitcoind instance using the same wallet")
+                if descriptors:
+                    expected_msg = "Error: SQLiteDatabase: Unable to obtain an exclusive lock on the database, is it being used by another bitcoind?"
+                else:
+                    expected_msg = "Error: Error initializing wallet database environment"
+                self.nodes[1].assert_start_raises_init_error(extra_args=['-walletdir={}'.format(wallet_dir), '-wallet=' + wallet_name, '-noserver'], expected_msg=expected_msg, match=ErrorMatch.PARTIAL_REGEX)
+
+            if self.is_bdb_compiled():
+                check_wallet_filelock(False)
+            if self.is_sqlite_compiled():
+                check_wallet_filelock(True)
 
 if __name__ == '__main__':
     FilelockTest().main()

BitgesellOfficial/bitgesell

test/functional/feature_filelock.py
@@ -4,6 +4,8 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Check that it's not possible to start a second BGLd instance using the same datadir or wallet."""
 import os
+import random
+import string
 
 from test_framework.test_framework import BGLTestFramework
 from test_framework.test_node import ErrorMatch
@@ -27,11 +29,21 @@ class FilelockTest(BGLTestFramework):
         self.nodes[1].assert_start_raises_init_error(extra_args=['-datadir={}'.format(self.nodes[0].datadir), '-noserver'], expected_msg=expected_msg)
 
         if self.is_wallet_compiled():
-            self.nodes[0].createwallet(self.default_wallet_name)
-            wallet_dir = os.path.join(datadir, 'wallets')
-            self.log.info("Check that we can't start a second BGLd instance using the same wallet")
-            expected_msg = "Error: Error initializing wallet database environment"
-            self.nodes[1].assert_start_raises_init_error(extra_args=['-walletdir={}'.format(wallet_dir), '-wallet=' + self.default_wallet_name, '-noserver'], expected_msg=expected_msg, match=ErrorMatch.PARTIAL_REGEX)
+            def check_wallet_filelock(descriptors):
+                wallet_name = ''.join([random.choice(string.ascii_lowercase) for _ in range(6)])
+                self.nodes[0].createwallet(wallet_name=wallet_name, descriptors=descriptors)
+                wallet_dir = os.path.join(datadir, 'wallets')
+                self.log.info("Check that we can't start a second BGLd instance using the same wallet")
+                if descriptors:
+                    expected_msg = "Error: SQLiteDatabase: Unable to obtain an exclusive lock on the database, is it being used by another BGLd?"
+                else:
+                    expected_msg = "Error: Error initializing wallet database environment"
+                self.nodes[1].assert_start_raises_init_error(extra_args=['-walletdir={}'.format(wallet_dir), '-wallet=' + wallet_name, '-noserver'], expected_msg=expected_msg, match=ErrorMatch.PARTIAL_REGEX)
+
+            if self.is_bdb_compiled():
+                check_wallet_filelock(False)
+            if self.is_sqlite_compiled():
+                check_wallet_filelock(True)
 
 if __name__ == '__main__':
     FilelockTest().main()