d516886f50801a93993c2c4988f24deb51aa8100
Jon Atack <jon@atack.com>
2021-04-23 14:57:05 +0200

    gui: add Direction column to peers tab
    Co-authored-by: Jarol Rodriguez <jarolrod@tutanota.com>

--- src/qt/peertablemodel.h
+++ src/qt/peertablemodel.h
@@ -60,6 +60,7 @@ public:
     enum ColumnIndex {
         NetNodeId = 0,
         Address,
+        Direction,
         ConnectionType,
         Network,
         Ping,
@@ -95,6 +96,9 @@ private:
         /*: Title of Peers Table column which contains the
             IP/Onion/I2P address of the connected peer. */
         tr("Address"),
+        /*: Title of Peers Table column which indicates the direction
+            the peer connection was initiated from. */
+        tr("Direction"),
         /*: Title of Peers Table column which describes the type of
             peer connection. The "type" describes why the connection exists. */
         tr("Type"),
