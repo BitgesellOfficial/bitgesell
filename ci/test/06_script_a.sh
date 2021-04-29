hebasto/bitcoin-qtw

CI: add Android APK build script

ci/test/06_script_a.sh
@@ -6,6 +6,14 @@
 
 export LC_ALL=C.UTF-8
 
+if [ -n "$ANDROID_TOOLS_URL" ]; then
+  DOCKER_EXEC make distclean || true
+  DOCKER_EXEC ./autogen.sh
+  DOCKER_EXEC ./configure $BITCOIN_CONFIG --prefix=$DEPENDS_DIR/aarch64-linux-android || ( (DOCKER_EXEC cat config.log) && false)
+  DOCKER_EXEC "cd src/qt && make $MAKEJOBS && ANDROID_HOME=${ANDROID_HOME} ANDROID_NDK_HOME=${ANDROID_NDK_HOME} make apk"
+  exit 0
+fi
+
 BITCOIN_CONFIG_ALL="--enable-suppress-external-warnings --disable-dependency-tracking --prefix=$DEPENDS_DIR/$HOST --bindir=$BASE_OUTDIR/bin --libdir=$BASE_OUTDIR/lib"
 if [ -z "$NO_WERROR" ]; then
   BITCOIN_CONFIG_ALL="${BITCOIN_CONFIG_ALL} --enable-werror"

BitgesellOfficial/bitgesell

ci/test/06_script_a.sh
@@ -6,6 +6,14 @@
 
 export LC_ALL=C.UTF-8
 
+if [ -n "$ANDROID_TOOLS_URL" ]; then
+  DOCKER_EXEC make distclean || true
+  DOCKER_EXEC ./autogen.sh
+  DOCKER_EXEC ./configure $BGL_CONFIG --prefix=$DEPENDS_DIR/aarch64-linux-android || ( (DOCKER_EXEC cat config.log) && false)
+  DOCKER_EXEC "cd src/qt && make $MAKEJOBS && ANDROID_HOME=${ANDROID_HOME} ANDROID_NDK_HOME=${ANDROID_NDK_HOME} make apk"
+  exit 0
+fi
+
 BGL_CONFIG_ALL="--enable-suppress-external-warnings --disable-dependency-tracking --prefix=$DEPENDS_DIR/$HOST --bindir=$BASE_OUTDIR/bin --libdir=$BASE_OUTDIR/lib"
 if [ -z "$NO_WERROR" ]; then
   BGL_CONFIG_ALL="${BGL_CONFIG_ALL} --enable-werror"