339cc1659a654237a5acf5bf3530505d8aa5bb22
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2021-05-03 11:16:06 +0300

    qt, build: Optimize string concatenation
    The defined QT_USE_QSTRINGBUILDER macro means using the QStringBuilder
    for efficient string concatenation in all Qt code by default.

--- src/qt/recentrequeststablemodel.cpp
+++ src/qt/recentrequeststablemodel.cpp
@@ -14,6 +14,9 @@
 
 #include <utility>
 
+#include <QLatin1Char>
+#include <QLatin1String>
+
 RecentRequestsTableModel::RecentRequestsTableModel(WalletModel *parent) :
     QAbstractTableModel(parent), walletModel(parent)
 {
@@ -124,7 +127,11 @@ void RecentRequestsTableModel::updateAmountColumnTitle()
 /** Gets title for amount column including current display unit if optionsModel reference available. */
 QString RecentRequestsTableModel::getAmountTitle()
 {
-    return (this->walletModel->getOptionsModel() != nullptr) ? tr("Requested") + " ("+BGLUnits::shortName(this->walletModel->getOptionsModel()->getDisplayUnit()) + ")" : "";
+    if (!walletModel->getOptionsModel()) return {};
+    return tr("Requested") +
+           QLatin1String(" (") +
+           BGLUnits::shortName(this->walletModel->getOptionsModel()->getDisplayUnit()) +
+           QLatin1Char(')');
 }
 
 QModelIndex RecentRequestsTableModel::index(int row, int column, const QModelIndex &parent) const
