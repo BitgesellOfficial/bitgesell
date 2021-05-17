861abfc5da36b3d4dbea36bce1a4304925c369ba
Tawanda Kembo <tkembo@gmail.com>
2021-05-17 23:24:56 +0200

    comments out titleVersionVSpace variable declaration

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -41,7 +41,7 @@ SplashScreen::SplashScreen(Qt::WindowFlags f, const NetworkStyle *networkStyle) 
     // set reference point, paddings
     // int paddingRight            = 14;
     // int paddingTop              = 490;
-    int titleVersionVSpace      = 17;
+    // int titleVersionVSpace      = 17;
     // int titleCopyrightVSpace    = 22;
 
     float fontFactor            = 1.0;
