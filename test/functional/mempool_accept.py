7c998183b5f45755d06c77293e05c4a18f027b5f
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2021-07-01 01:43:45 +0200

    test: remove confusing `MAX_BLOCK_BASE_SIZE`
    The constant `MAX_BLOCK_BASE_SIZE` has been removed from the
    core implementation years ago due to being confusing and
    superfluous, as it is implied by the block weight limit (see
    PRs #10618 and #10608). Since there is also no point in
    still keeping it in the functional test framework, we switch
    to weight-based accounting on the relevant test code parts
    and use `MAX_BLOCK_WEIGHT` instead for the block limit
    checks.

--- test/functional/mempool_accept.py
+++ test/functional/mempool_accept.py
@@ -13,7 +13,7 @@ from test_framework.messages import (
     COIN,
     COutPoint,
     CTxOut,
-    MAX_BLOCK_BASE_SIZE,
+    MAX_BLOCK_WEIGHT,
     MAX_MONEY,
     tx_from_hex,
 )
@@ -203,7 +203,7 @@ class MempoolAcceptanceTest(BGLTestFramework):
 
         self.log.info('A really large transaction')
         tx = tx_from_hex(raw_tx_reference)
-        tx.vin = [tx.vin[0]] * math.ceil(MAX_BLOCK_BASE_SIZE / len(tx.vin[0].serialize()))
+        tx.vin = [tx.vin[0]] * math.ceil(MAX_BLOCK_WEIGHT // 4 / len(tx.vin[0].serialize()))
         self.check_mempool_result(
             result_expected=[{'txid': tx.rehash(), 'allowed': False, 'reject-reason': 'bad-txns-oversize'}],
             rawtxs=[tx.serialize().hex()],
