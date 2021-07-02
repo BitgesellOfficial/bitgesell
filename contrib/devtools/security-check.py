1aa18970f20b2fa0eda27bb2fcd5b3b17d5935de
fanquake <fanquake@gmail.com>
2021-07-02 19:53:33 +0800

    scripts: use LIEF for ELF checks in security-check.py

--- contrib/devtools/security-check.py
+++ contrib/devtools/security-check.py
@@ -12,89 +12,6 @@ import sys
 from typing import List
 
 import lief
-import pixie
-
-OBJDUMP_CMD = os.getenv('OBJDUMP', '/usr/bin/objdump')
-
-def run_command(command) -> str:
-    p = subprocess.run(command, stdout=subprocess.PIPE, check=True, universal_newlines=True)
-    return p.stdout
-
-def check_ELF_PIE(executable) -> bool:
-    '''
-    Check for position independent executable (PIE), allowing for address space randomization.
-    '''
-    stdout = run_command([READELF_CMD, '-h', '-W', executable])
-
-    ok = False
-    for line in stdout.splitlines():
-        tokens = line.split()
-        if len(line)>=2 and tokens[0] == 'Type:' and tokens[1] == 'DYN':
-            ok = True
-    return ok
-
-def get_ELF_program_headers(executable):
-    '''Return type and flags for ELF program headers'''
-    stdout = run_command([READELF_CMD, '-l', '-W', executable])
-
-    in_headers = False
-    headers = []
-    for line in stdout.splitlines():
-        if line.startswith('Program Headers:'):
-            in_headers = True
-            count = 0
-        if line == '':
-            in_headers = False
-        if in_headers:
-            if count == 1: # header line
-                header = [x.strip() for x in line.split()]
-                ofs_typ = header.index('Type')
-                ofs_flags = header.index('Flg')
-                # assert readelf output is what we expect
-                if ofs_typ == -1 or ofs_flags == -1:
-                    raise ValueError('Cannot parse elfread -lW output')
-            elif count > 1:
-                splitline = [x.strip() for x in line.split()]
-                typ = splitline[ofs_typ]
-                if not typ.startswith('[R'): # skip [Requesting ...]
-                    splitline = [x.strip() for x in line.split()]
-                    flags = splitline[ofs_flags]
-                    # check for 'R', ' E'
-                    if splitline[ofs_flags + 1] == 'E':
-                        flags += ' E'
-                    headers.append((typ, flags, []))
-            count += 1
-
-        if line.startswith(' Section to Segment mapping:'):
-            in_mapping = True
-            count = 0
-        if line == '':
-            in_mapping = False
-        if in_mapping:
-            if count == 1: # header line
-                ofs_segment = line.find('Segment')
-                ofs_sections = line.find('Sections...')
-                if ofs_segment == -1 or ofs_sections == -1:
-                    raise ValueError('Cannot parse elfread -lW output')
-            elif count > 1:
-                segment = int(line[ofs_segment:ofs_sections].strip())
-                sections = line[ofs_sections:].strip().split()
-                headers[segment][2].extend(sections)
-            count += 1
-    return headers
-
-def check_ELF_NX(executable) -> bool:
-    '''
-    Check that no sections are writable and executable (including the stack)
-    '''
-    have_wx = False
-    have_gnu_stack = False
-    for (typ, flags, _) in get_ELF_program_headers(executable):
-        if typ == 'GNU_STACK':
-            have_gnu_stack = True
-        if 'W' in flags and 'E' in flags: # section is both writable and executable
-            have_wx = True
-    return have_gnu_stack and not have_wx
 
 def check_ELF_RELRO(binary) -> bool:
     '''
@@ -102,19 +19,25 @@ def check_ELF_RELRO(binary) -> bool:
     GNU_RELRO program header must exist
     Dynamic section must have BIND_NOW flag
     '''
+    binary = lief.parse(executable)
     have_gnu_relro = False
-    for (typ, flags, _) in get_ELF_program_headers(executable):
-        # Note: not checking flags == 'R': here as linkers set the permission differently
+    for segment in binary.segments:
+        # Note: not checking p_flags == PF_R: here as linkers set the permission differently
         # This does not affect security: the permission flags of the GNU_RELRO program
         # header are ignored, the PT_LOAD header determines the effective permissions.
         # However, the dynamic linker need to write to this area so these are RW.
         # Glibc itself takes care of mprotecting this area R after relocations are finished.
         # See also https://marc.info/?l=binutils&m=1498883354122353
-        if typ == 'GNU_RELRO':
+        if segment.type == lief.ELF.SEGMENT_TYPES.GNU_RELRO:
             have_gnu_relro = True
 
     have_bindnow = False
-    stdout = run_command([READELF_CMD, '-d', '-W', executable])
+    try:
+        flags = binary.get(lief.ELF.DYNAMIC_TAGS.FLAGS)
+        if flags.value & lief.ELF.DYNAMIC_FLAGS.BIND_NOW:
+            have_bindnow = True
+    except:
+        have_bindnow = False
 
     for line in stdout.splitlines():
         tokens = line.split()
@@ -126,6 +49,7 @@ def check_ELF_Canary(binary) -> bool:
     '''
     Check for use of stack canary
     '''
+    binary = lief.parse(executable)
     return binary.has_symbol('__stack_chk_fail')
 
 def check_ELF_separate_code(binary):
@@ -134,56 +58,60 @@ def check_ELF_separate_code(binary):
     based on their permissions. This checks for missing -Wl,-z,separate-code
     and potentially other problems.
     '''
+    binary = lief.parse(executable)
     R = lief.ELF.SEGMENT_FLAGS.R
     W = lief.ELF.SEGMENT_FLAGS.W
     E = lief.ELF.SEGMENT_FLAGS.X
     EXPECTED_FLAGS = {
         # Read + execute
-        '.init': 'R E',
-        '.plt': 'R E',
-        '.plt.got': 'R E',
-        '.plt.sec': 'R E',
-        '.text': 'R E',
-        '.fini': 'R E',
+        '.init': R | E,
+        '.plt': R | E,
+        '.plt.got': R | E,
+        '.plt.sec': R | E,
+        '.text': R | E,
+        '.fini': R | E,
         # Read-only data
-        '.interp': 'R',
-        '.note.gnu.property': 'R',
-        '.note.gnu.build-id': 'R',
-        '.note.ABI-tag': 'R',
-        '.gnu.hash': 'R',
-        '.dynsym': 'R',
-        '.dynstr': 'R',
-        '.gnu.version': 'R',
-        '.gnu.version_r': 'R',
-        '.rela.dyn': 'R',
-        '.rela.plt': 'R',
-        '.rodata': 'R',
-        '.eh_frame_hdr': 'R',
-        '.eh_frame': 'R',
-        '.qtmetadata': 'R',
-        '.gcc_except_table': 'R',
-        '.stapsdt.base': 'R',
+        '.interp': R,
+        '.note.gnu.property': R,
+        '.note.gnu.build-id': R,
+        '.note.ABI-tag': R,
+        '.gnu.hash': R,
+        '.dynsym': R,
+        '.dynstr': R,
+        '.gnu.version': R,
+        '.gnu.version_r': R,
+        '.rela.dyn': R,
+        '.rela.plt': R,
+        '.rodata': R,
+        '.eh_frame_hdr': R,
+        '.eh_frame': R,
+        '.qtmetadata': R,
+        '.gcc_except_table': R,
+        '.stapsdt.base': R,
         # Writable data
-        '.init_array': 'RW',
-        '.fini_array': 'RW',
-        '.dynamic': 'RW',
-        '.got': 'RW',
-        '.data': 'RW',
-        '.bss': 'RW',
+        '.init_array': R | W,
+        '.fini_array': R | W,
+        '.dynamic': R | W,
+        '.got': R | W,
+        '.data': R | W,
+        '.bss': R | W,
     }
+    if binary.header.machine_type == lief.ELF.ARCH.PPC64:
+        # .plt is RW on ppc64 even with separate-code
+        EXPECTED_FLAGS['.plt'] = R | W
     # For all LOAD program headers get mapping to the list of sections,
     # and for each section, remember the flags of the associated program header.
     flags_per_section = {}
-    for (typ, flags, sections) in get_ELF_program_headers(executable):
-        if typ == 'LOAD':
-            for section in sections:
-                assert(section not in flags_per_section)
-                flags_per_section[section] = flags
+    for segment in binary.segments:
+        if segment.type ==  lief.ELF.SEGMENT_TYPES.LOAD:
+            for section in segment.sections:
+                assert(section.name not in flags_per_section)
+                flags_per_section[section.name] = segment.flags
     # Spot-check ELF LOAD program header flags per section
     # If these sections exist, check them against the expected R/W/E flags
     for (section, flags) in flags_per_section.items():
         if section in EXPECTED_FLAGS:
-            if EXPECTED_FLAGS[section] != flags:
+            if int(EXPECTED_FLAGS[section]) != int(flags):
                 return False
     return True
 
@@ -246,8 +174,8 @@ def check_control_flow(binary) -> bool:
 
 CHECKS = {
 'ELF': [
-    ('PIE', check_ELF_PIE),
-    ('NX', check_ELF_NX),
+    ('PIE', check_PIE),
+    ('NX', check_NX),
     ('RELRO', check_ELF_RELRO),
     ('Canary', check_ELF_Canary),
     ('separate_code', check_ELF_separate_code),
