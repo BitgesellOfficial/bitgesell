787de5e5d055bb37b08c66a61af347992c66e81e
Carl Dong <contact@carldong.me>
2021-09-22 15:36:24 -0400

    node/chainstate: Decouple from ShutdownRequested
    ...instead allow optionally passing in a std::function<bool()>

--- src/init.cpp
+++ src/init.cpp
@@ -1437,6 +1437,7 @@ bool AppInitMain(NodeContext& node, interfaces::BlockAndHeaderTipInfo* tip_info)
                                 nBlockTreeDBCache,
                                 nCoinDBCache,
                                 nCoinCacheUsage,
+                                ShutdownRequested,
                                 []() {
                                     uiInterface.ThreadSafeMessageBox(
                                                                      _("Error reading from database, shutting down."),
