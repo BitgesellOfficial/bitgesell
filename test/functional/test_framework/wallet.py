11c4e2717e2d9346a0554d495072b022ab0778a7
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2021-09-24 17:26:27 +0200

    test: introduce helper to create random P2WPKH scriptPubKeys

--- test/functional/test_framework/wallet.py
+++ test/functional/test_framework/wallet.py
@@ -28,6 +28,7 @@ from test_framework.script import (
     OP_NOP,
     SIGHASH_ALL,
 )
+from test_framework.script_util import key_to_p2wpkh_script
 from test_framework.util import (
     assert_equal,
     assert_greater_than_or_equal,
@@ -189,7 +190,15 @@ class MiniWallet:
         return txid
 
 
-def make_chain(node, address, privkeys, parent_txid, parent_value, n=0, parent_locking_script=None):
+def random_p2wpkh():
+    """Generate a random P2WPKH scriptPubKey. Can be used when a random destination is needed,
+    but no compiled wallet is available (e.g. as replacement to the getnewaddress RPC)."""
+    key = ECKey()
+    key.generate()
+    return key_to_p2wpkh_script(key.get_pubkey().get_bytes())
+
+
+def make_chain(node, address, privkeys, parent_txid, parent_value, n=0, parent_locking_script=None, fee=DEFAULT_FEE):
     """Build a transaction that spends parent_txid.vout[n] and produces one output with
     amount = parent_value with a fee deducted.
     Return tuple (CTransaction object, raw hex, nValue, scriptPubKey of the output created).
