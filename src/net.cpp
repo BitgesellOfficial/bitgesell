dbc6774fbb92ebeea7d3ac8cfca0c67aac338c61
Douglas Chimento <dchimento@gmail.com>
2021-09-29 10:59:33 +0300

    doc: Remove TODO 'exclude peers with download permission'

--- src/net.cpp
+++ src/net.cpp
@@ -2884,7 +2884,6 @@ void CConnman::RecordBytesSent(uint64_t bytes)
         nMaxOutboundTotalBytesSentInCycle = 0;
     }
 
-    // TODO, exclude peers with download permission
     nMaxOutboundTotalBytesSentInCycle += bytes;
 }
 
