6a7383c160303b7126f08a2f34d7af10579dfcbd
ijanus <emekamicro@gmail.com>
2021-10-08 01:49:10 +0100

    Fix rpcnestedtests

--- src/qt/test/rpcnestedtests.cpp
+++ src/qt/test/rpcnestedtests.cpp
@@ -80,7 +80,7 @@ void RPCNestedTests::rpcNestedTests()
     QVERIFY(result == result2);
 
     RPCConsole::RPCExecuteCommandLine(m_node, result, "getblock(getbestblockhash())[tx][0]", &filtered);
-    QVERIFY(result == "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b");
+    QVERIFY(result == "0926305e87ec7ee71ed8d119c9b4958cb22f298cc20f90586c35b5a47e1c3f9b");
     QVERIFY(filtered == "getblock(getbestblockhash())[tx][0]");
 
     RPCConsole::RPCParseCommandLine(nullptr, result, "importprivkey", false, &filtered);
