cef1dba22e5521881146aa4ab95a8d6e7c0ae4b7
Korrrba <gitcoindev@gmail.com>
2021-11-16 16:07:44 +0100

    test: functional: test_framework: p2p: Set asyncio.WindowsSelectorEventLoopPolicy for Windows and send additional msg during p2p connection.
    Ref: https://gitcoin.co/issue/bitgesellofficial/bitgesell/62/100027021

--- test/functional/test_framework/p2p.py
+++ test/functional/test_framework/p2p.py
@@ -437,6 +437,7 @@ class P2PInterface(P2PConnection):
             self.send_message(msg_sendaddrv2())
         self.send_message(msg_verack())
         self.nServices = message.nServices
+        self.send_message(msg_getaddr())
 
     # Connection helper methods
 
@@ -575,6 +576,8 @@ class NetworkThread(threading.Thread):
 
         NetworkThread.listeners = {}
         NetworkThread.protos = {}
+        if sys.platform == 'win32':
+            asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
         NetworkThread.network_event_loop = asyncio.new_event_loop()
 
     def run(self):
