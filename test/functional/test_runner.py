7e6c2ad2273a3f6ca89dcdd7c9c85442f44cfdb2
Korrrba <gitcoindev@gmail.com>
2021-11-17 09:37:33 +0100

    test: functional: test_runner: Disable feature_dersig test.
    The feature_dersig test is not applicable to BGL, it verifies BIP66
    soft fork before and after block 1251.
    
    BGL has this built in from block 0:
    
    https://github.com/BitgesellOfficial/bitgesell/blob/d3b67375c6931b580aad05afa44b0ae7bae9fcaf/src/chainparams.cpp#L75.
    
    Ref: https://gitcoin.co/issue/bitgesellofficial/bitgesell/62/100027021

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -246,7 +246,6 @@ BASE_SCRIPTS = [
     'p2p_leak.py',
     'wallet_encryption.py --legacy-wallet',
     'wallet_encryption.py --descriptors',
-    'feature_dersig.py',
     'feature_cltv.py',
     'rpc_uptime.py',
     'wallet_resendwallettransactions.py --legacy-wallet',
