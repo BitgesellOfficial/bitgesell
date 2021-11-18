92ce4f8efb24da9d1fb38366627b2054b4d1885b
Sjors Provoost <sjors@sprovoost.nl>
2021-11-18 15:06:49 +0100

    test: v0.20.1 backwards compatibility
    The file checksums were added in an earlier commit. Since the DMG
    file is never downloaded, we drop that checksum.

--- test/functional/feature_backwards_compatibility.py
+++ test/functional/feature_backwards_compatibility.py
@@ -35,12 +35,11 @@ from test_framework.util import (
 class BackwardsCompatibilityTest(BGLTestFramework):
     def set_test_params(self):
         self.setup_clean_chain = True
-        self.num_nodes = 8
+        self.num_nodes = 7
         # Add new version after each release:
         self.extra_args = [
             ["-addresstype=bech32"], # Pre-release: use to mine blocks
             ["-nowallet", "-walletrbf=1", "-addresstype=bech32"], # Pre-release: use to receive coins, swap wallets, etc
-            ["-nowallet", "-walletrbf=1", "-addresstype=bech32"], # v0.21.0
             ["-nowallet", "-walletrbf=1", "-addresstype=bech32"], # v0.20.1
             ["-nowallet", "-walletrbf=1", "-addresstype=bech32"], # v0.19.1
             ["-nowallet", "-walletrbf=1", "-addresstype=bech32"], # v0.18.1
@@ -57,7 +56,6 @@ class BackwardsCompatibilityTest(BGLTestFramework):
         self.add_nodes(self.num_nodes, extra_args=self.extra_args, versions=[
             None,
             None,
-            210000,
             200100,
             190100,
             180100,
