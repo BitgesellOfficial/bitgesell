f11c0de549ec90bf774bca1325e6ccd65420f8be
ayushkumar63123 <86553805+ayushkumar63123@users.noreply.github.com>
2021-11-26 14:21:41 +0530

    Update makeseeds.py

--- contrib/seeds/makeseeds.py
+++ contrib/seeds/makeseeds.py
@@ -156,9 +156,9 @@ def main():
 
     for ip in ips:
         if ip['net'] == 'ipv6':
-            print '[%s]:%i' % (ip['ip'], ip['port'])
+            print('[%s]:%i' % (ip['ip'], ip['port']))
         else:
-            print '%s:%i' % (ip['ip'], ip['port'])
+            print('%s:%i' % (ip['ip'], ip['port']))
 
 if __name__ == '__main__':
     main()
