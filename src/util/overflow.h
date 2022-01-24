f8f1c1d8588d470ef688bbf94f79594e4b8b8b56
Martin Zumsande <mzumsande@gmail.com>
2022-01-24 14:34:28 +0100

    index: Commit DB_MUHASH and DB_BEST_BLOCK to disk together
    If these are written to disk at different times,
    unclean shutdowns can lead to index corruption.

--- src/util/overflow.h
+++ src/util/overflow.h
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_UTIL_OVERFLOW_H
-#define BITCOIN_UTIL_OVERFLOW_H
+#ifndef BGL_UTIL_OVERFLOW_H
+#define BGL_UTIL_OVERFLOW_H
 
 #include <limits>
 #include <optional>
@@ -13,7 +13,7 @@ template <class T>
 [[nodiscard]] bool AdditionOverflow(const T i, const T j) noexcept
 {
     static_assert(std::is_integral<T>::value, "Integral required.");
-    if (std::numeric_limits<T>::is_signed) {
+    if constexpr (std::numeric_limits<T>::is_signed) {
         return (i > 0 && j > std::numeric_limits<T>::max() - i) ||
                (i < 0 && j < std::numeric_limits<T>::min() - i);
     }
@@ -29,4 +29,22 @@ template <class T>
     return i + j;
 }
 
-#endif // BITCOIN_UTIL_OVERFLOW_H
+template <class T>
+[[nodiscard]] T SaturatingAdd(const T i, const T j) noexcept
+{
+    if constexpr (std::numeric_limits<T>::is_signed) {
+        if (i > 0 && j > std::numeric_limits<T>::max() - i) {
+            return std::numeric_limits<T>::max();
+        }
+        if (i < 0 && j < std::numeric_limits<T>::min() - i) {
+            return std::numeric_limits<T>::min();
+        }
+    } else {
+        if (std::numeric_limits<T>::max() - i < j) {
+            return std::numeric_limits<T>::max();
+        }
+    }
+    return i + j;
+}
+
+#endif // BGL_UTIL_OVERFLOW_H
