24b0752685aad6be59fe606827c315e2d6dafc40
MarcoFalke <falke.marco@gmail.com>
2022-02-08 20:24:47 +0100

    ci: Bump fuzz tasks to jammy
    This gives them a newer clang version, which may have more sanitizers
    available.

--- ci/test/00_setup_env_native_fuzz_with_valgrind.sh
+++ ci/test/00_setup_env_native_fuzz_with_valgrind.sh
@@ -6,6 +6,7 @@
 
 export LC_ALL=C.UTF-8
 
+export DOCKER_NAME_TAG="ubuntu:22.04"
 export CONTAINER_NAME=ci_native_fuzz_valgrind
 export PACKAGES="clang llvm python3 libevent-dev bsdmainutils libboost-dev libboost-system-dev libboost-test-dev libsqlite3-dev valgrind"
 export NO_DEPENDS=1
