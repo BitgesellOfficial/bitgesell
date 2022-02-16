33ca16aba1f96865c6922f932954d48cadbdc949
ijanus <emekamicro@gmail.com>
2022-02-16 06:18:07 +0100

    Return Berkeley Database  as the preferred Database
    This is temporary solution until for now.
    Fix test_BGL_qt

--- src/wallet/walletdb.cpp
+++ src/wallet/walletdb.cpp
@@ -1176,9 +1176,11 @@ std::unique_ptr<WalletDatabase> CreateDummyWalletDatabase()
 /** Return object for accessing temporary in-memory database. */
 std::unique_ptr<WalletDatabase> CreateMockWalletDatabase()
 {
-#ifdef USE_SQLITE
-    return std::make_unique<SQLiteDatabase>("", "", true);
-#elif USE_BDB
+#ifdef USE_BDB
+    //return std::make_unique<SQLiteDatabase>("", "", true);
     return std::make_unique<BerkeleyDatabase>(std::make_shared<BerkeleyEnvironment>(), "");
+#elif USE_SQLITE
+    //return std::make_unique<BerkeleyDatabase>(std::make_shared<BerkeleyEnvironment>(), "");
+    return std::make_unique<SQLiteDatabase>("", "", true);
 #endif
 }
