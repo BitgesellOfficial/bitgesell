0a132799d226a276fd058e09d4a7f17cfdd86784
fanquake <fanquake@gmail.com>
2022-02-21 15:30:46 +0000

    refactor: shift CopyrightHolders() and LicenseInfo() to clientversion.cpp

--- src/init.h
+++ src/init.h
@@ -70,7 +70,4 @@ bool AppInitMain(node::NodeContext& node, interfaces::BlockAndHeaderTipInfo* tip
  */
 void SetupServerArgs(ArgsManager& argsman);
 
-/** Returns licensing information (for -version) */
-std::string LicenseInfo();
-
 #endif // BGL_INIT_H
