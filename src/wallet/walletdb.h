14cc259d4870f9857d71a2a47d3c3b44ecc984e5
Andrew Chow <achow101-github@achow101.com>
2022-04-13 16:07:59 -0400

    walletdb: Refactor encryption key loading to its own function

--- src/wallet/walletdb.h
+++ src/wallet/walletdb.h
@@ -307,6 +307,7 @@ bool ReadKeyValue(CWallet* pwallet, DataStream& ssKey, CDataStream& ssValue, std
 
 bool LoadKey(CWallet* pwallet, DataStream& ssKey, DataStream& ssValue, std::string& strErr);
 bool LoadCryptedKey(CWallet* pwallet, DataStream& ssKey, DataStream& ssValue, std::string& strErr);
+bool LoadEncryptionKey(CWallet* pwallet, DataStream& ssKey, DataStream& ssValue, std::string& strErr);
 } // namespace wallet
 
 #endif // BGL_WALLET_WALLETDB_H
