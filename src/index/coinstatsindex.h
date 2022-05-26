2545689921be6354f9fec32367d9d34ab896da1f
MacroFake <falke.marco@gmail.com>
2022-05-26 17:06:07 +0200

    refactor: Pass reference to LookUpStats

--- src/index/coinstatsindex.h
+++ src/index/coinstatsindex.h
@@ -6,11 +6,14 @@
 #ifndef BGL_INDEX_COINSTATSINDEX_H
 #define BGL_INDEX_COINSTATSINDEX_H
 
-#include <chain.h>
 #include <crypto/muhash.h>
-#include <flatfile.h>
 #include <index/base.h>
-#include <kernel/coinstats.h>
+
+class CBlockIndex;
+class CDBBatch;
+namespace kernel {
+struct CCoinsStats;
+}
 
 /**
  * CoinStatsIndex maintains statistics on the UTXO set.
@@ -57,7 +60,7 @@ public:
     explicit CoinStatsIndex(std::unique_ptr<interfaces::Chain> chain, size_t n_cache_size, bool f_memory = false, bool f_wipe = false);
 
     // Look up stats for a specific block using CBlockIndex
-    std::optional<kernel::CCoinsStats> LookUpStats(const CBlockIndex* block_index) const;
+    std::optional<kernel::CCoinsStats> LookUpStats(const CBlockIndex& block_index) const;
 };
 
 /// The global UTXO set hash object.
