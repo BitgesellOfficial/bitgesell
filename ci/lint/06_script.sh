75e577804ed70e2707edb881c67dcaba670b81a3
Martin Leitner-Ankerl <martin.leitner-ankerl@dynatrace.com>
2022-06-07 10:22:45 +0200

    test: rename lint-all.py to all-lint.py
    That way it is impossible for the script to call itself.

--- ci/lint/06_script.sh
+++ ci/lint/06_script.sh
@@ -18,7 +18,7 @@ test/lint/git-subtree-check.sh src/univalue
 test/lint/git-subtree-check.sh src/leveldb
 test/lint/git-subtree-check.sh src/crc32c
 test/lint/check-doc.py
-test/lint/lint-all.py
+test/lint/all-lint.py
 
 if [ "$CIRRUS_REPO_FULL_NAME" = "BGL/BGL" ] && [ "$CIRRUS_PR" = "" ] ; then
     # Sanity check only the last few commits to get notified of missing sigs,
