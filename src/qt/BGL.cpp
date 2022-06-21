2a3bb6b91f0a344b73e7de34c048f645ff2aa206
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-06-21 13:14:21 +0200

    qt6: Fix type registration
    In Qt 6, registration of `QDataStream` streaming operators is done
    automatically. Consequently, `qRegisterMetaTypeStreamOperators()` does
    no longer exist. Calls to this method have to be removed when porting
    to Qt 6.
    
    See https://doc.qt.io/qt-6/qtcore-changes-qt6.html#the-qmetatype-class

--- src/qt/BGL.cpp
+++ src/qt/BGL.cpp
@@ -96,7 +96,11 @@ static void RegisterMetaTypes()
     qRegisterMetaType<QMessageBox::Icon>("QMessageBox::Icon");
     qRegisterMetaType<interfaces::BlockAndHeaderTipInfo>("interfaces::BlockAndHeaderTipInfo");
 
+#if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0))
     qRegisterMetaTypeStreamOperators<BGLUnit>("BGLUnit");
+#else
+    qRegisterMetaType<BGLUnit>("BGLUnit");
+#endif
 }
 
 static QString GetLangTerritory()
