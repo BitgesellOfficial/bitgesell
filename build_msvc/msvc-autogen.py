hebasto/bitcoin-qtw

build_msvc/bitcoin_config.h is generated using build_msvc/msvc-autogen.py

build_msvc/msvc-autogen.py
@@ -57,6 +57,41 @@ def set_common_properties(toolset):
     with open(os.path.join(SOURCE_DIR, '../build_msvc/common.init.vcxproj'), 'w', encoding='utf-8',newline='\n') as wfile:
         wfile.write(s)
 
+def parse_config_into_btc_config():
+    def find_between( s, first, last ):
+        try:
+            start = s.index( first ) + len( first )
+            end = s.index( last, start )
+            return s[start:end]
+        except ValueError:
+            return ""
+
+    config_info = []
+    with open(os.path.join(SOURCE_DIR,'../configure.ac'), encoding="utf8") as f:
+        for line in f:
+            if line.startswith("define"):
+                config_info.append(find_between(line, "(_", ")"))
+
+    config_info = [c for c in config_info if not c.startswith("COPYRIGHT_HOLDERS")]
+
+    config_dict = dict(item.split(", ") for item in config_info)
+    config_dict["PACKAGE_VERSION"] = f"\"{config_dict['CLIENT_VERSION_MAJOR']}.{config_dict['CLIENT_VERSION_MINOR']}.{config_dict['CLIENT_VERSION_BUILD']}\""
+    version = config_dict["PACKAGE_VERSION"].strip('"')
+    config_dict["PACKAGE_STRING"] = f"\"Bitcoin Core {version}\""
+
+    with open(os.path.join(SOURCE_DIR,'../build_msvc/bitcoin_config.h.in'), "r", encoding="utf8") as template_file:
+        template = template_file.readlines()
+
+    for index, line in enumerate(template):
+        header = ""
+        if line.startswith("#define"):
+            header = line.split(" ")[1]
+        if header in config_dict:
+            template[index] = line.replace("$", f"{config_dict[header]}")
+
+    with open(os.path.join(SOURCE_DIR,'../build_msvc/bitcoin_config.h'), "w", encoding="utf8") as btc_config:
+        btc_config.writelines(template)
+
 def main():
     parser = argparse.ArgumentParser(description='Bitcoin-core msbuild configuration initialiser.')
     parser.add_argument('-toolset', nargs='?',help='Optionally sets the msbuild platform toolset, e.g. v142 for Visual Studio 2019.'
@@ -79,6 +114,7 @@ def main():
             with open(vcxproj_filename, 'w', encoding='utf-8') as vcxproj_file:
                 vcxproj_file.write(vcxproj_in_file.read().replace(
                     '@SOURCE_FILES@\n', content))
+    parse_config_into_btc_config()
     copyfile(os.path.join(SOURCE_DIR,'../build_msvc/bitcoin_config.h'), os.path.join(SOURCE_DIR, 'config/bitcoin-config.h'))
     copyfile(os.path.join(SOURCE_DIR,'../build_msvc/libsecp256k1_config.h'), os.path.join(SOURCE_DIR, 'secp256k1/src/libsecp256k1-config.h'))
 

BitgesellOfficial/bitgesell

build_msvc/msvc-autogen.py
@@ -55,6 +55,41 @@ def set_common_properties(toolset):
     with open(os.path.join(SOURCE_DIR, '../build_msvc/common.init.vcxproj'), 'w', encoding='utf-8',newline='\n') as wfile:
         wfile.write(s)
 
+def parse_config_into_bgl_config():
+    def find_between( s, first, last ):
+        try:
+            start = s.index( first ) + len( first )
+            end = s.index( last, start )
+            return s[start:end]
+        except ValueError:
+            return ""
+
+    config_info = []
+    with open(os.path.join(SOURCE_DIR,'../configure.ac'), encoding="utf8") as f:
+        for line in f:
+            if line.startswith("define"):
+                config_info.append(find_between(line, "(_", ")"))
+
+    config_info = [c for c in config_info if not c.startswith("COPYRIGHT_HOLDERS")]
+
+    config_dict = dict(item.split(", ") for item in config_info)
+    config_dict["PACKAGE_VERSION"] = f"\"{config_dict['CLIENT_VERSION_MAJOR']}.{config_dict['CLIENT_VERSION_MINOR']}.{config_dict['CLIENT_VERSION_BUILD']}\""
+    version = config_dict["PACKAGE_VERSION"].strip('"')
+    config_dict["PACKAGE_STRING"] = f"\"BGL Core {version}\""
+
+    with open(os.path.join(SOURCE_DIR,'../build_msvc/BGL_config.h.in'), "r", encoding="utf8") as template_file:
+        template = template_file.readlines()
+
+    for index, line in enumerate(template):
+        header = ""
+        if line.startswith("#define"):
+            header = line.split(" ")[1]
+        if header in config_dict:
+            template[index] = line.replace("$", f"{config_dict[header]}")
+
+    with open(os.path.join(SOURCE_DIR,'../build_msvc/BGL_config.h'), "w", encoding="utf8") as btc_config:
+        btc_config.writelines(template)
+
 def main():
     parser = argparse.ArgumentParser(description='BGL-core msbuild configuration initialiser.')
     parser.add_argument('-toolset', nargs='?',help='Optionally sets the msbuild platform toolset, e.g. v142 for Visual Studio 2019.'
@@ -77,6 +112,7 @@ def main():
             with open(vcxproj_filename, 'w', encoding='utf-8') as vcxproj_file:
                 vcxproj_file.write(vcxproj_in_file.read().replace(
                     '@SOURCE_FILES@\n', content))
+    parse_config_into_bgl_config()
     copyfile(os.path.join(SOURCE_DIR,'../build_msvc/BGL_config.h'), os.path.join(SOURCE_DIR, 'config/BGL-config.h'))
     copyfile(os.path.join(SOURCE_DIR,'../build_msvc/libsecp256k1_config.h'), os.path.join(SOURCE_DIR, 'secp256k1/src/libsecp256k1-config.h'))
 