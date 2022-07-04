hebasto/bitcoin-qtw

test: add interface_bitcoin_cli.py --descriptors to test_runner.py

test/functional/test_runner.py
@@ -138,7 +138,8 @@ BASE_SCRIPTS = [
     'feature_fee_estimation.py',
     'interface_zmq.py',
     'rpc_invalid_address_message.py',
-    'interface_bitcoin_cli.py',
+    'interface_bitcoin_cli.py --legacy-wallet',
+    'interface_bitcoin_cli.py --descriptors',
     'feature_bind_extra.py',
     'mempool_resurrect.py',
     'wallet_txn_doublespend.py --mineblock',

BitgesellOfficial/bitgesell

test/functional/test_runner.py
@@ -137,7 +137,8 @@ BASE_SCRIPTS = [
     'feature_fee_estimation.py',
     'interface_zmq.py',
     'rpc_invalid_address_message.py',
-    'interface_BGL_cli.py',
+    'interface_BGL_cli.py --legacy-wallet',
+    'interface_BGL_cli.py --descriptors',
     'feature_bind_extra.py',
     'mempool_resurrect.py',
     'wallet_txn_doublespend.py --mineblock',