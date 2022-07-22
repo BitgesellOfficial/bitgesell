bitcoin/bitcoin

timedata: make it possible to reset the state
Add a new function `TestOnlyResetTimeData()` which would reset the
internal state used by `GetTimeOffset()`, `GetAdjustedTime()` and
`AddTimeData()`.

This is needed so that unit tests that call `AddTimeData()` can restore
the state in order not to confuse other tests that rely on it.

Currently `timedata_tests/addtimedata` is the only test that modifies
the state (via `AddTimeData()`) and also the only test that relies on
that state.

src/timedata.cpp
@@ -38,18 +38,20 @@ int64_t GetAdjustedTime()
 
 #define BITCOIN_TIMEDATA_MAX_SAMPLES 200
 
+static std::set<CNetAddr> setKnown;
+static CMedianFilter<int64_t> vTimeOffsets(BITCOIN_TIMEDATA_MAX_SAMPLES, 0);
+static bool fDone;
+
 void AddTimeData(const CNetAddr& ip, int64_t nOffsetSample)
 {
     LOCK(g_timeoffset_mutex);
     // Ignore duplicates
-    static std::set<CNetAddr> setKnown;
     if (setKnown.size() == BITCOIN_TIMEDATA_MAX_SAMPLES)
         return;
     if (!setKnown.insert(ip).second)
         return;
 
     // Add data
-    static CMedianFilter<int64_t> vTimeOffsets(BITCOIN_TIMEDATA_MAX_SAMPLES, 0);
     vTimeOffsets.input(nOffsetSample);
     LogPrint(BCLog::NET, "added time data, samples %d, offset %+d (%+d minutes)\n", vTimeOffsets.size(), nOffsetSample, nOffsetSample / 60);
 
@@ -80,7 +82,6 @@ void AddTimeData(const CNetAddr& ip, int64_t nOffsetSample)
         } else {
             nTimeOffset = 0;
 
-            static bool fDone;
             if (!fDone) {
                 // If nobody has a time different than ours but within 5 minutes of ours, give a warning
                 bool fMatch = false;
@@ -105,4 +106,13 @@ void AddTimeData(const CNetAddr& ip, int64_t nOffsetSample)
             LogPrint(BCLog::NET, "nTimeOffset = %+d  (%+d minutes)\n", nTimeOffset, nTimeOffset / 60);
         }
     }
+}
+
+void TestOnlyResetTimeData()
+{
+    LOCK(g_timeoffset_mutex);
+    nTimeOffset = 0;
+    setKnown.clear();
+    vTimeOffsets = CMedianFilter<int64_t>(BITCOIN_TIMEDATA_MAX_SAMPLES, 0);
+    fDone = false;
 }

BitgesellOfficial/bitgesell

src/timedata.cpp
@@ -39,18 +39,20 @@ int64_t GetAdjustedTime()
 
 #define BGL_TIMEDATA_MAX_SAMPLES 200
 
+static std::set<CNetAddr> setKnown;
+static CMedianFilter<int64_t> vTimeOffsets(BGL_TIMEDATA_MAX_SAMPLES, 0);
+static bool fDone;
+
 void AddTimeData(const CNetAddr& ip, int64_t nOffsetSample)
 {
     LOCK(g_timeoffset_mutex);
     // Ignore duplicates
-    static std::set<CNetAddr> setKnown;
     if (setKnown.size() == BGL_TIMEDATA_MAX_SAMPLES)
         return;
     if (!setKnown.insert(ip).second)
         return;
 
     // Add data
-    static CMedianFilter<int64_t> vTimeOffsets(BGL_TIMEDATA_MAX_SAMPLES, 0);
     vTimeOffsets.input(nOffsetSample);
     LogPrint(BCLog::NET, "added time data, samples %d, offset %+d (%+d minutes)\n", vTimeOffsets.size(), nOffsetSample, nOffsetSample / 60);
 
@@ -81,7 +83,6 @@ void AddTimeData(const CNetAddr& ip, int64_t nOffsetSample)
         } else {
             nTimeOffset = 0;
 
-            static bool fDone;
             if (!fDone) {
                 // If nobody has a time different than ours but within 5 minutes of ours, give a warning
                 bool fMatch = false;
@@ -107,4 +108,13 @@ void AddTimeData(const CNetAddr& ip, int64_t nOffsetSample)
             LogPrint(BCLog::NET, "%s\n", log_message);
         }
     }
+}
+
+void TestOnlyResetTimeData()
+{
+    LOCK(g_timeoffset_mutex);
+    nTimeOffset = 0;
+    setKnown.clear();
+    vTimeOffsets = CMedianFilter<int64_t>(BGL_TIMEDATA_MAX_SAMPLES, 0);
+    fDone = false;
 }