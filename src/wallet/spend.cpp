8db0e57930585d3061f40f2b9adf738c0e5d39e8
Andrew Chow <achow101-github@achow101.com>
2022-07-22 15:47:06 -0400

    wallet: Try estimating input size with external data if wallet fails
    Instead of choosing whether to use the wallet or external data when
    estimating the size of an input, first use the wallet, then try external
    data if that failed.

--- src/wallet/spend.cpp
+++ src/wallet/spend.cpp
@@ -561,8 +561,12 @@ std::optional<SelectionResult> SelectCoins(const CWallet& wallet, CoinsResult& a
             if (!coin_control.GetExternalOutput(outpoint, txout)) {
                 return std::nullopt;
             }
-            input_bytes = CalculateMaximumSignedInputSize(txout, &coin_control.m_external_provider, /*use_max_sig=*/true);
         }
+
+        if (input_bytes == -1) {
+            input_bytes = CalculateMaximumSignedInputSize(txout, outpoint, &coin_control.m_external_provider, &coin_control);
+        }
+
         // If available, override calculated size with coin control specified size
         if (coin_control.HasInputWeight(outpoint)) {
             input_bytes = GetVirtualTransactionSize(coin_control.GetInputWeight(outpoint), 0, 0);
