hebasto/bitcoin-qtw

test: determine path to `bitcoin-util` in test framework
The path is stored in `self.options.bitcoinutil`, points to
`src/bitcoin-util` by default and can be overrided with the
`BITCOINUTIL` environment variable.

test/functional/test_framework/test_framework.py
@@ -244,8 +244,14 @@ class BitcoinTestFramework(metaclass=BitcoinTestMetaClass):
             "src",
             "bitcoin-cli" + config["environment"]["EXEEXT"],
         )
+        fname_bitcoinutil = os.path.join(
+            config["environment"]["BUILDDIR"],
+            "src",
+            "bitcoin-util" + config["environment"]["EXEEXT"],
+        )
         self.options.bitcoind = os.getenv("BITCOIND", default=fname_bitcoind)
         self.options.bitcoincli = os.getenv("BITCOINCLI", default=fname_bitcoincli)
+        self.options.bitcoinutil = os.getenv("BITCOINUTIL", default=fname_bitcoinutil)
 
         os.environ['PATH'] = os.pathsep.join([
             os.path.join(config['environment']['BUILDDIR'], 'src'),

BitgesellOfficial/bitgesell

test/functional/test_framework/test_framework.py
@@ -245,8 +245,14 @@ class BGLTestFramework(metaclass=BGLTestMetaClass):
             "src",
             "BGL-cli" + config["environment"]["EXEEXT"]
         )
+        fname_BGLutil = os.path.join(
+            config["environment"]["BUILDDIR"],
+            "src",
+            "BGL-util" + config["environment"]["EXEEXT"],
+        )
         self.options.BGLd = os.getenv("BGLD", default=fname_BGLd)
         self.options.BGLcli = os.getenv("BGLCLI", default=fname_BGLcli)
+        self.options.BGLutil = os.getenv("BGLUTIL", default=fname_BGLutil)
 
         os.environ['PATH'] = os.pathsep.join([
             os.path.join(config['environment']['BUILDDIR'], 'src'),