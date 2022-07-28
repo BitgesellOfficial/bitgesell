bitcoin-ghost/bitcoin-core-ladder-script

Guard `#include <config/bitcoin-config.h>`

src/shutdown.cpp
@@ -5,12 +5,14 @@
 
 #include <shutdown.h>
 
+#if defined(HAVE_CONFIG_H)
+#include <config/bitcoin-config.h>
+#endif
+
 #include <logging.h>
 #include <node/ui_interface.h>
 #include <util/tokenpipe.h>
 #include <warnings.h>
-
-#include <config/bitcoin-config.h>
 
 #include <assert.h>
 #include <atomic>

BitgesellOfficial/bitgesell

src/shutdown.cpp
@@ -5,12 +5,14 @@
 
 #include <shutdown.h>
 
+#if defined(HAVE_CONFIG_H)
+#include <config/BGL-config.h>
+#endif
+
 #include <logging.h>
 #include <node/ui_interface.h>
 #include <util/tokenpipe.h>
 #include <warnings.h>
-
-#include <config/BGL-config.h>
 
 #include <assert.h>
 #include <atomic>