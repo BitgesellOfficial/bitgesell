d94342bd32b0e45fff8885d773987da32400a2da
Anthony Towns <aj@erisian.com.au>
2022-09-13 12:22:18 +1000

    net: drop cs_sendProcessing
    SendMessages() is now protected g_msgproc_mutex; so this additional
    per-node mutex is redundant.

--- src/net.cpp
+++ src/net.cpp
@@ -2016,10 +2016,7 @@ void CConnman::ThreadMessageHandler()
                 if (flagInterruptMsgProc)
                     return;
                 // Send messages
-                {
-                    LOCK(pnode->cs_sendProcessing);
-                    m_msgproc->SendMessages(pnode);
-                }
+                m_msgproc->SendMessages(pnode);
 
                 if (flagInterruptMsgProc)
                     return;
