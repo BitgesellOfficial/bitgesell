4b5a1e5cd76d2bbe92b03744db0fa8a3e57e31e3
fanquake <fanquake@gmail.com>
2022-09-15 10:45:07 +0100

    refactor: add kernel/cs_main.*
    Co-authored-by: Anthony Towns <aj@erisian.com.au>

--- src/validationinterface.h
+++ src/validationinterface.h
@@ -6,13 +6,13 @@
 #ifndef BGL_VALIDATIONINTERFACE_H
 #define BGL_VALIDATIONINTERFACE_H
 
+#include <kernel/cs_main.h>
 #include <primitives/transaction.h> // CTransaction(Ref)
 #include <sync.h>
 
 #include <functional>
 #include <memory>
 
-extern RecursiveMutex cs_main;
 class BlockValidationState;
 class CBlock;
 class CBlockIndex;
