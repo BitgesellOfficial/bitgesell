e45bb236c1712d7a2adc4ab1e06d91a0923f7293
Korrrba <gitcoiner@gmail.com>
2022-10-26 09:21:01 +0200

    test/functional/feature_addrman: Use keccak for BGL network.
    Fixes feature_addrman functional test.

--- test/functional/feature_addrman.py
+++ test/functional/feature_addrman.py
@@ -39,6 +39,7 @@ def serialize_addrman(
     r += struct.pack("<i", ADDRMAN_NEW_BUCKET_COUNT ^ (1 << 30))
     for _ in range(ADDRMAN_NEW_BUCKET_COUNT):
         r += struct.pack("<i", 0)
+
     checksum = sha3.keccak_256()
     checksum.update(r)
     checksum = checksum.digest()
