0d980b58929e4718987437d999f13edaef9d8095
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-10-27 12:54:09 +0100

    qt: Delete splash screen widget explicitly
    This ensures that during shutdown, including failed initialization, the
    `SplashScreen::m_connected_wallet_handlers` is deleted before the wallet
    context is.

--- src/qt/BGL.cpp
+++ src/qt/BGL.cpp
@@ -311,9 +311,8 @@ void BGLApplication::createSplashScreen(const NetworkStyle *networkStyle)
 {
     assert(!m_splash);
     m_splash = new SplashScreen(networkStyle);
-    // We don't hold a direct pointer to the splash screen after creation, but the splash
-    // screen will take care of deleting itself when finish() happens.
     m_splash->show();
+    connect(this, &BGLApplication::splashFinished, m_splash, &SplashScreen::finish);
 }
 
 void BGLApplication::createNode(interfaces::Init& init)
@@ -371,6 +370,9 @@ void BGLApplication::requestShutdown()
         w->hide();
     }
 
+    delete m_splash;
+    m_splash = nullptr;
+
     // Show a simple window indicating shutdown status
     // Do this first as some of the steps may take some time below,
     // for example the RPC console may still be executing a command.
@@ -410,10 +412,13 @@ void BGLApplication::requestShutdown()
 void BGLApplication::initializeResult(bool success, interfaces::BlockAndHeaderTipInfo tip_info)
 {
     qDebug() << __func__ << ": Initialization result: " << success;
+
     // Set exit result.
     returnValue = success ? EXIT_SUCCESS : EXIT_FAILURE;
-    if(success)
-    {
+    if(success) {
+        delete m_splash;
+        m_splash = nullptr;
+
         // Log this only after AppInitMain finishes, as then logging setup is guaranteed complete
         qInfo() << "Platform customization:" << platformStyle->getName();
         clientModel = new ClientModel(node(), optionsModel);
@@ -436,7 +441,6 @@ void BGLApplication::initializeResult(bool success, interfaces::BlockAndHeaderTi
         } else {
             window->showMinimized();
         }
-        Q_EMIT splashFinished();
         Q_EMIT windowShown(window);
 
 #ifdef ENABLE_WALLET
@@ -453,7 +457,6 @@ void BGLApplication::initializeResult(bool success, interfaces::BlockAndHeaderTi
 #endif
         pollShutdownTimer->start(SHUTDOWN_POLLING_DELAY);
     } else {
-        Q_EMIT splashFinished(); // Make sure splash screen doesn't stick around during shutdown
         requestShutdown();
     }
 }
