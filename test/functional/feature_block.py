f456e802ef0f68e7c936f3c416b8d73c77464205
Korrrba <gitcoiner@gmail.com>
2022-12-19 00:21:06 +0100

    test: functional: Fix feature_block test for BGL.
    BGL max block weight is ten times smaller than Bitcoin, and even that consensus defines the same
    maximum number of SIGOPS in the block, due to signatures and transactions overhead, measured
    empirically using bisection, real maximum number of numTxes in sigops test for BGL is ~9 times
    less than the Bitcoin. The exact maximum measured is 371 numTxes for the maximum value b39 block size 399784 bytes.
    
    Adjust overspend test to spend 201 BGL instead of 51 as defined for
    Bitcoin.

--- test/functional/feature_block.py
+++ test/functional/feature_block.py
@@ -517,7 +517,7 @@ class FullBlockTest(BGLTestFramework):
 
         # Test sigops in P2SH redeem scripts
         #
-        # b40 creates 3333 tx's spending the 6-sigop P2SH outputs from b39 for a total of 19998 sigops.
+        # b40 creates 1072 tx's spending the 6-sigop P2SH outputs from b39 for a total of 6432 sigops.
         # The first tx has one sigop and then at the end we add 2 more to put us just over the max.
         #
         # b41 does the same, less one, so it has the maximum sigops permitted.
