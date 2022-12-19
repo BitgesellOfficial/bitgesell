61e0240d8153dfccbd9b0c076be58ce5378b4604
VanOrton <>
2022-12-19 12:36:21 +0100

    locale cleanup and Linux Docker .deb Qt build fixes

--- debian.qt/build-in-docker.sh
+++ debian.qt/build-in-docker.sh
@@ -9,20 +9,22 @@ finish() {
     docker container rm $container >/dev/null
 }
 
-docker pull ubuntu:18.04
-container=`docker run -dit ubuntu:18.04`
+docker pull ubuntu:20.04
+container=`docker run -dit ubuntu:20.04`
 trap finish EXIT
 
-docker exec $container apt-get -y update
-docker exec $container apt-get -y install apt-utils devscripts sudo #dpkg-dev
-docker exec $container apt-get -y install libqrencode-dev qt5-default qttools5-dev-tools
+docker cp ./debian/updateunattended.sh $container:/root/
+docker exec $container /root/updateunattended.sh
+#docker exec $container apt-get -y update
+#docker exec $container apt-get -y install apt-utils devscripts sudo #dpkg-dev
+#docker exec $container apt-get -y install libqrencode-dev qt5-default qttools5-dev-tools
 docker exec $container mkdir -p /root/repo
 docker exec $container useradd user
 docker exec $container mkdir /home/user
 docker exec $container chown user.user /home/user
 docker exec $container sudo -u user mkdir /home/user/build
-docker cp ../bitgesell-qt_0.1.7.dsc $container:/root/repo/
-docker cp ../bitgesell-qt_0.1.7.tar.xz $container:/root/repo/
+docker cp ../bitgesell-qt_0.1.9.dsc $container:/root/repo/
+docker cp ../bitgesell-qt_0.1.9.tar.xz $container:/root/repo/
 # docker exec -w /root/repo $container sh -c "dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz"
 
 docker exec -w /root/repo $container sh -c "apt-ftparchive sources . > Sources"
@@ -32,5 +34,5 @@ docker exec $container apt-get -y build-dep bitgesell-qt
 docker exec $container chmod a+rX /root
 docker exec $container chmod -R a+rX /root/repo
 docker exec -w /home/user/build $container sudo -u user apt-get -y source bitgesell-qt
-docker exec -w /home/user/build/bitgesell-qt-0.1.7 $container sudo -u user debuild -b
-docker cp $container:/home/user/build/bitgesell-qt_0.1.7_amd64.deb bitgesell-qt_0.1.7_amd64.deb
+docker exec -w /home/user/build/bitgesell-qt-0.1.9 $container sudo -u user debuild -b
+docker cp $container:/home/user/build/bitgesell-qt_0.1.9_amd64.deb bitgesell-qt_0.1.9_amd64.deb
