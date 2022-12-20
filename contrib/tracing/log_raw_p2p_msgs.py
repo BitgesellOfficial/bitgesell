c20e6c8dd72ac8cc062ab9460b93a5bdc6c11076
0xb10c <b10c@b10c.me>
2022-12-20 12:51:13 +0100

    tracing: use bitcoind pid in bcc tracing examples
    BCC needs the PID of a bitcoind process to attach to the tracepoints
    (instead of the binary path used before) when the tracepoints have a
    semaphore.
    
    For reference, we already use the PID in our tracepoint interface
    tests. See 220a5a2841172a07d6d7849596316f0e0933e272.

--- contrib/tracing/log_raw_p2p_msgs.py
+++ contrib/tracing/log_raw_p2p_msgs.py
@@ -132,8 +132,9 @@ def print_message(event, inbound):
           )
 
 
-def main(BGLd_path):
-    BGLd_with_usdts = USDT(path=str(BGLd_path))
+def main(pid):
+    print(f"Hooking into BGLd with pid {pid}")
+    bitcoind_with_usdts = USDT(pid=int(pid))
 
     # attaching the trace functions defined in the BPF program to the tracepoints
     BGLd_with_usdts.enable_probe(
@@ -176,8 +177,8 @@ def main(BGLd_path):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("USAGE:", sys.argv[0], "path/to/BGLd")
+    if len(sys.argv) != 2:
+        print("USAGE:", sys.argv[0], "<pid of BGLd>")
         exit()
-    path = sys.argv[1]
-    main(path)
+    pid = sys.argv[1]
+    main(pid)
--- contrib/tracing/log_utxocache_flush.py
+++ contrib/tracing/log_utxocache_flush.py
@@ -70,8 +70,9 @@ def print_event(event):
     ))
 
 
-def main(BGLd_path):
-    BGLd_with_usdts = USDT(path=str(BGLd_path))
+def main(pid):
+    print(f"Hooking into BGLd with pid {pid}")
+    bitcoind_with_usdts = USDT(pid=int(pid))
 
     # attaching the trace functions defined in the BPF program
     # to the tracepoints
@@ -99,9 +100,9 @@ def main(BGLd_path):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("USAGE: ", sys.argv[0], "path/to/BGLd")
+    if len(sys.argv) != 2:
+        print("USAGE: ", sys.argv[0], "<pid of BGLd>")
         exit(1)
 
-    path = sys.argv[1]
-    main(path)
+    pid = sys.argv[1]
+    main(pid)
