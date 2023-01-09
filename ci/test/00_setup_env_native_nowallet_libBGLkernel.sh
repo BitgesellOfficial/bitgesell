98cca45482be55dae47fa9f26fe076d84ae8330f
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2023-01-09 10:22:18 +0100

    scripted-diff: ci: Rework docker naming
    DOCKER in names is confusingly used as synonym for "image", "container",
    and "ci". Fix the confusion by picking the term that fits the context.
    
    -BEGIN VERIFY SCRIPT-
     ren() { sed -i "s:$1:$2:g" $( git grep -l "$1" ) ; }
    
     ren DOCKER_PACKAGES CI_BASE_PACKAGES
     # This better reflects that they are the common base for all CI
     # containers.
    
     ren DOCKER_ID CI_CONTAINER_ID
     # This is according to the documentation of "--detach , -d: Run
     # container in background and print container ID".
    
     ren DOCKER_NAME_TAG CI_IMAGE_NAME_TAG
     # This avoids confusing with CONTAINER_NAME and clarifies that it is an
     # image.
    
     ren DOCKER_ADMIN CI_CONTAINER_CAP
     # This clarifies that it is a capability added to the container.
    
     ren DOCKER_CI_CMD_PREFIX CI_EXEC_CMD_PREFIX
     # This brings it in line with the CI_EXEC naming.
    
    -END VERIFY SCRIPT-

--- ci/test/00_setup_env_native_nowallet_libBGLkernel.sh
+++ ci/test/00_setup_env_native_nowallet_libBGLkernel.sh
@@ -12,8 +12,8 @@ export LC_ALL=C.UTF-8
 #export CCACHE_SIZE #default
 #export CCACHE_DIR #default
 
-export CONTAINER_NAME=ci_native_nowallet_libBGLkernel
-export DOCKER_NAME_TAG=ubuntu:18.04  # Use bionic to have one config run the tests in python3.6, see doc/dependencies.md
+export CONTAINER_NAME=ci_native_nowallet_libbitcoinkernel
+export CI_IMAGE_NAME_TAG=ubuntu:18.04  # Use bionic to have one config run the tests in python3.6, see doc/dependencies.md
 export PACKAGES="python3-zmq clang-8 llvm-8 libc++abi-8-dev libc++-8-dev"  # Use clang-8 to test C++17 compatibility, see doc/dependencies.md
 export DEP_OPTS="NO_WALLET=1 CC=clang-8 CXX='clang++-8 -stdlib=libc++'"
 export GOAL="install"
