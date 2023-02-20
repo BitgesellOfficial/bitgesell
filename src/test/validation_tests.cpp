0cdeae3cf65ef0f12d04004c5511755e66d5a140
ijanus <emekamicro@gmail.com>
2023-02-20 09:41:44 +0100

    Fix validation test

--- src/test/validation_tests.cpp
+++ src/test/validation_tests.cpp
@@ -18,7 +18,7 @@ BOOST_FIXTURE_TEST_SUITE(validation_tests, TestingSetup)
 static void TestBlockSubsidyHalvings(const Consensus::Params& consensusParams)
 {
     int maxHalvings = 64;
-    CAmount nInitialSubsidy = 4 * 50 * COIN; // Added based on the fact that at 0, 1, 2 we would get this range.
+    CAmount nInitialSubsidy = 50 * COIN;
 
     CAmount nPreviousSubsidy = nInitialSubsidy * 2; // for height == 0
     BOOST_CHECK_EQUAL(nPreviousSubsidy, nInitialSubsidy * 2);
@@ -53,8 +53,7 @@ BOOST_AUTO_TEST_CASE(subsidy_limit_test)
     CAmount nSum = 0;
     for (int nHeight = 0; nHeight < 14000000; nHeight += 1000) {
         CAmount nSubsidy = GetBlockSubsidy(nHeight, chainParams->GetConsensus());
-        BOOST_CHECK(nSubsidy <= 4 * 50 * COIN); // Added based on the fact that at 0, 1, 2 we would get this range.
-        nSubsidy = nSubsidy / 4; // Added because we divided halving block by 4
+        BOOST_CHECK(nSubsidy <= 50 * COIN);
         nSum += nSubsidy * 1000;
         BOOST_CHECK(MoneyRange(nSum));
     }
