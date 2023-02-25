2d8e7d4668b605b8dbe36fc41a27e3c6d2aff3ab
Pieter Wuille <pieter@wuille.net>
2023-02-25 15:35:43 -0500

    Enforce type consistency in miniscript_stable fuzz test
    Add a self-check to the fuzzer that the constructed types match the expected
    types in the miniscript_stable fuzzer too.

--- src/test/fuzz/miniscript.cpp
+++ src/test/fuzz/miniscript.cpp
@@ -753,9 +753,12 @@ NodeRef GenNode(F ConsumeNode, Type root_type = ""_mst, bool strict_valid = fals
                 node = MakeNodeRef(info.fragment, std::move(info.keys), info.k);
             }
             // Verify acceptability.
-            if (!node || !(node->GetType() << type_needed)) {
+            if (!node || (node->GetType() & "KVWB"_mst) == ""_mst) {
                 assert(!strict_valid);
                 return {};
+            }
+            if (!(type_needed == ""_mst)) {
+                assert(node->GetType() << type_needed);
             }
             if (!node->IsValid()) return {};
             // Move it to the stack.
