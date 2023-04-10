5b0690aec13bbd19fd31a3835767313b6ea12373
fanquake <fanquake@gmail.com>
2023-04-10 21:00:51 +0100

    ci: use Debian Bookworm and Valgrind 3.19 in Valgrind jobs
    https://packages.debian.org/bookworm/valgrind

--- ci/test/00_setup_env_native_fuzz_with_valgrind.sh
+++ ci/test/00_setup_env_native_fuzz_with_valgrind.sh
@@ -6,7 +6,7 @@
 
 export LC_ALL=C.UTF-8
 
-export CI_IMAGE_NAME_TAG="ubuntu:22.04"
+export CI_IMAGE_NAME_TAG="debian:bookworm"
 export CONTAINER_NAME=ci_native_fuzz_valgrind
 export PACKAGES="clang llvm python3 libevent-dev bsdmainutils libboost-dev libsqlite3-dev valgrind"
 export NO_DEPENDS=1
@@ -15,6 +15,11 @@ export RUN_FUNCTIONAL_TESTS=false
 export RUN_FUZZ_TESTS=true
 export FUZZ_TESTS_CONFIG="--valgrind"
 export GOAL="install"
+<<<<<<< HEAD
 # Temporarily pin dwarf 4, until valgrind can understand clang's dwarf 5
 export BGL_CONFIG="--enable-fuzz --with-sanitizers=fuzzer CC=clang CXX=clang++ CFLAGS='-gdwarf-4' CXXFLAGS='-gdwarf-4'"
+=======
+# Temporarily pin dwarf 4, until using Valgrind 3.20 or later
+export BITCOIN_CONFIG="--enable-fuzz --with-sanitizers=fuzzer CC=clang CXX=clang++ CFLAGS='-gdwarf-4' CXXFLAGS='-gdwarf-4'"
+>>>>>>> ba29143d98... ci: use Debian Bookworm and Valgrind 3.19 in Valgrind jobs
 export CCACHE_SIZE=200M
