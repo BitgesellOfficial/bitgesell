8d524427d519e32150695ffe86b8a5d73e6e7d0f
TheCharlatan <seb.kung@gmail.com>
2023-05-10 22:29:17 +0200

    kernel: Add notification interface
    This commit is part of the libbitcoinkernel project and seeks to remove
    the ChainstateManager's and, more generally, the kernel library's
    dependency on interface_ui with options methods in this and the following
    few commits. By removing interface_ui from the kernel library, its
    dependency on boost is reduced to just boost::multi_index.
    
    Define a new kernel notification class with virtual methods for
    notifying about internal kernel events. Create a new file in the node
    library for defining a function creating the default set of notification
    methods such that these do not need to be re-defined all over the
    codebase. As a first step, add a `blockTip` method, wrapping
    `uiInterface.NotifyBlockTip`.

--- src/validation.cpp
+++ src/validation.cpp
@@ -3193,7 +3193,7 @@ bool Chainstate::ActivateBestChain(BlockValidationState& state, std::shared_ptr<
                 GetMainSignals().UpdatedBlockTip(pindexNewTip, pindexFork, fInitialDownload);
 
                 // Always notify the UI if a new block tip was connected
-                uiInterface.NotifyBlockTip(GetSynchronizationState(fInitialDownload), pindexNewTip);
+                m_chainman.GetNotifications().blockTip(GetSynchronizationState(fInitialDownload), *pindexNewTip);
             }
         }
         // When we reach this point, we switched to a new tip (stored in pindexNewTip).
@@ -3390,7 +3390,7 @@ bool Chainstate::InvalidateBlock(BlockValidationState& state, CBlockIndex* pinde
 
     // Only notify about a new block tip if the active chain was modified.
     if (pindex_was_in_chain) {
-        uiInterface.NotifyBlockTip(GetSynchronizationState(IsInitialBlockDownload()), to_mark_failed->pprev);
+        m_chainman.GetNotifications().blockTip(GetSynchronizationState(IsInitialBlockDownload()), *to_mark_failed->pprev);
     }
     return true;
 }
