c3320d8d7192b68aca8de722589eedd1105c20b7
Antoine Poinsot <darosior@protonmail.com>
2023-05-16 19:55:10 +0200

    fuzz: make the parsed descriptor testing into a function
    We'll be reusing it in the new target.

--- src/test/fuzz/descriptor_parse.cpp
+++ src/test/fuzz/descriptor_parse.cpp
@@ -8,6 +8,14 @@
 #include <test/fuzz/fuzz.h>
 #include <util/chaintype.h>
 
+/** Test a successfully parsed descriptor. */
+static void TestDescriptor(const Descriptor& desc)
+{
+    (void)desc.ToString();
+    (void)desc.IsRange();
+    (void)desc.IsSolvable();
+}
+
 void initialize_descriptor_parse()
 {
     static const ECCVerifyHandle verify_handle;
@@ -22,10 +30,6 @@ FUZZ_TARGET(descriptor_parse, .init = initialize_descriptor_parse)
     std::string error;
     for (const bool require_checksum : {true, false}) {
         const auto desc = Parse(descriptor, signing_provider, error, require_checksum);
-        if (desc) {
-            (void)desc->ToString();
-            (void)desc->IsRange();
-            (void)desc->IsSolvable();
-        }
+        if (desc) TestDescriptor(*desc);
     }
 }
