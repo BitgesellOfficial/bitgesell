ff42d0b9bb19aa642ea358ea729c07c209b5f99d
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2023-06-22 17:02:28 +0200

    Use only Span{} constructor for byte-like types where possible
    This removes bloat that is not needed.

--- src/uint256.h
+++ src/uint256.h
@@ -94,7 +94,7 @@ public:
     template<typename Stream>
     void Serialize(Stream& s) const
     {
-        s.write(MakeByteSpan(m_data));
+        s << Span(m_data);
     }
 
     template<typename Stream>
