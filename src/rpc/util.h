864fbd7c172967506cf8b3ad93907db2db227210
Jon Atack <jon@atack.com>
2023-07-23 20:45:10 -0600

    refactor: drop protocol.h include header in rpc/util.h
    as it was only needed for GetServicesNames(). This potentially avoids needlessly
    compiling the 500 lines of protocol.h in the 35 files other than rpc/net.cpp
    that include rpc/util.h.
    
    Drop an unneeded CPubKey forward declaration. The other IWYU suggestions would
    require more extensive changes in other files.
    
    Add 3 already-missing include headers in other translation units that are needed
    to compile without protocol.h in rpc/util.h, as it includes netaddress.h, which
    in turn includes util/strencodings.h.

--- src/rpc/util.h
+++ src/rpc/util.h
@@ -9,7 +9,6 @@
 #include <consensus/amount.h>
 #include <node/transaction.h>
 #include <outputtype.h>
-#include <protocol.h>
 #include <pubkey.h>
 #include <rpc/protocol.h>
 #include <rpc/request.h>
@@ -60,7 +59,6 @@ extern const std::string UNIX_EPOCH_TIME;
 extern const std::string EXAMPLE_ADDRESS[2];
 
 class FillableSigningProvider;
-class CPubKey;
 class CScript;
 struct Sections;
 
