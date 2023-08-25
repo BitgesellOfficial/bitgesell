bitcoin-ghost/bitcoin-core-ladder-script

build: Re-enable external signer on Windows

ci/test/00_setup_env_win64.sh
@@ -13,4 +13,4 @@ export DPKG_ADD_ARCH="i386"
 export PACKAGES="python3 nsis g++-mingw-w64-x86-64-posix wine-binfmt wine64 wine32 file"
 export RUN_FUNCTIONAL_TESTS=false
 export GOAL="deploy"
-export BITCOIN_CONFIG="--enable-reduce-exports --disable-external-signer --disable-gui-tests"
+export BITCOIN_CONFIG="--enable-reduce-exports --enable-external-signer --disable-gui-tests"

BitgesellOfficial/bitgesell

ci/test/00_setup_env_win64.sh
@@ -20,4 +20,4 @@ export DPKG_ADD_ARCH="i386"
 export PACKAGES="python3 nsis g++-mingw-w64-x86-64-posix wine-binfmt wine64 wine32 file"
 export RUN_FUNCTIONAL_TESTS=false
 export GOAL="deploy"
-export BGL_CONFIG="--enable-reduce-exports --disable-external-signer --disable-gui-tests"
+export BGL_CONFIG="--enable-reduce-exports --enable-external-signer --disable-gui-tests"