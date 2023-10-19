d0d0926c90d217dac431952259e98ea0370cb739
brunoerg <brunoely.gc@gmail.com>
2023-10-19 09:37:17 -0300

    addrman: log AS only when using asmap

--- test/functional/p2p_invalid_messages.py
+++ test/functional/p2p_invalid_messages.py
@@ -219,7 +219,7 @@ class InvalidMessagesTest(BGLTestFramework):
         self.test_addrv2('unrecognized network',
             [
                 'received: addrv2 (25 bytes)',
-                '9.9.9.9:8333 mapped',
+                '9.9.9.9:8333',
                 'Added 1 addresses',
             ],
             bytes.fromhex(
