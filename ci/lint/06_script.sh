96ef8cd5c62461293b587933d7fe58940a5b05dc
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2023-11-13 16:54:20 +0100

    lint: Report all lint errors instead of early exit

--- ci/lint/06_script.sh
+++ ci/lint/06_script.sh
@@ -22,14 +22,7 @@ else
   COMMIT_RANGE="SKIP_EMPTY_NOT_A_PR"
 fi
 
-test/lint/git-subtree-check.sh src/crypto/ctaes
-test/lint/git-subtree-check.sh src/secp256k1
-test/lint/git-subtree-check.sh src/minisketch
-test/lint/git-subtree-check.sh src/leveldb
-test/lint/git-subtree-check.sh src/crc32c
 RUST_BACKTRACE=1 "${LINT_RUNNER_PATH}/test_runner"
-test/lint/check-doc.py
-test/lint/all-lint.py
 
 if [ "$CIRRUS_REPO_FULL_NAME" = "BGL/BGL" ] && [ "$CIRRUS_PR" = "" ] ; then
     # Sanity check only the last few commits to get notified of missing sigs,
