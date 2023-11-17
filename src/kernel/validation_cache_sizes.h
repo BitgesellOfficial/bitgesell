bd85d8e20f3b7290a27c0c85ae851a0dd571633e
ijanus <emekamicro@gmail.com>
2023-11-17 11:08:50 +0100

    Change BITCOIN to BGL

--- src/kernel/validation_cache_sizes.h
+++ src/kernel/validation_cache_sizes.h
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_KERNEL_VALIDATION_CACHE_SIZES_H
-#define BITCOIN_KERNEL_VALIDATION_CACHE_SIZES_H
+#ifndef BGL_KERNEL_VALIDATION_CACHE_SIZES_H
+#define BGL_KERNEL_VALIDATION_CACHE_SIZES_H
 
 #include <script/sigcache.h>
 
@@ -17,4 +17,4 @@ struct ValidationCacheSizes {
 };
 }
 
-#endif // BITCOIN_KERNEL_VALIDATION_CACHE_SIZES_H
+#endif // BGL_KERNEL_VALIDATION_CACHE_SIZES_H
