dd78c39d3037bd678abcd2ed9a5b84c619c30fcf
Ava Chow <github@achow101.com>
2024-01-05 18:42:23 -0500

    wallet: Delete LegacySPKM
    Deletes LegacyScriptPubKeyMan and related tests
    
    Best reviewed with `git diff --patience` or `git diff --histogram`

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -195,28 +195,10 @@ OverviewPage::~OverviewPage()
 void OverviewPage::setBalance(const interfaces::WalletBalances& balances)
 {
     BGLUnit unit = walletModel->getOptionsModel()->getDisplayUnit();
-    if (walletModel->wallet().isLegacy()) {
-        if (walletModel->wallet().privateKeysDisabled()) {
-            ui->labelBalance->setText(BGLUnits::formatWithPrivacy(unit, balances.watch_only_balance, BGLUnits::SeparatorStyle::ALWAYS, m_privacy));
-            ui->labelUnconfirmed->setText(BGLUnits::formatWithPrivacy(unit, balances.unconfirmed_watch_only_balance, BGLUnits::SeparatorStyle::ALWAYS, m_privacy));
-            ui->labelImmature->setText(BGLUnits::formatWithPrivacy(unit, balances.immature_watch_only_balance, BGLUnits::SeparatorStyle::ALWAYS, m_privacy));
-            ui->labelTotal->setText(BGLUnits::formatWithPrivacy(unit, balances.watch_only_balance + balances.unconfirmed_watch_only_balance + balances.immature_watch_only_balance, BGLUnits::SeparatorStyle::ALWAYS, m_privacy));
-        } else {
-            ui->labelBalance->setText(BGLUnits::formatWithPrivacy(unit, balances.balance, BGLUnits::SeparatorStyle::ALWAYS, m_privacy));
-            ui->labelUnconfirmed->setText(BGLUnits::formatWithPrivacy(unit, balances.unconfirmed_balance, BGLUnits::SeparatorStyle::ALWAYS, m_privacy));
-            ui->labelImmature->setText(BGLUnits::formatWithPrivacy(unit, balances.immature_balance, BGLUnits::SeparatorStyle::ALWAYS, m_privacy));
-            ui->labelTotal->setText(BGLUnits::formatWithPrivacy(unit, balances.balance + balances.unconfirmed_balance + balances.immature_balance, BGLUnits::SeparatorStyle::ALWAYS, m_privacy));
-            ui->labelWatchAvailable->setText(BGLUnits::formatWithPrivacy(unit, balances.watch_only_balance, BGLUnits::SeparatorStyle::ALWAYS, m_privacy));
-            ui->labelWatchPending->setText(BGLUnits::formatWithPrivacy(unit, balances.unconfirmed_watch_only_balance, BGLUnits::SeparatorStyle::ALWAYS, m_privacy));
-            ui->labelWatchImmature->setText(BGLUnits::formatWithPrivacy(unit, balances.immature_watch_only_balance, BGLUnits::SeparatorStyle::ALWAYS, m_privacy));
-            ui->labelWatchTotal->setText(BGLUnits::formatWithPrivacy(unit, balances.watch_only_balance + balances.unconfirmed_watch_only_balance + balances.immature_watch_only_balance, BGLUnits::SeparatorStyle::ALWAYS, m_privacy));
-        }
-    } else {
-        ui->labelBalance->setText(BGLUnits::formatWithPrivacy(unit, balances.balance, BGLUnits::SeparatorStyle::ALWAYS, m_privacy));
-        ui->labelUnconfirmed->setText(BGLUnits::formatWithPrivacy(unit, balances.unconfirmed_balance, BGLUnits::SeparatorStyle::ALWAYS, m_privacy));
-        ui->labelImmature->setText(BGLUnits::formatWithPrivacy(unit, balances.immature_balance, BGLUnits::SeparatorStyle::ALWAYS, m_privacy));
-        ui->labelTotal->setText(BGLUnits::formatWithPrivacy(unit, balances.balance + balances.unconfirmed_balance + balances.immature_balance, BGLUnits::SeparatorStyle::ALWAYS, m_privacy));
-    }
+    ui->labelBalance->setText(BGLUnits::formatWithPrivacy(unit, balances.balance, BGLUnits::SeparatorStyle::ALWAYS, m_privacy));
+    ui->labelUnconfirmed->setText(BGLUnits::formatWithPrivacy(unit, balances.unconfirmed_balance, BGLUnits::SeparatorStyle::ALWAYS, m_privacy));
+    ui->labelImmature->setText(BGLUnits::formatWithPrivacy(unit, balances.immature_balance, BGLUnits::SeparatorStyle::ALWAYS, m_privacy));
+    ui->labelTotal->setText(BGLUnits::formatWithPrivacy(unit, balances.balance + balances.unconfirmed_balance + balances.immature_balance, BGLUnits::SeparatorStyle::ALWAYS, m_privacy));
     // only show immature (newly mined) balance if it's non-zero, so as not to complicate things
     // for the non-mining users
     bool showImmature = balances.immature_balance != 0;
@@ -281,11 +263,7 @@ void OverviewPage::setWalletModel(WalletModel *model)
 
         connect(model->getOptionsModel(), &OptionsModel::displayUnitChanged, this, &OverviewPage::updateDisplayUnit);
 
-        interfaces::Wallet& wallet = model->wallet();
-        updateWatchOnlyLabels(wallet.haveWatchOnly() && !wallet.privateKeysDisabled());
-        connect(model, &WalletModel::notifyWatchonlyChanged, [this](bool showWatchOnly) {
-            updateWatchOnlyLabels(showWatchOnly && !walletModel->wallet().privateKeysDisabled());
-        });
+        updateWatchOnlyLabels(false);
     }
 
     // update the display unit, to not use the default ("BTC")
--- src/wallet/walletdb.h
+++ src/wallet/walletdb.h
@@ -21,7 +21,6 @@ class uint256;
 struct CBlockLocator;
 
 namespace wallet {
-class CKeyPool;
 class CMasterKey;
 class CWallet;
 class CWalletTx;
@@ -238,8 +237,6 @@ public:
     bool WriteCryptedKey(const CPubKey& vchPubKey, const std::vector<unsigned char>& vchCryptedSecret, const CKeyMetadata &keyMeta);
     bool WriteMasterKey(unsigned int nID, const CMasterKey& kMasterKey);
 
-    bool WriteCScript(const uint160& hash, const CScript& redeemScript);
-
     bool WriteWatchOnly(const CScript &script, const CKeyMetadata &keymeta);
     bool EraseWatchOnly(const CScript &script);
 
@@ -250,10 +247,6 @@ public:
     bool IsEncrypted();
 
     bool WriteOrderPosNext(int64_t nOrderPosNext);
-
-    bool ReadPool(int64_t nPool, CKeyPool& keypool);
-    bool WritePool(int64_t nPool, const CKeyPool& keypool);
-    bool ErasePool(int64_t nPool);
 
     bool WriteMinVersion(int nVersion);
 
@@ -277,9 +270,6 @@ public:
     bool EraseActiveScriptPubKeyMan(uint8_t type, bool internal);
 
     DBErrors LoadWallet(CWallet* pwallet);
-
-    //! write the hdchain model (external chain child index counter)
-    bool WriteHDChain(const CHDChain& chain);
 
     //! Delete records of the given types
     bool EraseRecords(const std::unordered_set<std::string>& types);
