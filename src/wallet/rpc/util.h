30862a8353414b82d8379402a49a6bd93cadc5a1
ijanus <emekamicro@gmail.com>
2024-02-04 07:19:37 +0100

    Replace BITCOIN with BGL

--- src/wallet/rpc/util.h
+++ src/wallet/rpc/util.h
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_WALLET_RPC_UTIL_H
-#define BITCOIN_WALLET_RPC_UTIL_H
+#ifndef BGL_WALLET_RPC_UTIL_H
+#define BGL_WALLET_RPC_UTIL_H
 
 #include <rpc/util.h>
 #include <script/script.h>
@@ -55,4 +55,4 @@ int64_t ParseISO8601DateTime(const std::string& str);
 void AppendLastProcessedBlock(UniValue& entry, const CWallet& wallet) EXCLUSIVE_LOCKS_REQUIRED(wallet.cs_wallet);
 } //  namespace wallet
 
-#endif // BITCOIN_WALLET_RPC_UTIL_H
+#endif // BGL_WALLET_RPC_UTIL_H
