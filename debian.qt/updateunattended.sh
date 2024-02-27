db1226774e0ae86e1687623eb8464688a3c9caac
ijanus <emekamicro@gmail.com>
2024-02-27 06:41:12 +0100

    Remove redundant folders

--- debian.qt/updateunattended.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export DEBIAN_FRONTEND=noninteractive
-export TZ=Etc/UTC
-apt-get update && apt-get install -y tzdata
-ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
-dpkg-reconfigure --frontend noninteractive tzdata
-#apt-get -y update
-apt-get -y install apt-utils devscripts sudo #dpkg-dev
-apt-get -y install libqrencode-dev qt5-default qttools5-dev-tools
