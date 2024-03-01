f81e1f34450de8e5e9e1f1cc38e0fb34cc089ee4
brunoerg <brunoely.gc@gmail.com>
2024-03-01 18:38:48 -0300

    fuzz: use `ConsumeNetGroupManager` in connman target

--- src/test/fuzz/connman.cpp
+++ src/test/fuzz/connman.cpp
@@ -32,10 +32,22 @@ FUZZ_TARGET(connman, .init = initialize_connman)
 {
     FuzzedDataProvider fuzzed_data_provider{buffer.data(), buffer.size()};
     SetMockTime(ConsumeTime(fuzzed_data_provider));
+    auto netgroupman{ConsumeNetGroupManager(fuzzed_data_provider)};
+    auto addr_man_ptr{std::make_unique<AddrManDeterministic>(netgroupman, fuzzed_data_provider, GetCheckRatio())};
+    if (fuzzed_data_provider.ConsumeBool()) {
+        const std::vector<uint8_t> serialized_data{ConsumeRandomLengthByteVector(fuzzed_data_provider)};
+        DataStream ds{serialized_data};
+        try {
+            ds >> *addr_man_ptr;
+        } catch (const std::ios_base::failure&) {
+            addr_man_ptr = std::make_unique<AddrManDeterministic>(netgroupman, fuzzed_data_provider, GetCheckRatio());
+        }
+    }
+    AddrManDeterministic& addr_man{*addr_man_ptr};
     ConnmanTestMsg connman{fuzzed_data_provider.ConsumeIntegral<uint64_t>(),
                      fuzzed_data_provider.ConsumeIntegral<uint64_t>(),
-                     *g_setup->m_node.addrman,
-                     *g_setup->m_node.netgroupman,
+                     addr_man,
+                     netgroupman,
                      Params(),
                      fuzzed_data_provider.ConsumeBool()};
 
