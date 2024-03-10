1ee006935e734fab9d415204e7c0fd50692e3c9e
Pieter Wuille <pieter@wuille.net>
2024-03-10 12:38:14 -0400

    random: add RandomMixin::randbits with compile-known bits
    In many cases, it is known at compile time how many bits are requested from
    randbits. Provide a variant of randbits that accepts this number as a template,
    to make sure the compiler can make use of this knowledge. This is used immediately
    in rand32() and randbool(), and a few further call sites.

--- src/random.cpp
+++ src/random.cpp
@@ -782,5 +782,6 @@ void RandomInit()
 
 double MakeExponentiallyDistributed(uint64_t uniform) noexcept
 {
-    return -std::log1p((uniform >> 16) * -0.0000000000000035527136788 /* -1/2^48 */);
+    double unscaled = -std::log1p(FastRandomContext().randbits<48>() * -0.0000000000000035527136788 /* -1/2^48 */);
+    return now + std::chrono::duration_cast<std::chrono::microseconds>(unscaled * average_interval + 0.5us);
 }
