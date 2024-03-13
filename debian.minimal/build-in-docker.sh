f87e389c55a9de49d020e3685795c60c4bdbe73a
wu-emma <wuemma@protonmail.com>
2024-03-13 18:28:22 +0400

    add debian package build tools

---
+++ debian.minimal/build-in-docker.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+debuild -S
+
+finish() {
+    docker stop $container >/dev/null
+    docker container rm $container >/dev/null
+}
+
+docker pull ubuntu:20.04
+container=`docker run -dit -e TZ='Etc/UTC' -e DEBIAN_FRONTEND='noninteractive' ubuntu:20.04 `
+trap finish EXIT
+
+docker exec $container apt-get -y update
+docker exec $container apt-get -y install apt-utils devscripts sudo #dpkg-dev
+docker exec $container mkdir -p /root/repo
+docker exec $container useradd user
+docker exec $container mkdir /home/user
+docker exec $container chown user.user /home/user
+docker exec $container sudo -u user mkdir /home/user/build
+docker cp ../bitgesell_0.1.11.dsc $container:/root/repo/
+docker cp ../bitgesell_0.1.11.tar.xz $container:/root/repo/
+# docker exec -w /root/repo $container sh -c "dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz"
+docker exec -w /root/repo $container sh -c "apt-ftparchive sources . > Sources"
+docker exec $container sh -c "echo deb-src [trusted=yes] file:/root/repo ./ >> /etc/apt/sources.list"
+docker exec $container apt-get -y -o APT::Sandbox::User=root update
+docker exec $container apt-get -y build-dep bitgesell
+docker exec $container chmod a+rX /root
+docker exec $container chmod -R a+rX /root/repo
+docker exec -w /home/user/build $container sudo -u user apt-get -y source bitgesell
+docker exec -w /home/user/build/bitgesell-0.1.11 $container sudo -u user debuild -b
+docker cp $container:/home/user/build/bitgesell_0.1.11_amd64.deb bitgesell_0.1.11_amd64.deb
---
+++ debian.qt/test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+finish() {
+    docker stop $container >/dev/null
+    docker container rm $container >/dev/null
+}
+
+docker pull ubuntu:20.04
+container=`docker run -dit -e TZ='Etc/UTC' -e DEBIAN_FRONTEND='noninteractive' ubuntu:20.04`
+trap finish EXIT
+
+docker exec $container apt-get -y update
+docker exec $container apt-get -y install apt-utils #dpkg-dev
+docker exec $container mkdir /root/repo
+docker cp ./bitgesell-qt_0.1.11_amd64.deb $container:/root/repo
+# docker exec -w /root/repo $container sh -c "dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz"
+docker exec -w /root/repo $container sh -c "apt-ftparchive packages . > Packages"
+docker exec $container sh -c "echo deb [trusted=yes] file:/root/repo ./ >> /etc/apt/sources.list"
+# docker exec $container rm -rf /var/lib/apt/lists/ # Why is this needed?
+# docker exec $container apt-get -y clean
+docker exec $container apt-get -y -o APT::Sandbox::User=root update
+docker exec $container apt-get -y install bitgesell-qt
+docker exec $container sh -c \
+    "if { BGL-cli --help && BGLd --help && BGL-tx --help; } > /dev/null; then \
+        echo 'Test passed.'; \
+    else
+        echo 'Test failed.'; \
+    fi"
