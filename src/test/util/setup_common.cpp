075f86bad0b82675924a61c8749f62901424bc1e
Pieter Wuille <pieter@wuille.net>
2024-03-15 14:32:20 -0400

    tests: make fuzz tests (mostly) deterministic with fixed seed

--- src/test/util/setup_common.cpp
+++ src/test/util/setup_common.cpp
@@ -182,10 +182,6 @@ BasicTestingSetup::BasicTestingSetup(const ChainType chainType, const std::vecto
     gArgs.ForceSetArg("-datadir", fs::PathToString(m_path_root));
 
     SelectParams(chainType);
-<<<<<<< HEAD
-=======
-    SeedRandomForTest();
->>>>>>> 810cdf6b4e... tests: overhaul deterministic test randomness
     if (G_TEST_LOG_FUN) LogInstance().PushBackCallback(G_TEST_LOG_FUN);
     InitLogging(*m_node.args);
     AppInitParameterInteraction(*m_node.args);
