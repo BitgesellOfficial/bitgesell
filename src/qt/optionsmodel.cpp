84a98ce4e4f4c138bdedea504cc3ae7e5b8a7ca8
dergoegge <n.goeggi@gmail.com>
2024-03-21 11:35:11 +0000

    [clang-tidy] Enable the misc-no-recursion check
    Co-authored-by: stickies-v <stickies-v@protonmail.com>
    Co-authored-by: Gloria Zhao <gloriajzhao@gmail.com>

--- src/qt/optionsmodel.cpp
+++ src/qt/optionsmodel.cpp
@@ -360,6 +360,7 @@ bool OptionsModel::setData(const QModelIndex & index, const QVariant & value, in
     return successful;
 }
 
+// NOLINTNEXTLINE(misc-no-recursion)
 QVariant OptionsModel::getOption(OptionID option, const std::string& suffix) const
 {
     auto setting = [&]{ return node().getPersistentSetting(SettingName(option) + suffix); };
@@ -455,6 +456,24 @@ QVariant OptionsModel::getOption(OptionID option, const std::string& suffix) con
     }
 }
 
+QFont OptionsModel::getFontForChoice(const FontChoice& fc)
+{
+    QFont f;
+    if (std::holds_alternative<FontChoiceAbstract>(fc)) {
+        f = GUIUtil::fixedPitchFont(fc != UseBestSystemFont);
+        f.setWeight(QFont::Bold);
+    } else {
+        f = std::get<QFont>(fc);
+    }
+    return f;
+}
+
+QFont OptionsModel::getFontForMoney() const
+{
+    return getFontForChoice(m_font_money);
+}
+
+// NOLINTNEXTLINE(misc-no-recursion)
 bool OptionsModel::setOption(OptionID option, const QVariant& value, const std::string& suffix)
 {
     auto changed = [&] { return value.isValid() && value != getOption(option, suffix); };
