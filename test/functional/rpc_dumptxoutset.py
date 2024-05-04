a7daad08b52a7dbf290553df294a601ba29086dd
ijanus <emekamicro@gmail.com>
2024-05-04 21:35:38 +0100

    Fix functional/rpc_dumptxoutset.py file

--- test/functional/rpc_dumptxoutset.py
+++ test/functional/rpc_dumptxoutset.py
@@ -46,7 +46,7 @@ class DumptxoutsetTest(BGLTestFramework):
             'bb5a2be762988a5d08c787c82e1e782efb6cf1a0544e95dbcb1cab338e510c8b')
 
         assert_equal(
-            out['txoutset_hash'], '1f1493bf052e2c0fdd637c2fa3067bc2b36df0a6a9deffda60cf5d9698f990e6')
+            out['txoutset_hash'], 'accd33eb0199de6727cb6e5f5c3d14e9a543163818ed75fcc3b65626c13c4df7')
         assert_equal(out['nchaintx'], 101)
 
         # Specifying a path to an existing or invalid file will fail.
