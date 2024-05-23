3a0d137deb9826ff4d4b0560698c23ab89e7920b
Jadi <jadijadi@gmail.com>
2024-05-23 17:41:45 +0330

    net: Providing an interface for mapLocalHost
    Contributes to #564 by providing an interface for mapLocalHost
    through net -> node interface -> clientModel. Later this value can be
    read by GUI to show the local addresses.

--- src/net.cpp
+++ src/net.cpp
@@ -3570,6 +3570,13 @@ size_t CConnman::GetNodeCount(ConnectionDirection flags) const
     return nNum;
 }
 
+
+std::map<CNetAddr, LocalServiceInfo> CConnman::getNetLocalAddresses() const
+{
+    LOCK(g_maplocalhost_mutex);
+    return mapLocalHost;
+}
+
 uint32_t CConnman::GetMappedAS(const CNetAddr& addr) const
 {
     return m_netgroupman.GetMappedAS(addr);
