73e0a29677bf53364af98b4b77fba6fa75faeac8
Andrew Toth <andrewstoth@gmail.com>
2024-06-28 17:14:33 -0400

    refactor: require self and sentinel parameters for AddFlags
    No behavior change. Prepares for adding the CoinsCachePairs to
    a linked list when flagged.

--- src/coins.h
+++ src/coins.h
@@ -13,6 +13,7 @@
 #include <serialize.h>
 #include <support/allocators/pool.h>
 #include <uint256.h>
+#include <util/check.h>
 #include <util/hasher.h>
 
 #include <assert.h>
@@ -140,7 +141,14 @@ public:
         ClearFlags();
     }
 
-    inline void AddFlags(uint8_t flags) noexcept { m_flags |= flags; }
+    //! Adding a flag also requires a self reference to the pair that contains
+    //! this entry in the CCoinsCache map and a reference to the sentinel of the
+    //! flagged pair linked list.
+    inline void AddFlags(uint8_t flags, CoinsCachePair& self, CoinsCachePair& sentinel) noexcept
+    {
+        Assume(&self.second == this);
+        m_flags |= flags;
+    }
     inline void ClearFlags() noexcept
     {
         m_flags = 0;
@@ -148,6 +156,12 @@ public:
     inline uint8_t GetFlags() const noexcept { return m_flags; }
     inline bool IsDirty() const noexcept { return m_flags & DIRTY; }
     inline bool IsFresh() const noexcept { return m_flags & FRESH; }
+
+    //! Only use this for initializing the linked list sentinel
+    inline void SelfRef(CoinsCachePair& self) noexcept
+    {
+        Assume(&self.second == this);
+    }
 };
 
 /**
@@ -255,6 +269,8 @@ protected:
      */
     mutable uint256 hashBlock;
     mutable CCoinsMapMemoryResource m_cache_coins_memory_resource{};
+    /* The starting sentinel of the flagged entry circular doubly linked list. */
+    mutable CoinsCachePair m_sentinel;
     mutable CCoinsMap cacheCoins;
 
     /* Cached dynamic memory usage for the inner Coin objects. */
