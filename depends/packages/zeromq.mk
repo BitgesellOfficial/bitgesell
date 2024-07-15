5824e160d152fef1a9b70b1d658f77df123d63c4
fanquake <fanquake@gmail.com>
2024-07-15 16:43:18 +0100

    depends: add zeromq no librt patch

--- depends/packages/zeromq.mk
+++ depends/packages/zeromq.mk
@@ -7,6 +7,8 @@ $(package)_patches = remove_libstd_link.patch
 $(package)_patches += macos_mktemp_check.patch
 $(package)_patches += builtin_sha1.patch
 $(package)_patches += fix_have_windows.patch
+$(package)_patches += cmake_minimum.patch
+$(package)_patches += no_librt.patch
 
 define $(package)_set_vars
   $(package)_config_opts = --without-docs --disable-shared --disable-valgrind
@@ -20,7 +22,9 @@ define $(package)_preprocess_cmds
   patch -p1 < $($(package)_patch_dir)/remove_libstd_link.patch && \
   patch -p1 < $($(package)_patch_dir)/macos_mktemp_check.patch && \
   patch -p1 < $($(package)_patch_dir)/builtin_sha1.patch && \
-  patch -p1 < $($(package)_patch_dir)/fix_have_windows.patch
+  patch -p1 < $($(package)_patch_dir)/fix_have_windows.patch && \
+  patch -p1 < $($(package)_patch_dir)/cmake_minimum.patch && \
+  patch -p1 < $($(package)_patch_dir)/no_librt.patchs
 endef
 
 define $(package)_config_cmds
