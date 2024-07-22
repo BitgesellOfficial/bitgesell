1a255baab44db9f762486fb514e82deeaf7a7ceb
Lőrinc <pap.lorinc@gmail.com>
2024-07-22 13:56:24 +0200

    Fix lint-spelling warnings
    These warnings were often polluting the CI output, e.g. https://github.com/bitcoin/bitcoin/pull/30499/checks?check_run_id=27745036545
    
    > ./test/lint/lint-spelling.py
    
    before the change:
    ```
    doc/design/libraries.md:100: targetted ==> targeted
    doc/developer-notes.md:495: dependant ==> dependent
    src/bench/sign_transaction.cpp:49: hashIn ==> hashing, hash in
    src/bitcoin-chainstate.cpp:213: hashIn ==> hashing, hash in
    src/bitcoin-chainstate.cpp:213: hashIn ==> hashing, hash in
    src/coins.cpp:24: viewIn ==> viewing, view in
    src/coins.cpp:24: viewIn ==> viewing, view in
    src/coins.cpp:29: viewIn ==> viewing, view in
    src/coins.cpp:29: viewIn ==> viewing, view in
    src/coins.h:44: outIn ==> outing, out in
    src/coins.h:44: outIn ==> outing, out in
    src/coins.h:45: outIn ==> outing, out in
    src/coins.h:45: outIn ==> outing, out in
    src/coins.h:215: viewIn ==> viewing, view in
    src/coins.h:220: viewIn ==> viewing, view in
    src/primitives/transaction.h:37: hashIn ==> hashing, hash in
    src/primitives/transaction.h:37: hashIn ==> hashing, hash in
    src/protocol.cpp:51: hashIn ==> hashing, hash in
    src/protocol.cpp:51: hashIn ==> hashing, hash in
    src/protocol.h:497: hashIn ==> hashing, hash in
    src/qt/forms/optionsdialog.ui:344: incomin ==> incoming
    src/qt/optionsdialog.cpp:445: proxys ==> proxies
    src/rpc/mining.cpp:987: hashIn ==> hashing, hash in
    src/rpc/mining.cpp:987: hashIn ==> hashing, hash in
    src/script/interpreter.h:298: amountIn ==> amounting, amount in
    src/script/interpreter.h:298: amountIn ==> amounting, amount in
    src/script/interpreter.h:299: amountIn ==> amounting, amount in
    src/script/interpreter.h:299: amountIn ==> amounting, amount in
    src/script/sigcache.h:70: amountIn ==> amounting, amount in
    src/script/sigcache.h:70: amountIn ==> amounting, amount in
    src/signet.cpp:144: amountIn ==> amounting, amount in
    src/test/fuzz/util/net.cpp:386: occured ==> occurred
    src/test/fuzz/util/net.cpp:398: occured ==> occurred
    src/util/vecdeque.h:79: deques ==> dequeues
    src/util/vecdeque.h:160: deques ==> dequeues
    src/util/vecdeque.h:184: deques ==> dequeues
    src/util/vecdeque.h:194: deques ==> dequeues
    src/validation.cpp:2130: re-declared ==> redeclared
    src/validation.h:348: outIn ==> outing, out in
    src/validation.h:349: outIn ==> outing, out in
    test/functional/wallet_bumpfee.py:851: atleast ==> at least
    ```

--- test/functional/wallet_bumpfee.py
+++ test/functional/wallet_bumpfee.py
@@ -800,5 +800,27 @@ def test_feerate_checks_replaced_outputs(self, rbf_node, peer_node):
     self.clear_mempool()
 
 
+def test_bumpfee_with_feerate_ignores_walletincrementalrelayfee(self, rbf_node, peer_node):
+    self.log.info('Test that bumpfee with fee_rate ignores walletincrementalrelayfee')
+    # Make sure there is enough balance
+    peer_node.sendtoaddress(rbf_node.getnewaddress(), 2)
+    self.generate(peer_node, 1)
+
+    dest_address = peer_node.getnewaddress(address_type="bech32")
+    tx = rbf_node.send(outputs=[{dest_address: 1}], fee_rate=2)
+
+    # Ensure you can not fee bump with a fee_rate below or equal to the original fee_rate
+    assert_raises_rpc_error(-8, "Insufficient total fee", rbf_node.bumpfee, tx["txid"], {"fee_rate": 1})
+    assert_raises_rpc_error(-8, "Insufficient total fee", rbf_node.bumpfee, tx["txid"], {"fee_rate": 2})
+
+    # Ensure you can not fee bump if the fee_rate is more than original fee_rate but the total fee from new fee_rate is
+    # less than (original fee + incrementalrelayfee)
+    assert_raises_rpc_error(-8, "Insufficient total fee", rbf_node.bumpfee, tx["txid"], {"fee_rate": 2.8})
+
+    # You can fee bump as long as the new fee set from fee_rate is at least (original fee + incrementalrelayfee)
+    rbf_node.bumpfee(tx["txid"], {"fee_rate": 3})
+    self.clear_mempool()
+
+
 if __name__ == "__main__":
     BumpFeeTest(__file__).main()
