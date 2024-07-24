e9f3245d3537a1f004e9a18e8e9fd49696785589
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-07-24 11:54:41 +0100

    ci: Migrate CI scripts to CMake

--- ci/test/00_setup_env_native_msan.sh
+++ ci/test/00_setup_env_native_msan.sh
@@ -18,7 +18,7 @@ export PACKAGES="ninja-build"
 export DEP_OPTS="DEBUG=1 NO_BDB=1 NO_QT=1 CC=clang CXX=clang++ CFLAGS='${MSAN_FLAGS}' CXXFLAGS='${MSAN_AND_LIBCXX_FLAGS}'"
 export GOAL="install"
 # _FORTIFY_SOURCE is not compatible with MSAN.
-export BGL_CONFIG="--with-sanitizers=memory CPPFLAGS='-U_FORTIFY_SOURCE'"
+export BGL_CONFIG="-DSANITIZERS=memory -DAPPEND_CPPFLAGS='-U_FORTIFY_SOURCE'"
 export USE_MEMORY_SANITIZER="true"
 export RUN_FUNCTIONAL_TESTS="false"
 export CCACHE_MAXSIZE=250M
