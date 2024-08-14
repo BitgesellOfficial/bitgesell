0b66bc290d0b292a3b85bcc847f48dcce50a11c2
Martin Zumsande <mzumsande@gmail.com>
2024-08-14 16:47:49 -0400

    rpc: call RecalculateBestHeader as part of reconsiderblock
    Co-authored-by: Fabian Jahr <fjahr@protonmail.com>

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -1658,6 +1658,7 @@ void ReconsiderBlock(ChainstateManager& chainman, uint256 block_hash) {
         }
 
         chainman.ActiveChainstate().ResetBlockFailureFlags(pblockindex);
+        chainman.RecalculateBestHeader();
     }
 
     BlockValidationState state;
