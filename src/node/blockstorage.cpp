67c2920670d9563cdc52fcfa130803d4479e7b99
TheCharlatan <seb.kung@gmail.com>
2024-08-30 10:16:29 +0200

    validation: Use span for ImportBlocks paths
    Makes it friendlier for potential future users of the kernel library if
    they do not store the headers in a std::vector, but can guarantee
    contiguous memory.

--- src/node/blockstorage.cpp
+++ src/node/blockstorage.cpp
@@ -1211,7 +1211,7 @@ public:
     }
 };
 
-void ImportBlocks(ChainstateManager& chainman, std::vector<fs::path> vImportFiles)
+void ImportBlocks(ChainstateManager& chainman, std::span<const fs::path> import_paths)
 {
     ImportingNow imp{chainman.m_blockman.m_importing};
 
@@ -1246,7 +1246,7 @@ void ImportBlocks(ChainstateManager& chainman, std::vector<fs::path> vImportFile
     }
 
     // -loadblock=
-    for (const fs::path& path : vImportFiles) {
+    for (const fs::path& path : import_paths) {
         AutoFile file{fsbridge::fopen(path, "rb")};
         if (!file.IsNull()) {
             LogPrintf("Importing blocks file %s...\n", fs::PathToString(path));
