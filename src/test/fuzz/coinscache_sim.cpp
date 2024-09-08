baa4ccc8dafce0d456c840ac8d473f24b14990c4
Lőrinc <pap.lorinc@gmail.com>
2024-09-08 21:57:18 +0200

    refactor: Return optional of Coin in GetCoin
    Leaving the parameter as well for now.
    
    Co-authored-by: TheCharlatan <seb.kung@gmail.com>

--- src/test/fuzz/coinscache_sim.cpp
+++ src/test/fuzz/coinscache_sim.cpp
@@ -147,18 +147,14 @@ class CoinsViewBottom final : public CCoinsView
     std::map<COutPoint, Coin> m_data;
 
 public:
-    bool GetCoin(const COutPoint& outpoint, Coin& coin) const final
+    std::optional<Coin> GetCoin(const COutPoint& outpoint, Coin& coin) const final
     {
         auto it = m_data.find(outpoint);
         if (it == m_data.end()) {
-            if ((outpoint.n % 5) == 3) {
-                coin.Clear();
-                return true;
-            }
-            return false;
+            return std::nullopt;
         } else {
             coin = it->second;
-            return true;
+            return coin; // TODO GetCoin shouldn't return spent coins
         }
     }
 
@@ -466,7 +462,7 @@ FUZZ_TARGET(coinscache_sim)
     // Compare the bottom coinsview (not a CCoinsViewCache) with sim_cache[0].
     for (uint32_t outpointidx = 0; outpointidx < NUM_OUTPOINTS; ++outpointidx) {
         Coin realcoin;
-        bool real = bottom.GetCoin(data.outpoints[outpointidx], realcoin);
+        auto real = bottom.GetCoin(data.outpoints[outpointidx], realcoin);
         auto sim = lookup(outpointidx, 0);
         if (!sim.has_value()) {
             assert(!real || realcoin.IsSpent());
