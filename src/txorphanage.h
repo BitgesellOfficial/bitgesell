696f8a02a57c32ecb0664f142533da100f88abd5
tdb3 <106488469+tdb3@users.noreply.github.com>
2024-09-18 12:28:04 -0400

    net: add GetOrphanTransactions() to PeerManager
    Updates PeerManager (and Impl) to provide
    orphans with metadata

--- src/txorphanage.h
+++ src/txorphanage.h
@@ -74,6 +74,8 @@ public:
         NodeSeconds nTimeExpire;
     };
 
+    std::vector<OrphanTxBase> GetOrphanTransactions() const;
+
 protected:
     struct OrphanTx : public OrphanTxBase {
         size_t list_pos;
