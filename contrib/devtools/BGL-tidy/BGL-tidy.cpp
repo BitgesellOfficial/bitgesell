abdf13b482b18f65b930d640ce5f389bf8f13c12
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2024-09-19 16:46:09 +0200

    Remove redundant unterminated-logprintf tidy check

--- contrib/devtools/BGL-tidy/BGL-tidy.cpp
+++ contrib/devtools/BGL-tidy/BGL-tidy.cpp
@@ -2,7 +2,6 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "logprintf.h"
 #include "nontrivial-threadlocal.h"
 
 #include <clang-tidy/ClangTidyModule.h>
@@ -13,7 +12,7 @@ class BGLModule final : public clang::tidy::ClangTidyModule
 public:
     void addCheckFactories(clang::tidy::ClangTidyCheckFactories& CheckFactories) override
     {
-        CheckFactories.registerCheck<BGL::LogPrintfCheck>("BGL-unterminated-logprintf");
+        CheckFactories.registerCheck<BGL::NonTrivialThreadLocal>("BGL-nontrivial-threadlocal");
     }
 };
 
