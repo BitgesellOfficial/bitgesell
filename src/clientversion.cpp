fcacaa76f05c85a4fb686293523893f45807a82e
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-10-11 10:43:49 +0100

    scripted-diff: Clarify "user agent" variable name
    This change allows to the use of the `CLIENT_` namespace without
    potential name clashes.
    
    -BEGIN VERIFY SCRIPT-
    sed -i "s/\<CLIENT_NAME\>/UA_NAME/g" $( git grep -l "CLIENT_NAME" ./src)
    -END VERIFY SCRIPT-

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -20,7 +20,7 @@ using util::Join;
  * for both bitcoind and bitcoin-qt, to make it harder for attackers to
  * target servers or GUI users specifically.
  */
-const std::string CLIENT_NAME("SatoshiBGL");
+const std::string UA_NAME("SatoshiBGL");
 
 
 #include <BGL-build-info.h>
