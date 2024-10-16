b11cd9f6ed5330fcff70a0407e715c7db62d96e6
Antoine Poinsot <darosior@protonmail.com>
2024-10-16 13:34:15 +0100

    [test] A non-standard transaction which is also consensus-invalid should return the consensus error

--- test/functional/feature_block.py
+++ test/functional/feature_block.py
@@ -89,6 +89,7 @@ class FullBlockTest(BGLTestFramework):
         self.extra_args = [[
             '-acceptnonstdtxn=1',  # This is a consensus block test, we don't care about tx policy
             '-testactivationheight=bip34@2',
+            '-par=1', # Until https://github.com/bitcoin/bitcoin/issues/30960 is fixed
         ]]
 
     def run_test(self):
