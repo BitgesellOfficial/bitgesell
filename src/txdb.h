3bc121ed682407718352d18bb437a4d55f253f8f
TheCharlatan <seb.kung@gmail.com>
2024-11-22 20:51:34 +0100

    kernel: Move kernel-specific cache size options to kernel
    Carrying non-kernel related fields in the cache sizes for the indexes is
    confusing for kernel library users. The cache sizes also are set
    currently with magic numbers in bitcoin-chainstate. The comments for the
    cache size calculations are also not completely clear.
    
    Solve these things by moving the kernel-specific cache size fields to
    their own struct.
    
    This slightly changes the way the cache is allocated if the txindex
    and/or blockfilterindex is used. Since they are now given precedence
    over the block tree db cache, this results in a bit less cache being
    allocated to the block tree db, coinsdb and coins caches. The effect is
    negligible though, i.e. cache sizes with default dbcache reported
    through the logs are:
    
    master:
    Cache configuration:
    * Using 2.0 MiB for block index database
    * Using 56.0 MiB for transaction index database
    * Using 49.0 MiB for basic block filter index database
    * Using 8.0 MiB for chain state database
    * Using 335.0 MiB for in-memory UTXO set (plus up to 286.1 MiB of unused mempool space)
    
    this branch:
    Cache configuration:
    * Using 2.0 MiB for block index database
    * Using 56.2 MiB for transaction index database
    * Using 49.2 MiB for basic block filter index database
    * Using 8.0 MiB for chain state database
    * Using 334.5 MiB for in-memory UTXO set (plus up to 286.1 MiB of unused mempool space)

--- src/txdb.h
+++ src/txdb.h
@@ -29,16 +29,12 @@ static const int64_t nDefaultDbBatchSize = 16 << 20;
 static const int64_t nMaxDbCache = sizeof(void*) > 4 ? 16384 : 1024;
 //! min. -dbcache (MiB)
 static const int64_t nMinDbCache = 4;
-//! Max memory allocated to block tree DB specific cache (MiB)
-static const int64_t nMaxBlockDBCache = 2;
 // Unlike for the UTXO database, for the txindex scenario the leveldb cache make
 // a meaningful difference: https://github.com/bitcoin/bitcoin/pull/8273#issuecomment-229601991
 //! Max memory allocated to tx index DB specific cache in MiB.
 static const int64_t nMaxTxIndexCache = 1024;
 //! Max memory allocated to all block filter index caches combined in MiB.
 static const int64_t max_filter_index_cache = 1024;
-//! Max memory allocated to coin DB specific cache (MiB)
-static const int64_t nMaxCoinsDBCache = 8;
 
 //! User-controlled performance and debug options.
 struct CoinsViewOptions {
