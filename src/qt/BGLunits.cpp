49ce3ee3bc2a877974f945a36a7fb987ee2ba845
Lőrinc <pap.lorinc@gmail.com>
2024-11-24 19:20:11 +0100

    refactor: Fix remaining clang-tidy performance-unnecessary-copy-initialization errors

--- src/qt/BGLunits.cpp
+++ src/qt/BGLunits.cpp
@@ -165,7 +165,7 @@ bool BGLUnits::parse(Unit unit, const QString& value, CAmount* val_out)
     {
         return false; // More than one dot
     }
-    QString whole = parts[0];
+    const QString& whole = parts[0];
     QString decimals;
 
     if(parts.size() > 1)
