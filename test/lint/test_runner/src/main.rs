974f203e169f9d80380180d5343c368c4ecc71bf
willcl-ark <will@256k1.dev>
2024-12-05 11:23:08 +0000

    lint: bump MLC to v0.19.0
    Fixes: #31044
    
    This MLC update includes a change which will ignore files being ignored
    by git, and help avoid false-positives when linting in this repo.

--- test/lint/test_runner/src/main.rs
+++ test/lint/test_runner/src/main.rs
@@ -518,6 +518,7 @@ fn lint_markdown() -> LintResult {
         "--ignore-path",
         md_ignore_path_str.as_str(),
         "--gitignore",
+        "--gituntracked",
         "--root-dir",
         ".",
     ])
