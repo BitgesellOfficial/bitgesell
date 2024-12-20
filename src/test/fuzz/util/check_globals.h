4ca4e47c4bd74d3979365b2812efe38df0e88431
marcofleon <marleo23@proton.me>
2024-12-20 13:11:14 +0000

    fuzz: Abort when calling system time without setting mock time

--- src/test/fuzz/util/check_globals.h
+++ src/test/fuzz/util/check_globals.h
@@ -5,9 +5,12 @@
 #ifndef BGL_TEST_FUZZ_UTIL_CHECK_GLOBALS_H
 #define BGL_TEST_FUZZ_UTIL_CHECK_GLOBALS_H
 
+#include <atomic>
 #include <memory>
 #include <optional>
 #include <string>
+
+extern std::atomic<bool> g_used_system_time;
 
 struct CheckGlobalsImpl;
 struct CheckGlobals {
