f56f8a839cdeb6d82b9861ca64a0f1f644c6e4f3
tdb3 <106488469+tdb3@users.noreply.github.com>
2025-01-01 19:50:03 -0500

    test: use REGTEST_N_BITS in feature_block

--- test/functional/feature_block.py
+++ test/functional/feature_block.py
@@ -13,6 +13,7 @@ from test_framework.blocktools import (
     create_tx_with_script,
     get_legacy_sigopcount_block,
     MAX_BLOCK_SIGOPS,
+    REGTEST_N_BITS,
 )
 from test_framework.messages import (
     CBlock,
@@ -597,7 +598,7 @@ class FullBlockTest(BGLTestFramework):
         b44 = CBlock()
         b44.nTime = self.tip.nTime + 1
         b44.hashPrevBlock = self.tip.sha256
-        b44.nBits = 0x207fffff
+        b44.nBits = REGTEST_N_BITS
         b44.vtx.append(coinbase)
         tx = self.create_and_sign_transaction(out[14], 1)
         b44.vtx.append(tx)
@@ -613,7 +614,7 @@ class FullBlockTest(BGLTestFramework):
         b45 = CBlock()
         b45.nTime = self.tip.nTime + 1
         b45.hashPrevBlock = self.tip.sha256
-        b45.nBits = 0x207fffff
+        b45.nBits = REGTEST_N_BITS
         b45.vtx.append(non_coinbase)
         b45.hashMerkleRoot = b45.calc_merkle_root()
         b45.solve()
@@ -627,7 +628,7 @@ class FullBlockTest(BGLTestFramework):
         b46 = CBlock()
         b46.nTime = b44.nTime + 1
         b46.hashPrevBlock = b44.sha256
-        b46.nBits = 0x207fffff
+        b46.nBits = REGTEST_N_BITS
         b46.vtx = []
         b46.hashMerkleRoot = 0
         b46.solve()
