e7823aacb75708280e3a2ba94a79409753223aef
tdb3 <106488469+tdb3@users.noreply.github.com>
2025-01-01 19:47:54 -0500

    test: use DIFF_1_N_BITS in tool_signet_miner

--- test/functional/tool_signet_miner.py
+++ test/functional/tool_signet_miner.py
@@ -9,6 +9,7 @@ import subprocess
 import sys
 import time
 
+from test_framework.blocktools import DIFF_1_N_BITS
 from test_framework.key import ECKey
 from test_framework.script_util import key_to_p2wpkh_script
 from test_framework.test_framework import BGLTestFramework
@@ -55,7 +56,7 @@ class SignetMinerTest(BGLTestFramework):
                 'generate',
                 f'--address={node.getnewaddress()}',
                 f'--grind-cmd={self.options.BGLutil} grind',
-                '--nbits=1d00ffff',
+                f'--nbits={DIFF_1_N_BITS:08x}',
                 f'--set-block-time={int(time.time())}',
             ], check=True, stderr=subprocess.STDOUT)
         assert_equal(node.getblockcount(), 1)
