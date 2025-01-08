d5724b2ded142457ddcb0575bb7a6abb3c9b7705
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2025-01-08 11:02:46 +0100

    test: Remove --noshutdown flag

--- test/functional/test_framework/test_framework.py
+++ test/functional/test_framework/test_framework.py
@@ -170,10 +170,8 @@ class BGLTestFramework(metaclass=BGLTestMetaClass):
         previous_releases_path = os.getenv("PREVIOUS_RELEASES_DIR") or os.getcwd() + "/releases"
         parser = argparse.ArgumentParser(usage="%(prog)s [options]")
         parser.add_argument("--nocleanup", dest="nocleanup", default=False, action="store_true",
-                            help="Leave BGLds and test.* datadir on exit or error")
-        parser.add_argument("--noshutdown", dest="noshutdown", default=False, action="store_true",
-                            help="Don't stop BGLds after the test execution")
-        parser.add_argument("--cachedir", dest="cachedir", default=os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + "/../../cache"),
+                            help="Leave bitcoinds and test.* datadir on exit or error")
+        parser.add_argument("--cachedir", dest="cachedir", default=os.path.abspath(os.path.dirname(test_file) + "/../cache"),
                             help="Directory for caching pregenerated datadirs (default: %(default)s)")
         parser.add_argument("--tmpdir", dest="tmpdir", help="Root directory for datadirs")
         parser.add_argument("-l", "--loglevel", dest="loglevel", default="INFO",
@@ -325,18 +323,12 @@ class BGLTestFramework(metaclass=BGLTestMetaClass):
 
         self.log.debug('Closing down network thread')
         self.network_thread.close()
-        if not self.options.noshutdown:
-            self.log.info("Stopping nodes")
-            if self.nodes:
-                self.stop_nodes()
-        else:
-            for node in self.nodes:
-                node.cleanup_on_exit = False
-            self.log.info("Note: BGLds were not stopped and may still be running")
+        self.log.info("Stopping nodes")
+        if self.nodes:
+            self.stop_nodes()
 
         should_clean_up = (
             not self.options.nocleanup and
-            not self.options.noshutdown and
             self.success != TestStatus.FAILED and
             not self.options.perf
         )
