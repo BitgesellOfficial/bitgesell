f9cab78523cc69b5d61a4870950f952f9613cdaf
Ava Chow <github@achow101.com>
2025-01-08 17:48:41 -0500

    wallet: change FillPSBT to take sighash as optional
    Instead of having the caller have to figure out the correct sane default
    to provide to FillPSBT, have FillPSBT do that by having it take the
    sighash type as an optional. This further allows it to distinguish
    between an explicit sighash type being provided and expecting the
    default value to be used.

--- src/wallet/test/fuzz/scriptpubkeyman.cpp
+++ src/wallet/test/fuzz/scriptpubkeyman.cpp
@@ -192,8 +192,12 @@ FUZZ_TARGET(scriptpubkeyman, .init = initialize_spkm)
                 }
                 auto psbt{*opt_psbt};
                 const PrecomputedTransactionData txdata{PrecomputePSBTData(psbt)};
-                const int sighash_type{fuzzed_data_provider.ConsumeIntegralInRange<int>(0, 150)};
-                (void)spk_manager->FillPSBT(psbt, txdata, sighash_type, fuzzed_data_provider.ConsumeBool(), fuzzed_data_provider.ConsumeBool(), nullptr, fuzzed_data_provider.ConsumeBool());
+                std::optional<int> sighash_type{fuzzed_data_provider.ConsumeIntegralInRange<int>(0, 151)};
+                if (sighash_type == 151) sighash_type = std::nullopt;
+                auto sign  = fuzzed_data_provider.ConsumeBool();
+                auto bip32derivs = fuzzed_data_provider.ConsumeBool();
+                auto finalize = fuzzed_data_provider.ConsumeBool();
+                (void)spk_manager->FillPSBT(psbt, txdata, sighash_type, sign, bip32derivs, nullptr, finalize);
             }
         );
     }
