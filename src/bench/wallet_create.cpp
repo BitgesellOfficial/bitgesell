bitcoin-ghost/bitcoin-core-ladder-script

bench: [refactor] iwyu

src/bench/wallet_create.cpp
@@ -2,14 +2,25 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or https://www.opensource.org/licenses/mit-license.php.
 
-#include <config/bitcoin-config.h> // IWYU pragma: keep
-
 #include <bench/bench.h>
-#include <node/context.h>
+#include <config/bitcoin-config.h> // IWYU pragma: keep
 #include <random.h>
+#include <support/allocators/secure.h>
 #include <test/util/setup_common.h>
+#include <uint256.h>
+#include <util/fs.h>
+#include <util/translation.h>
 #include <wallet/context.h>
+#include <wallet/db.h>
 #include <wallet/wallet.h>
+#include <wallet/walletutil.h>
+
+#include <cassert>
+#include <memory>
+#include <optional>
+#include <string>
+#include <utility>
+#include <vector>
 
 namespace wallet {
 static void WalletCreate(benchmark::Bench& bench, bool encrypted)

src/bench/wallet_ismine.cpp
@@ -2,19 +2,28 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <config/bitcoin-config.h> // IWYU pragma: keep
+#include <addresstype.h>
 #include <bench/bench.h>
-#include <interfaces/chain.h>
+#include <config/bitcoin-config.h> // IWYU pragma: keep
 #include <key.h>
 #include <key_io.h>
-#include <node/context.h>
+#include <script/descriptor.h>
+#include <script/script.h>
+#include <script/signingprovider.h>
+#include <sync.h>
 #include <test/util/setup_common.h>
-#include <util/translation.h>
-#include <validationinterface.h>
 #include <wallet/context.h>
+#include <wallet/db.h>
 #include <wallet/test/util.h>
+#include <wallet/types.h>
 #include <wallet/wallet.h>
 #include <wallet/walletutil.h>
+
+#include <cassert>
+#include <cstdint>
+#include <memory>
+#include <string>
+#include <utility>
 
 namespace wallet {
 static void WalletIsMine(benchmark::Bench& bench, bool legacy_wallet, int num_combo = 0)

src/bench/wallet_loading.cpp
@@ -2,21 +2,25 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <config/bitcoin-config.h> // IWYU pragma: keep
-
+#include <addresstype.h>
 #include <bench/bench.h>
-#include <interfaces/chain.h>
-#include <node/context.h>
-#include <test/util/mining.h>
+#include <config/bitcoin-config.h> // IWYU pragma: keep
+#include <consensus/amount.h>
+#include <outputtype.h>
+#include <primitives/transaction.h>
 #include <test/util/setup_common.h>
-#include <wallet/test/util.h>
-#include <util/translation.h>
-#include <validationinterface.h>
+#include <util/check.h>
 #include <wallet/context.h>
-#include <wallet/receive.h>
+#include <wallet/db.h>
+#include <wallet/test/util.h>
+#include <wallet/transaction.h>
 #include <wallet/wallet.h>
+#include <wallet/walletutil.h>
 
-#include <optional>
+#include <cstdint>
+#include <memory>
+#include <utility>
+#include <vector>
 
 namespace wallet{
 static void AddTx(CWallet& wallet)

BitgesellOfficial/bitgesell

src/bench/wallet_create.cpp
@@ -2,14 +2,25 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or https://www.opensource.org/licenses/mit-license.php.
 
-#include <config/BGL-config.h> // IWYU pragma: keep
-
 #include <bench/bench.h>
-#include <node/context.h>
+#include <config/BGL-config.h> // IWYU pragma: keep
 #include <random.h>
+#include <support/allocators/secure.h>
 #include <test/util/setup_common.h>
+#include <uint256.h>
+#include <util/fs.h>
+#include <util/translation.h>
 #include <wallet/context.h>
+#include <wallet/db.h>
 #include <wallet/wallet.h>
+#include <wallet/walletutil.h>
+
+#include <cassert>
+#include <memory>
+#include <optional>
+#include <string>
+#include <utility>
+#include <vector>
 
 namespace wallet {
 static void WalletCreate(benchmark::Bench& bench, bool encrypted)

src/bench/wallet_ismine.cpp
@@ -2,19 +2,28 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <config/BGL-config.h> // IWYU pragma: keep
+#include <addresstype.h>
 #include <bench/bench.h>
-#include <interfaces/chain.h>
+#include <config/BGL-config.h> // IWYU pragma: keep
 #include <key.h>
 #include <key_io.h>
-#include <node/context.h>
+#include <script/descriptor.h>
+#include <script/script.h>
+#include <script/signingprovider.h>
+#include <sync.h>
 #include <test/util/setup_common.h>
-#include <util/translation.h>
-#include <validationinterface.h>
 #include <wallet/context.h>
+#include <wallet/db.h>
 #include <wallet/test/util.h>
+#include <wallet/types.h>
 #include <wallet/wallet.h>
 #include <wallet/walletutil.h>
+
+#include <cassert>
+#include <cstdint>
+#include <memory>
+#include <string>
+#include <utility>
 
 namespace wallet {
 static void WalletIsMine(benchmark::Bench& bench, bool legacy_wallet, int num_combo = 0)

src/bench/wallet_loading.cpp
@@ -2,21 +2,25 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <config/BGL-config.h> // IWYU pragma: keep
-
+#include <addresstype.h>
 #include <bench/bench.h>
-#include <interfaces/chain.h>
-#include <node/context.h>
-#include <test/util/mining.h>
+#include <config/BGL-config.h> // IWYU pragma: keep
+#include <consensus/amount.h>
+#include <outputtype.h>
+#include <primitives/transaction.h>
 #include <test/util/setup_common.h>
-#include <wallet/test/util.h>
-#include <util/translation.h>
-#include <validationinterface.h>
+#include <util/check.h>
 #include <wallet/context.h>
-#include <wallet/receive.h>
+#include <wallet/db.h>
+#include <wallet/test/util.h>
+#include <wallet/transaction.h>
 #include <wallet/wallet.h>
+#include <wallet/walletutil.h>
 
-#include <optional>
+#include <cstdint>
+#include <memory>
+#include <utility>
+#include <vector>
 
 namespace wallet{
 static void AddTx(CWallet& wallet)