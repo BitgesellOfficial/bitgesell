bitcoin-ghost/bitcoin-core-ladder-script

doc: Updating docs from autotools to cmake
replaced --enable-debug with -DCMAKE_BUILD_TYPE=Debug in developer-notes
replaced --enable-multiprocess with -DWITH_MULTIPROCESS=ON
replaced --disable-zmq with -DWITH_ZMQ=OFF

contrib/devtools/test_deterministic_coverage.sh
@@ -81,7 +81,7 @@ if ! command -v gcovr > /dev/null; then
 fi
 
 if [[ ! -e ${TEST_BITCOIN_BINARY} ]]; then
-    echo "Error: Executable ${TEST_BITCOIN_BINARY} not found. Run \"./configure --enable-lcov\" and compile."
+    echo "Error: Executable ${TEST_BITCOIN_BINARY} not found. Run \"cmake -B build -DCMAKE_BUILD_TYPE=Coverage\" and compile."
     exit 1
 fi
 
@@ -90,7 +90,7 @@ get_file_suffix_count() {
 }
 
 if [[ $(get_file_suffix_count gcno) == 0 ]]; then
-    echo "Error: Could not find any *.gcno files. The *.gcno files are generated by the compiler. Run \"./configure --enable-lcov\" and re-compile."
+    echo "Error: Could not find any *.gcno files. The *.gcno files are generated by the compiler. Run \"cmake -B build -DCMAKE_BUILD_TYPE=Coverage\" and re-compile."
     exit 1
 fi
 
@@ -115,7 +115,7 @@ while [[ ${TEST_RUN_ID} -lt ${N_TEST_RUNS} ]]; do
     fi
     rm "${TEST_OUTPUT_TEMPFILE}"
     if [[ $(get_file_suffix_count gcda) == 0 ]]; then
-        echo "Error: Running the test suite did not create any *.gcda files. The gcda files are generated when the instrumented test programs are executed. Run \"./configure --enable-lcov\" and re-compile."
+        echo "Error: Running the test suite did not create any *.gcda files. The gcda files are generated when the instrumented test programs are executed. Run \"cmake -B build -DCMAKE_BUILD_TYPE=Coverage\" and re-compile."
         exit 1
     fi
     GCOVR_TEMPFILE=$(mktemp)

BitgesellOfficial/bitgesell

contrib/devtools/test_deterministic_coverage.sh
@@ -81,7 +81,7 @@ if ! command -v gcovr > /dev/null; then
 fi
 
 if [[ ! -e ${TEST_BGL_BINARY} ]]; then
-    echo "Error: Executable ${TEST_BGL_BINARY} not found. Run \"./configure --enable-lcov\" and compile."
+    echo "Error: Executable ${TEST_BGL_BINARY} not found. Run \"cmake -B build -DCMAKE_BUILD_TYPE=Coverage\" and compile."
     exit 1
 fi
 
@@ -90,7 +90,7 @@ get_file_suffix_count() {
 }
 
 if [[ $(get_file_suffix_count gcno) == 0 ]]; then
-    echo "Error: Could not find any *.gcno files. The *.gcno files are generated by the compiler. Run \"./configure --enable-lcov\" and re-compile."
+    echo "Error: Could not find any *.gcno files. The *.gcno files are generated by the compiler. Run \"cmake -B build -DCMAKE_BUILD_TYPE=Coverage\" and re-compile."
     exit 1
 fi
 
@@ -115,7 +115,7 @@ while [[ ${TEST_RUN_ID} -lt ${N_TEST_RUNS} ]]; do
     fi
     rm "${TEST_OUTPUT_TEMPFILE}"
     if [[ $(get_file_suffix_count gcda) == 0 ]]; then
-        echo "Error: Running the test suite did not create any *.gcda files. The gcda files are generated when the instrumented test programs are executed. Run \"./configure --enable-lcov\" and re-compile."
+        echo "Error: Running the test suite did not create any *.gcda files. The gcda files are generated when the instrumented test programs are executed. Run \"cmake -B build -DCMAKE_BUILD_TYPE=Coverage\" and re-compile."
         exit 1
     fi
     GCOVR_TEMPFILE=$(mktemp)