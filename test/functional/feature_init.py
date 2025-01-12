tdb3/bitcoin

test: add test for specifying custom pidfile via `-pid`

test/functional/feature_init.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2021-present The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
-"""Stress tests related to node initialization."""
+"""Tests related to node initialization."""
 from pathlib import Path
 import platform
 import shutil
@@ -12,7 +12,11 @@ from test_framework.test_node import ErrorMatch
 from test_framework.util import assert_equal
 
 
-class InitStressTest(BitcoinTestFramework):
+BITCOIN_PID_FILENAME_DEFAULT = "bitcoind.pid"
+BITCOIN_PID_FILENAME_CUSTOM = "my_fancy_bitcoin_pid_file.foobar"
+
+
+class InitTest(BitcoinTestFramework):
     """
     Ensure that initialization can be interrupted at a number of points and not impair
     subsequent starts.
@@ -25,7 +29,7 @@ class InitStressTest(BitcoinTestFramework):
         self.setup_clean_chain = False
         self.num_nodes = 1
 
-    def run_test(self):
+    def init_stress_test(self):
         """
         - test terminating initialization after seeing a certain log line.
         - test removing certain essential files to test startup error paths.
@@ -147,6 +151,29 @@ class InitStressTest(BitcoinTestFramework):
             shutil.move(node.chain_path / "blocks_bak", node.chain_path / "blocks")
             shutil.move(node.chain_path / "chainstate_bak", node.chain_path / "chainstate")
 
+    def init_pid_test(self):
+        self.log.info("Test specifying custom pid file via -pid command line option")
+        custom_pidfile_relative = BITCOIN_PID_FILENAME_CUSTOM
+        self.log.info(f"-> path relative to datadir ({custom_pidfile_relative})")
+        self.restart_node(0, [f"-pid={custom_pidfile_relative}"])
+        datadir = self.nodes[0].chain_path
+        assert not (datadir / BITCOIN_PID_FILENAME_DEFAULT).exists()
+        assert (datadir / custom_pidfile_relative).exists()
+        self.stop_node(0)
+        assert not (datadir / custom_pidfile_relative).exists()
+
+        custom_pidfile_absolute = Path(self.options.tmpdir) / BITCOIN_PID_FILENAME_CUSTOM
+        self.log.info(f"-> absolute path ({custom_pidfile_absolute})")
+        self.restart_node(0, [f"-pid={custom_pidfile_absolute}"])
+        assert not (datadir / BITCOIN_PID_FILENAME_DEFAULT).exists()
+        assert custom_pidfile_absolute.exists()
+        self.stop_node(0)
+        assert not custom_pidfile_absolute.exists()
+
+    def run_test(self):
+        self.init_pid_test()
+        self.init_stress_test()
+
 
 if __name__ == '__main__':
-    InitStressTest(__file__).main()
+    InitTest(__file__).main()

BitgesellOfficial/bitgesell

test/functional/feature_init.py
@@ -2,17 +2,20 @@
 # Copyright (c) 2021-present The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
-"""Stress tests related to node initialization."""
+"""Tests related to node initialization."""
 from pathlib import Path
 import platform
 import shutil
 
 from test_framework.test_framework import BGLTestFramework, SkipTest
-from test_framework.test_node import ErrorMatch
+from test_framework.test_node import (
+    BGL_PID_FILENAME_DEFAULT,
+    ErrorMatch,
+)
 from test_framework.util import assert_equal
 
 
-class InitStressTest(BGLTestFramework):
+class InitTest(BGLTestFramework):
     """
     Ensure that initialization can be interrupted at a number of points and not impair
     subsequent starts.
@@ -25,7 +28,7 @@ class InitStressTest(BGLTestFramework):
         self.setup_clean_chain = False
         self.num_nodes = 1
 
-    def run_test(self):
+    def init_stress_test(self):
         """
         - test terminating initialization after seeing a certain log line.
         - test removing certain essential files to test startup error paths.
@@ -149,6 +152,31 @@ class InitStressTest(BGLTestFramework):
             shutil.move(node.chain_path / "blocks_bak", node.chain_path / "blocks")
             shutil.move(node.chain_path / "chainstate_bak", node.chain_path / "chainstate")
 
+    def init_pid_test(self):
+        BGL_PID_FILENAME_CUSTOM = "my_fancy_BGL_pid_file.foobar"
+
+        self.log.info("Test specifying custom pid file via -pid command line option")
+        custom_pidfile_relative = BGL_PID_FILENAME_CUSTOM
+        self.log.info(f"-> path relative to datadir ({custom_pidfile_relative})")
+        self.restart_node(0, [f"-pid={custom_pidfile_relative}"])
+        datadir = self.nodes[0].chain_path
+        assert not (datadir / BGL_PID_FILENAME_DEFAULT).exists()
+        assert (datadir / custom_pidfile_relative).exists()
+        self.stop_node(0)
+        assert not (datadir / custom_pidfile_relative).exists()
+
+        custom_pidfile_absolute = Path(self.options.tmpdir) / BGL_PID_FILENAME_CUSTOM
+        self.log.info(f"-> absolute path ({custom_pidfile_absolute})")
+        self.restart_node(0, [f"-pid={custom_pidfile_absolute}"])
+        assert not (datadir / BGL_PID_FILENAME_DEFAULT).exists()
+        assert custom_pidfile_absolute.exists()
+        self.stop_node(0)
+        assert not custom_pidfile_absolute.exists()
+
+    def run_test(self):
+        self.init_pid_test()
+        self.init_stress_test()
+
 
 if __name__ == '__main__':
-    InitStressTest(__file__).main()
+    InitTest(__file__).main()