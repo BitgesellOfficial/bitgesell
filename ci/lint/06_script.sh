7e6b0529e1ef4041e7d29f46e266f8808c79a2e8
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2025-01-14 14:02:26 +0100

    lint: Move commit range printing to test_runner
    Having a single test_runner for all logic improves the consistency and
    UX.

--- ci/lint/06_script.sh
+++ ci/lint/06_script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright (c) 2018-2022 The Bitcoin Core developers
+# Copyright (c) 2018-present The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -8,23 +8,14 @@ export LC_ALL=C
 
 set -ex
 
-if [ -n "$LOCAL_BRANCH" ]; then
-  # To faithfully recreate CI linting locally, specify all commits on the current
-  # branch.
-  COMMIT_RANGE="$(git merge-base HEAD master)..HEAD"
-elif [ -n "$CIRRUS_PR" ]; then
-  COMMIT_RANGE="HEAD~..HEAD"
-  echo
-  git log --no-merges --oneline "$COMMIT_RANGE"
-  echo
-  test/lint/commit-script-check.sh "$COMMIT_RANGE"
-else
-  COMMIT_RANGE="SKIP_EMPTY_NOT_A_PR"
+if [ -n "$CIRRUS_PR" ]; then
+  export COMMIT_RANGE="HEAD~..HEAD"
+  if [ "$(git rev-list -1 HEAD)" != "$(git rev-list -1 --merges HEAD)" ]; then
+    echo "Error: The top commit must be a merge commit, usually the remote 'pull/${PR_NUMBER}/merge' branch."
+    false
+  fi
 fi
 
-echo
-git log --no-merges --oneline "$COMMIT_RANGE"
-echo
 RUST_BACKTRACE=1 "${LINT_RUNNER_PATH}/test_runner"
 
 if [ "$CIRRUS_REPO_FULL_NAME" = "BGL/BGL" ] && [ "$CIRRUS_PR" = "" ] ; then
