bitcoin/bitcoin

doc: add bitcoin-qt man description

src/qt/utilitydialog.cpp
@@ -57,9 +57,12 @@ HelpMessageDialog::HelpMessageDialog(QWidget *parent, bool about) :
         ui->aboutMessage->setWordWrap(true);
         ui->helpMessage->setVisible(false);
     } else {
-        setWindowTitle(tr("Command-line options"));
-        QString header = "Usage: bitcoin-qt [command-line options] [URI]\n\n"
-                         "Optional URI is a Bitcoin address in BIP21 URI format.\n";
+        setWindowTitle(tr("options"));
+        QString header = "The bitcoin-qt application provides a graphical interface for interacting with " PACKAGE_NAME ".\n\n"
+                         "It combines the core functionalities of bitcoind with a user-friendly interface for wallet management, transaction history, and network statistics.\n\n"
+                         "It is suitable for desktop users preferring a graphical over a command-line interface.\n\n"
+                         "You can optionally specify a [BIP21URI] using the BIP21 URI format.\n\n"
+                         "Usage: bitcoin-qt [options] [BIP21URI]\n\n";
         QTextCursor cursor(ui->helpMessage->document());
         cursor.insertText(version);
         cursor.insertBlock();

BitgesellOfficial/bitgesell

src/qt/utilitydialog.cpp
@@ -2,11 +2,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-<<<<<<< HEAD
-#include <config/BGL-config.h> // IWYU pragma: keep
-=======
-#include <bitcoin-build-config.h> // IWYU pragma: keep
->>>>>>> 1786be7b4a... scripted-diff: drop config/ subdir for bitcoin-config.h, rename to bitcoin-build-config.h
+#include <BGL-build-config.h> // IWYU pragma: keep
 
 #include <qt/utilitydialog.h>
 
@@ -60,8 +56,11 @@ HelpMessageDialog::HelpMessageDialog(QWidget *parent, bool about) :
         ui->helpMessage->setVisible(false);
     } else {
         setWindowTitle(tr("Command-line options"));
-        QString header = "Usage: BGL-qt [command-line options] [URI]\n\n"
-                         "Optional URI is a Bitcoin address in BIP21 URI format.\n";
+        QString header = "The BGL-qt application provides a graphical interface for interacting with " CLIENT_NAME ".\n\n"
+                         "It combines the core functionalities of BGLd with a user-friendly interface for wallet management, transaction history, and network statistics.\n\n"
+                         "It is suitable for users who prefer a graphical over a command-line interface.\n\n"
+                         "You can optionally specify a payment [URI], in e.g. the BIP21 URI format.\n\n"
+                         "Usage: BGL-qt [options] [URI]\n\n";
         QTextCursor cursor(ui->helpMessage->document());
         cursor.insertText(version);
         cursor.insertBlock();