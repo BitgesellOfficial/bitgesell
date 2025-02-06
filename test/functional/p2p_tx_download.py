16a5acafd9c185d7ddd5fd62fba3a5fd0b9993fe
Greg Sanders <gsanders87@gmail.com>
2025-02-06 11:34:56 -0500

    test: test_inv_block, use mocktime instead of waiting

--- test/functional/p2p_tx_download.py
+++ test/functional/p2p_tx_download.py
@@ -95,6 +95,8 @@ class TxDownloadTest(BGLTestFramework):
         tx = self.wallet.create_self_transfer()
         wtxid = int(tx['wtxid'], 16)
 
+        self.nodes[0].setmocktime(int(time.time()))
+
         self.log.info(
             "Announce the transaction to all nodes from all {} incoming peers, but never send it".format(NUM_INBOUND))
         msg = msg_inv([CInv(t=MSG_WTX, h=wtxid)])
@@ -115,7 +117,10 @@ class TxDownloadTest(BGLTestFramework):
         assert self.nodes[1].getpeerinfo()[0]['inbound'] == False
         timeout = 2 + NONPREF_PEER_TX_DELAY + GETDATA_TX_INTERVAL
         self.log.info("Tx should be received at node 1 after {} seconds".format(timeout))
-        self.sync_mempools(timeout=timeout)
+        self.nodes[0].bumpmocktime(timeout)
+        self.sync_mempools()
+
+        self.nodes[0].setmocktime(0)
 
     def test_in_flight_max(self):
         self.log.info("Test that we don't load peers with more than {} transaction requests immediately".format(MAX_PEER_TX_REQUEST_IN_FLIGHT))
