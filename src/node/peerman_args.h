4354a17bd2341fff7bacd6167093f5315042b62d
fanquake <fanquake@gmail.com>
2025-02-14 16:36:39 +0100

    doc: add & amend copyright headers

--- src/node/peerman_args.h
+++ src/node/peerman_args.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2023-present The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or https://opensource.org/license/mit.
+
 #ifndef BGL_NODE_PEERMAN_ARGS_H
 #define BGL_NODE_PEERMAN_ARGS_H
 
