972270960e920a9fbdbf3012ca58878375688086
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2025-02-18 12:56:59 +0100

    refactor: Remove IsArgSet guard when fallback value is provided
    Checking for IsArgSet before calling GetArg while providing the args
    default value as fallback is both confusing and fragile.
    
    It is confusing, because the provided fallback is dead code. So it would
    be better to just call GetArg without a fallback.
    
    However, ignoring the fallback value is fragile, because it would not be
    sanitized.
    
    Fix all issues by sanitizing the fallback value.

--- src/init.cpp
+++ src/init.cpp
@@ -1044,10 +1044,20 @@ bool AppInitParameterInteraction(const ArgsManager& args)
         }
     }
 
-    if (args.IsArgSet("-blockmaxweight")) {
+    {
         const auto max_block_weight = args.GetIntArg("-blockmaxweight", DEFAULT_BLOCK_MAX_WEIGHT);
         if (max_block_weight > MAX_BLOCK_WEIGHT) {
             return InitError(strprintf(_("Specified -blockmaxweight (%d) exceeds consensus maximum block weight (%d)"), max_block_weight, MAX_BLOCK_WEIGHT));
+        }
+    }
+
+    {
+        const auto block_reserved_weight = args.GetIntArg("-blockreservedweight", DEFAULT_BLOCK_RESERVED_WEIGHT);
+        if (block_reserved_weight > MAX_BLOCK_WEIGHT) {
+            return InitError(strprintf(_("Specified -blockreservedweight (%d) exceeds consensus maximum block weight (%d)"), block_reserved_weight, MAX_BLOCK_WEIGHT));
+        }
+        if (block_reserved_weight < MINIMUM_BLOCK_RESERVED_WEIGHT) {
+            return InitError(strprintf(_("Specified -blockreservedweight (%d) is lower than minimum safety value of (%d)"), block_reserved_weight, MINIMUM_BLOCK_RESERVED_WEIGHT));
         }
     }
 
