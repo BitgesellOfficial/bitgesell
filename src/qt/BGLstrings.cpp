ee5eea557b176b7f1e564e0358e846bc5298d863
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2025-02-20 17:04:16 +0000

    qt: Update `src/qt/locale/bitcoin_en.xlf` after string freeze
    Steps to reproduce the diff on Ubuntu:
    ```
    $ cmake --preset dev-mode -DWITH_USDT=OFF -DWITH_MULTIPROCESS=OFF
    $ cmake --build build_dev_mode --target translate
    ```

--- src/qt/BGLstrings.cpp
+++ src/qt/BGLstrings.cpp
@@ -169,6 +169,14 @@ QT_TRANSLATE_NOOP("BGL-core", ""
 "SQLiteDatabase: Unknown sqlite wallet schema version %d. Only version %d is "
 "supported"),
 QT_TRANSLATE_NOOP("BGL-core", ""
+"Specified -blockmaxweight (%d) exceeds consensus maximum block weight (%d)"),
+QT_TRANSLATE_NOOP("BGL-core", ""
+"Specified -blockreservedweight (%d) exceeds consensus maximum block weight "
+"(%d)"),
+QT_TRANSLATE_NOOP("BGL-core", ""
+"Specified -blockreservedweight (%d) is lower than minimum safety value of "
+"(%d)"),
+QT_TRANSLATE_NOOP("BGL-core", ""
 "The block database contains a block which appears to be from the future. "
 "This may be due to your computer's date and time being set incorrectly. Only "
 "rebuild the block database if you are sure that your computer's date and "
