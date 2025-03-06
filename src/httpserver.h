d7251c264d5f1450c4ed75cfbb4faaa0f695ac55
naftalimurgor <murgornaftali@gmail.com>
2025-03-06 16:53:58 +0300

    increase rpc HTTP threads to 16, assignable on init

--- src/httpserver.h
+++ src/httpserver.h
@@ -14,8 +14,18 @@ namespace util {
 class SignalInterrupt;
 } // namespace util
 
-static const int DEFAULT_HTTP_THREADS=4;
-static const int DEFAULT_HTTP_WORKQUEUE=16;
+/**
+ * The default value for `-rpcthreads`. This number of threads will be created at startup.
+* More reference: https://github.com/bitcoin/bitcoin/pull/31215/commits/e56fc7ce6a92eae7e80657d9f57a148cc002358d
+ **/
+static const int DEFAULT_HTTP_THREADS=16
+
+/**
+ * The default value for `-rpcworkqueue`. This is the maximum depth of the work queue,
+ * we don't allocate this number of work queue items upfront.
+ */
+static const int DEFAULT_HTTP_WORKQUEUE=64;
+
 static const int DEFAULT_HTTP_SERVER_TIMEOUT=30;
 
 struct evhttp_request;
