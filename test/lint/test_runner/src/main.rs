2f3efe4c6e64539f2fb21139746cfb668eab44a4
dennsikl <comf000747@gmail.com>
2025-03-26 09:07:11 +0200

    Remove needless borrow to fix Clippy warning

--- test/lint/test_runner/src/main.rs
+++ test/lint/test_runner/src/main.rs
@@ -240,7 +240,7 @@ fn lint_scripted_diff() -> LintResult {
 
 fn lint_commit_msg() -> LintResult {
     let mut good = true;
-    let commit_hashes = check_output(git().args(&[
+    let commit_hashes = check_output(git().args([
         "-c",
         "log.showSignature=false",
         "log",
