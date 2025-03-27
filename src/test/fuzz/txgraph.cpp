fb4de29755ec469082823772f93b1e32933d66fa
Pieter Wuille <pieter@wuille.net>
2025-03-27 10:01:51 -0400

    clusterlin: add GetConnectedComponent
    This abstracts out the finding of the connected component that includes
    a given element from FindConnectedComponent (which just finds any connected
    component).
    
    Use this in the txgraph fuzz test, which was effectively reimplementing this
    logic. At the same time, improve its performance by replacing a vector with a
    set.

--- src/test/fuzz/txgraph.cpp
+++ src/test/fuzz/txgraph.cpp
@@ -548,6 +548,37 @@ FUZZ_TARGET(txgraph)
                 // these here without making more calls to real, which could affect its internal
                 // state. A full comparison is done at the end.
                 break;
+            } else if (!sel_sim.IsOversized() && command-- == 0) {
+                // CountDistinctClusters.
+                std::vector<TxGraph::Ref*> refs;
+                // Gather a list of up to 15 (or up to 255) Ref pointers.
+                auto count = provider.ConsumeIntegralInRange<size_t>(0, alt ? 255 : 15);
+                refs.resize(count);
+                for (size_t i = 0; i < count; ++i) {
+                    refs[i] = pick_fn();
+                }
+                // Their order should not matter, shuffle them.
+                std::shuffle(refs.begin(), refs.end(), rng);
+                // Invoke the real function.
+                auto result = real->CountDistinctClusters(refs, use_main);
+                // Build a set with representatives of the clusters the Refs occur in in the
+                // simulated graph. For each, remember the lowest-index transaction SimPos in the
+                // cluster.
+                SimTxGraph::SetType sim_reps;
+                for (auto ref : refs) {
+                    // Skip Refs that do not occur in the simulated graph.
+                    auto simpos = sel_sim.Find(ref);
+                    if (simpos == SimTxGraph::MISSING) continue;
+                    // Find the component that includes ref.
+                    auto component = sel_sim.graph.GetConnectedComponent(sel_sim.graph.Positions(), simpos);
+                    // Remember the lowest-index SimPos in component, as a representative for it.
+                    assert(component.Any());
+                    sim_reps.Set(component.First());
+                }
+                // Compare the number of deduplicated representatives with the value returned by
+                // the real function.
+                assert(result == sim_reps.Count());
+                break;
             } else if (command-- == 0) {
                 // DoWork.
                 real->DoWork();
