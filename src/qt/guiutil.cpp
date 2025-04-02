ce7a4c12e5eb73ac48381974382f8fda499c5d76
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2025-04-02 20:48:36 +0100

    qt, refactor: Inline `GUIUtil::GetImage` function

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -973,15 +973,6 @@ bool HasPixmap(const QLabel* label)
     return !label->pixmap(Qt::ReturnByValue).isNull();
 }
 
-QImage GetImage(const QLabel* label)
-{
-    if (!HasPixmap(label)) {
-        return QImage();
-    }
-
-    return label->pixmap(Qt::ReturnByValue).toImage();
-}
-
 QString MakeHtmlLink(const QString& source, const QString& link)
 {
     return QString(source).replace(
