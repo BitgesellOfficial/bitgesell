3b97384d66b1e0d14c755a8f1c5990f666e4a19a
Ava Chow <github@achow101.com>
2025-04-10 14:22:20 -0700

    descriptor: Remove unused parent_info from BIP32PUbKeyProvider::GetPubKey

--- src/script/descriptor.cpp
+++ src/script/descriptor.cpp
@@ -399,16 +399,12 @@ public:
     size_t GetSize() const override { return 33; }
     std::optional<CPubKey> GetPubKey(int pos, const SigningProvider& arg, FlatSigningProvider& out, const DescriptorCache* read_cache = nullptr, DescriptorCache* write_cache = nullptr) const override
     {
-        // Info of parent of the to be derived pubkey
-        KeyOriginInfo parent_info;
+        KeyOriginInfo info;
         CKeyID keyid = m_root_extkey.pubkey.GetID();
-        std::copy(keyid.begin(), keyid.begin() + sizeof(parent_info.fingerprint), parent_info.fingerprint);
-        parent_info.path = m_path;
-
-        // Info of the derived key itself which is copied out upon successful completion
-        KeyOriginInfo final_info_out_tmp = parent_info;
-        if (m_derive == DeriveType::UNHARDENED) final_info_out_tmp.path.push_back((uint32_t)pos);
-        if (m_derive == DeriveType::HARDENED) final_info_out_tmp.path.push_back(((uint32_t)pos) | 0x80000000L);
+        std::copy(keyid.begin(), keyid.begin() + sizeof(info.fingerprint), info.fingerprint);
+        info.path = m_path;
+        if (m_derive == DeriveType::UNHARDENED) info.path.push_back((uint32_t)pos);
+        if (m_derive == DeriveType::HARDENED) info.path.push_back(((uint32_t)pos) | 0x80000000L);
 
         // Derive keys or fetch them from cache
         CExtPubKey final_extkey = m_root_extkey;
@@ -444,8 +440,8 @@ public:
         }
         if (!der) return std::nullopt;
 
-        out.origins.emplace(final_extkey.pubkey.GetID(), std::make_pair(final_extkey.pubkey, info));
-        out.pubkeys.emplace(final_extkey.pubkey.GetID(), final_extkey.pubkey);
+        final_info_out = info;
+        key_out = final_extkey.pubkey;
 
         if (write_cache) {
             // Only cache parent if there is any unhardened derivation
