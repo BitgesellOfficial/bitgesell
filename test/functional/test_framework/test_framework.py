bcb349b47032d0f717d692ce88d1cab6818ccd5b
Martin Zumsande <mzumsande@gmail.com>
2025-04-16 17:36:06 -0400

    test: add function to convert to json for height_or_hash params
    This is necessary for bitcoin-cli because a string without quotes
    is not a valid json.

--- test/functional/test_framework/test_framework.py
+++ test/functional/test_framework/test_framework.py
@@ -8,6 +8,7 @@ import configparser
 from enum import Enum
 import argparse
 from datetime import datetime, timezone
+import json
 import logging
 import os
 import platform
@@ -1083,3 +1084,8 @@ class BGLTestFramework(metaclass=BGLTestMetaClass):
 
     def has_blockfile(self, node, filenum: str):
         return (node.blocks_path/ f"blk{filenum}.dat").is_file()
+
+    def convert_to_json_for_cli(self, text):
+        if self.options.usecli:
+            return json.dumps(text)
+        return text
