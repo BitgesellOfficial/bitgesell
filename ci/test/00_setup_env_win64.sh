c5fd96f1dc7a1e32604fe81cf9a87d6ac64eb454
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2025-04-19 08:11:47 +0100

    ci: Drop no longer necessary `-Wno-error=array-bounds`

--- ci/test/00_setup_env_win64.sh
+++ ci/test/00_setup_env_win64.sh
@@ -22,4 +22,4 @@ export RUN_UNIT_TESTS=false
 export RUN_FUNCTIONAL_TESTS=false
 export GOAL="deploy"
 export BGL_CONFIG="-DREDUCE_EXPORTS=ON -DBUILD_GUI_TESTS=OFF \
--DCMAKE_CXX_FLAGS='-Wno-error=maybe-uninitialized -Wno-error=array-bounds'"
+-DCMAKE_CXX_FLAGS='-Wno-error=maybe-uninitialized'"
