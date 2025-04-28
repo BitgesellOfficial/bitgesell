4bc795cd3ae95349cbd1f16016f23afb90302e5b
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2025-04-28 10:59:07 +0200

    test: Force named args for RPCOverloadWrapper optional args
    This can avoid bugs and makes the test code easier to read, because the
    order of positional args does not have to be known or assumed.

--- test/functional/wallet_labels.py
+++ test/functional/wallet_labels.py
@@ -50,7 +50,7 @@ class WalletLabelsTest(BGLTestFramework):
         assert_equal(response[0]['error']['message'], "Invalid label name")
 
         for rpc_call in rpc_calls:
-            assert_raises_rpc_error(-11, "Invalid label name", *rpc_call, "*")
+            assert_raises_rpc_error(-11, "Invalid label name", *rpc_call, label="*")
 
     def run_test(self):
         # Check that there's no UTXO on the node
