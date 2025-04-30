7beb2e8b945ee235cb7ff3c45d294e33b7fced71
Sjors Provoost <sjors@sprovoost.nl>
2025-04-30 14:47:33 +0200

    scripted-diff: rename gbt_force and gbt_force_name
    The term "force" is ambiguous and not used in BIP9 where the ! rule
    prefix is introduced.
    
    Additionally, #29039 renamed gbt_vb_name to gbt_force_name which
    might increase the confusion.
    
    -BEGIN VERIFY SCRIPT-
    sed -i s/gbt_force_name/gbt_rule_value/g ./src/rpc/mining.cpp
    sed -i s/gbt_force/gbt_optional_rule/g $(git grep -l gbt_force)
    -END VERIFY SCRIPT-

--- src/deploymentinfo.h
+++ src/deploymentinfo.h
@@ -14,7 +14,7 @@ struct VBDeploymentInfo {
     /** Deployment name */
     const char *name;
     /** Whether GBT clients can safely ignore this rule in simplified usage */
-    bool gbt_force;
+    bool gbt_optional_rule;
 };
 
 extern const VBDeploymentInfo VersionBitsDeploymentInfo[Consensus::MAX_VERSION_BITS_DEPLOYMENTS];
