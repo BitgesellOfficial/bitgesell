c7f7c10033522b1ccb3dec2dc140ce1442c39e8f
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2025-05-07 17:32:12 +0200

    Remove unused LegacyDataSPKM::DeleteRecords()

--- src/wallet/scriptpubkeyman.h
+++ src/wallet/scriptpubkeyman.h
@@ -266,7 +266,6 @@ public:
      * Does not modify this ScriptPubKeyMan. */
     std::optional<MigrationData> MigrateToDescriptor();
     /** Delete all the records of this LegacyScriptPubKeyMan from disk*/
-    bool DeleteRecords();
     bool DeleteRecordsWithDB(WalletBatch& batch);
 };
 
