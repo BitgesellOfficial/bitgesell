bc88b0a78f9027ca489cbbcdad3ffdc49cb7a2a9
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2025-05-12 17:11:27 +0100

    doc: Fix typo
    An ellipsis (…) is typically used in such cases.

--- src/init.cpp
+++ src/init.cpp
@@ -1843,7 +1843,7 @@ bool AppInitMain(NodeContext& node, interfaces::BlockAndHeaderTipInfo* tip_info)
 
         // Start indexes initial sync
         if (!StartIndexBackgroundSync(node)) {
-            bilingual_str err_str = _("Failed to start indexes, shutting down..");
+            bilingual_str err_str = _("Failed to start indexes, shutting down…");
             chainman.GetNotifications().fatalError(err_str);
             return;
         }
