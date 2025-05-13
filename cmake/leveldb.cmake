4e76836869c37dc44903a0583ec6285cb1a011b6
fanquake <fanquake@gmail.com>
2025-05-13 16:41:09 +0100

    build: remove Wsuggest-override suppression from leveldb build

--- cmake/leveldb.cmake
+++ cmake/leveldb.cmake
@@ -87,9 +87,8 @@ if(MSVC)
     _CRT_NONSTDC_NO_WARNINGS
   )
 else()
-  target_compile_options(nowarn_leveldb_interface INTERFACE
-    -Wno-conditional-uninitialized
-    -Wno-suggest-override
+  try_append_cxx_flags("-Wconditional-uninitialized" TARGET nowarn_leveldb_interface SKIP_LINK
+    IF_CHECK_PASSED "-Wno-conditional-uninitialized"
   )
 endif()
 
