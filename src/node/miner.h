041f57f8e34f562cae006a8b29eb1fb1a36d1a97
Sjors Provoost <sjors@sprovoost.nl>
2025-05-14 09:51:15 +0200

    interfaces: move getTip implementation to miner

--- src/node/miner.h
+++ src/node/miner.h
@@ -6,6 +6,7 @@
 #ifndef BGL_NODE_MINER_H
 #define BGL_NODE_MINER_H
 
+#include <interfaces/types.h>
 #include <node/types.h>
 #include <policy/policy.h>
 #include <primitives/block.h>
@@ -30,6 +31,8 @@ class Chainstate;
 class ChainstateManager;
 
 namespace Consensus { struct Params; };
+
+using interfaces::BlockRef;
 
 namespace node {
 class KernelNotifications;
@@ -234,6 +237,20 @@ void ApplyArgsManOptions(const ArgsManager& gArgs, BlockAssembler::Options& opti
 
 /* Compute the block's merkle root, insert or replace the coinbase transaction and the merkle root into the block */
 void AddMerkleRootAndCoinbase(CBlock& block, CTransactionRef coinbase, uint32_t version, uint32_t timestamp, uint32_t nonce);
+
+/**
+ * Return a new block template when fees rise to a certain threshold or after a
+ * new tip; return nullopt if timeout is reached.
+ */
+std::unique_ptr<CBlockTemplate> WaitAndCreateNewBlock(ChainstateManager& chainman,
+                                                      KernelNotifications& kernel_notifications,
+                                                      CTxMemPool* mempool,
+                                                      const std::unique_ptr<CBlockTemplate>& block_template,
+                                                      const BlockWaitOptions& options,
+                                                      const BlockAssembler::Options& assemble_options);
+
+/* Locks cs_main and returns the block hash and block height of the active chain if it exists; otherwise, returns nullopt.*/
+std::optional<BlockRef> GetTip(ChainstateManager& chainman);
 } // namespace node
 
 #endif // BGL_NODE_MINER_H
