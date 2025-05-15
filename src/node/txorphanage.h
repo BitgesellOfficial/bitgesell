e1388f906686b28a5458a7045084205a1584a012
glozow <gloriajzhao@gmail.com>
2025-05-15 09:59:23 -0400

    [prep/rpc] remove entry and expiry time from getorphantxs
    Expiry is going away in a later commit.
    This is only an RPC change. Behavior of the orphanage does not change.
    Note that getorphantxs is marked experimental.

--- src/node/txorphanage.h
+++ src/node/txorphanage.h
@@ -82,7 +82,6 @@ public:
         CTransactionRef tx;
         /** Peers added with AddTx or AddAnnouncer. */
         std::set<NodeId> announcers;
-        NodeSeconds nTimeExpire;
 
         /** Get the weight of this transaction, an approximation of its memory usage. */
         unsigned int GetUsage() const {
@@ -110,6 +109,7 @@ public:
 
 protected:
     struct OrphanTx : public OrphanTxBase {
+        NodeSeconds nTimeExpire;
         size_t list_pos;
     };
 
