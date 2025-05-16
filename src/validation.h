32aac6bac12723996f6ac70e950e7faf97ce4052
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2025-05-16 13:32:23 +0200

    rpc: Round verificationprogress to exactly 1 for a recent tip
    This requires a new lock annotation, but all relevant callers already
    held the lock.

--- src/validation.h
+++ src/validation.h
@@ -1146,7 +1146,7 @@ public:
     bool IsInitialBlockDownload() const;
 
     /** Guess verification progress (as a fraction between 0.0=genesis and 1.0=current tip). */
-    double GuessVerificationProgress(const CBlockIndex* pindex) const;
+    double GuessVerificationProgress(const CBlockIndex* pindex) const EXCLUSIVE_LOCKS_REQUIRED(GetMutex());
 
     /**
      * Import blocks from an external file
