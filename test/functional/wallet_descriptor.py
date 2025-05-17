6977fbb209d569e3fa1293ec4b72b27193b142fe
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2025-05-17 18:14:24 +0200

    scripted-diff: test: remove 'descriptors=True' argument for `createwallet` calls
    Descriptor wallets are already created by default since v23.0, but
    since the recent legacy wallet removal this parameter *must* be True
    (see commit 9f04e02ffaee0fe64027dc56c7bea3885254321a), i.e. still
    passing it wouldn't contain any information for test readers
    anymore. So simply drop them in the functional tests in order to
    reduce code bloat.
    
    -BEGIN VERIFY SCRIPT-
    sed -i 's/, descriptors=True//g' $(git ls-files -- 'test/functional' ':(exclude)test/functional/wallet_backwards_compatibility.py')
    sed -i '/descriptors=True,/d' ./test/functional/mempool_persist.py
    -END VERIFY SCRIPT-

--- test/functional/wallet_descriptor.py
+++ test/functional/wallet_descriptor.py
@@ -33,7 +33,7 @@ class WalletDescriptorTest(BGLTestFramework):
     def run_test(self):
         # Make a descriptor wallet
         self.log.info("Making a descriptor wallet")
-        self.nodes[0].createwallet(wallet_name="desc1", descriptors=True)
+        self.nodes[0].createwallet(wallet_name="desc1")
 
         # A descriptor wallet should have 100 addresses * 4 types = 400 keys
         self.log.info("Checking wallet info")
@@ -87,7 +87,7 @@ class WalletDescriptorTest(BGLTestFramework):
         assert_equal(addr_info['hdkeypath'], 'm/86h/1h/0h/1/0')
 
         # Make a wallet to receive coins at
-        self.nodes[0].createwallet(wallet_name="desc2", descriptors=True)
+        self.nodes[0].createwallet(wallet_name="desc2")
         recv_wrpc = self.nodes[0].get_wallet_rpc("desc2")
         send_wrpc = self.nodes[0].get_wallet_rpc("desc1")
 
@@ -137,19 +137,19 @@ class WalletDescriptorTest(BGLTestFramework):
         enc_rpc.getnewaddress() # Makes sure that we can get a new address from a born encrypted wallet
 
         self.log.info("Test blank descriptor wallets")
-        self.nodes[0].createwallet(wallet_name='desc_blank', blank=True, descriptors=True)
+        self.nodes[0].createwallet(wallet_name='desc_blank', blank=True)
         blank_rpc = self.nodes[0].get_wallet_rpc('desc_blank')
         assert_raises_rpc_error(-4, 'This wallet has no available keys', blank_rpc.getnewaddress)
 
         self.log.info("Test descriptor wallet with disabled private keys")
-        self.nodes[0].createwallet(wallet_name='desc_no_priv', disable_private_keys=True, descriptors=True)
+        self.nodes[0].createwallet(wallet_name='desc_no_priv', disable_private_keys=True)
         nopriv_rpc = self.nodes[0].get_wallet_rpc('desc_no_priv')
         assert_raises_rpc_error(-4, 'This wallet has no available keys', nopriv_rpc.getnewaddress)
 
         self.log.info("Test descriptor exports")
-        self.nodes[0].createwallet(wallet_name='desc_export', descriptors=True)
+        self.nodes[0].createwallet(wallet_name='desc_export')
         exp_rpc = self.nodes[0].get_wallet_rpc('desc_export')
-        self.nodes[0].createwallet(wallet_name='desc_import', disable_private_keys=True, descriptors=True)
+        self.nodes[0].createwallet(wallet_name='desc_import', disable_private_keys=True)
         imp_rpc = self.nodes[0].get_wallet_rpc('desc_import')
 
         addr_types = [('legacy', False, 'pkh(', '44h/1h/0h', -13),
@@ -206,7 +206,7 @@ class WalletDescriptorTest(BGLTestFramework):
                 assert_equal(exp_addr, imp_addr)
 
         self.log.info("Test that loading descriptor wallet containing legacy key types throws error")
-        self.nodes[0].createwallet(wallet_name="crashme", descriptors=True)
+        self.nodes[0].createwallet(wallet_name="crashme")
         self.nodes[0].unloadwallet("crashme")
         wallet_db = self.nodes[0].wallets_path / "crashme" / self.wallet_data_filename
         conn = sqlite3.connect(wallet_db)
