52b3fa2a62176f49fe94687d88c33c5dfca72079
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2025-05-18 16:15:46 +0100

    cmake: Remove `ENABLE_ARM_SHANI` from `bitcoin-build-config.h`
    `ENABLE_ARM_SHANI` is already conditionally defined for the
    `bitcoin_crypto` target, so defining it in `bitcoin-build-config.h` is
    redundant.

--- test/lint/test_runner/src/main.rs
+++ test/lint/test_runner/src/main.rs
@@ -549,13 +549,7 @@ fn lint_includes_build_config() -> LintResult {
                     "*.cpp",
                     "*.h",
                 ])
-                .args(get_pathspecs_default_excludes())
-                .args([
-                    // These are exceptions which don't use bitcoin-build-config.h, rather CMakeLists.txt adds
-                    // these cppflags manually.
-                    ":(exclude)src/crypto/sha256_arm_shani.cpp",
-                    ":(exclude)src/crypto/sha256_x86_shani.cpp",
-                ]),
+                .args(get_pathspecs_default_excludes()),
         )
         .expect("grep failed");
         git()
