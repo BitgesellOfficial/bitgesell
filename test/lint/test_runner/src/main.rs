0a24adf5f69deaea42e408ae8e923dda609faa4a
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2025-05-18 16:13:32 +0100

    cmake: Remove `ENABLE_X86_SHANI` from `bitcoin-build-config.h`
    `ENABLE_X86_SHANI` is already conditionally defined for the
    `bitcoin_crypto` target, so defining it in `bitcoin-build-config.h` is
    redundant.

--- test/lint/test_runner/src/main.rs
+++ test/lint/test_runner/src/main.rs
@@ -29,7 +29,7 @@ fn get_linter_list() -> Vec<&'static Linter> {
             lint_fn: lint_doc
         },
         &Linter {
-            description: "Check that no symbol from bitcoin-build-config.h is used without the header being included",
+            description: "Check that no symbol from BGL-build-config.h is used without the header being included",
             name: "includes_build_config",
             lint_fn: lint_includes_build_config
         },
@@ -411,7 +411,7 @@ fn get_pathspecs_exclude_whitespace() -> Vec<String> {
             "contrib/windeploy/win-codesign.cert",
             "doc/README_windows.txt",
             // Temporary excludes, or existing violations
-            "contrib/init/bitcoind.openrc",
+            "contrib/init/BGLd.openrc",
             "contrib/macdeploy/macdeployqtplus",
             "src/crypto/sha256_sse4.cpp",
             "src/qt/res/src/*.svg",
@@ -517,7 +517,7 @@ Please add any false positives, such as subtrees, or externally sourced files to
 }
 
 fn lint_includes_build_config() -> LintResult {
-    let config_path = "./cmake/bitcoin-build-config.h.in";
+    let config_path = "./cmake/BGL-build-config.h.in";
     let defines_regex = format!(
         r"^\s*(?!//).*({})",
         check_output(Command::new("grep").args(["define", "--", config_path]))
@@ -554,8 +554,6 @@ fn lint_includes_build_config() -> LintResult {
                     // These are exceptions which don't use bitcoin-build-config.h, rather CMakeLists.txt adds
                     // these cppflags manually.
                     ":(exclude)src/crypto/sha256_arm_shani.cpp",
-                    ":(exclude)src/crypto/sha256_avx2.cpp",
-                    ":(exclude)src/crypto/sha256_x86_shani.cpp",
                 ]),
         )
         .expect("grep failed");
@@ -568,9 +566,9 @@ fn lint_includes_build_config() -> LintResult {
                     "--files-with-matches"
                 },
                 if mode {
-                    "^#include <bitcoin-build-config.h> // IWYU pragma: keep$"
+                    "^#include <BGL-build-config.h> // IWYU pragma: keep$"
                 } else {
-                    "#include <bitcoin-build-config.h>" // Catch redundant includes with and without the IWYU pragma
+                    "#include <BGL-build-config.h>" // Catch redundant includes with and without the IWYU pragma
                 },
                 "--",
             ])
@@ -584,11 +582,11 @@ fn lint_includes_build_config() -> LintResult {
         return Err(format!(
             r#"
 ^^^
-One or more files use a symbol declared in the bitcoin-build-config.h header. However, they are not
+One or more files use a symbol declared in the BGL-build-config.h header. However, they are not
 including the header. This is problematic, because the header may or may not be indirectly
 included. If the indirect include were to be intentionally or accidentally removed, the build could
 still succeed, but silently be buggy. For example, a slower fallback algorithm could be picked,
-even though bitcoin-build-config.h indicates that a faster feature is available and should be used.
+even though BGL-build-config.h indicates that a faster feature is available and should be used.
 
 If you are unsure which symbol is used, you can find it with this command:
 git grep --perl-regexp '{}' -- file_name
@@ -596,7 +594,7 @@ git grep --perl-regexp '{}' -- file_name
 Make sure to include it with the IWYU pragma. Otherwise, IWYU may falsely instruct to remove the
 include again.
 
-#include <bitcoin-build-config.h> // IWYU pragma: keep
+#include <BGL-build-config.h> // IWYU pragma: keep
             "#,
             defines_regex
         ));
@@ -605,7 +603,7 @@ include again.
     if redundant {
         return Err(r#"
 ^^^
-None of the files use a symbol declared in the bitcoin-build-config.h header. However, they are including
+None of the files use a symbol declared in the BGL-build-config.h header. However, they are including
 the header. Consider removing the unused include.
             "#
         .to_string());
