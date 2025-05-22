262c473cea5934eab8d3fab0026a8471a6ddfe23
Greg Sanders <gsanders87@gmail.com>
2025-05-22 10:30:13 -0400

    test: fix block tests of invalid_txs

--- test/functional/feature_block.py
+++ test/functional/feature_block.py
@@ -179,6 +179,11 @@ class FullBlockTest(BGLTestFramework):
         for TxTemplate in invalid_txs.iter_all_templates():
             template = TxTemplate(spend_tx=attempt_spend_tx)
 
+            # belt-and-suspenders checking we won't pass up validating something
+            # we expect a disconnect from
+            if template.expect_disconnect:
+                assert not template.valid_in_block
+
             if template.valid_in_block:
                 continue
 
