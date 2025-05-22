7dd687f976da2180b526ffb5471d1adc39b1a264
Greg Sanders <gsanders87@gmail.com>
2025-05-22 10:43:44 -0400

    test: ensure reason is checked for invalid blocks rejection

--- test/functional/feature_block.py
+++ test/functional/feature_block.py
@@ -187,6 +187,8 @@ class FullBlockTest(BGLTestFramework):
             if template.valid_in_block:
                 continue
 
+            assert template.block_reject_reason or template.reject_reason
+
             self.log.info(f"Reject block with invalid tx: {TxTemplate.__name__}")
             blockname = f"for_invalid.{TxTemplate.__name__}"
             badblock = self.next_block(blockname)
