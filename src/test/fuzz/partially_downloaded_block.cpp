1704f4292ecf96e3aa1fe956576d449227bee66e
Greg Sanders <gsanders87@gmail.com>
2025-06-03 10:29:00 -0400

    p2p: remove vestigial READ_STATUS_CHECKBLOCK_FAILED

--- src/test/fuzz/partially_downloaded_block.cpp
+++ src/test/fuzz/partially_downloaded_block.cpp
@@ -141,7 +141,6 @@ FUZZ_TARGET(partially_downloaded_block, .init = initialize_pdb)
         assert(!fail_check_block);
         assert(block->GetHash() == reconstructed_block.GetHash());
         break;
-    case READ_STATUS_CHECKBLOCK_FAILED: [[fallthrough]];
     case READ_STATUS_FAILED:
         assert(fail_check_block);
         break;
