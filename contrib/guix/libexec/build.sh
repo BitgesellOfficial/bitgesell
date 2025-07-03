a558e1661a66aeb4d46a2ed3c0dffc150f31bf4e
fanquake <fanquake@gmail.com>
2025-07-03 13:51:42 +0100

    guix: configure with -Werror=dev

--- contrib/guix/libexec/build.sh
+++ contrib/guix/libexec/build.sh
@@ -240,6 +240,7 @@ mkdir -p "$DISTSRC"
     cmake -S . -B build \
           --toolchain "${BASEPREFIX}/${HOST}/toolchain.cmake" \
           -DWITH_CCACHE=OFF \
+          -Werror=dev \
           ${CONFIGFLAGS}
 
     # Build Bitcoin Core
