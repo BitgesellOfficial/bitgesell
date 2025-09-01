hebasto/bitcoin

cmake: Set top-level target output locations
This change:
1. Collects build artifacts in dedicated locations.
2. Allows running bitcoin-chainstate.exe with bitcoinkernel.dll directly
from the build tree on Windows.

contrib/devtools/check-deps.sh
@@ -8,10 +8,10 @@ declare -A LIBS
 LIBS[cli]="libbitcoin_cli.a"
 LIBS[common]="libbitcoin_common.a"
 LIBS[consensus]="libbitcoin_consensus.a"
-LIBS[crypto]="crypto/libbitcoin_crypto.a"
+LIBS[crypto]="libbitcoin_crypto.a"
 LIBS[node]="libbitcoin_node.a"
-LIBS[util]="util/libbitcoin_util.a"
-LIBS[wallet]="wallet/libbitcoin_wallet.a"
+LIBS[util]="libbitcoin_util.a"
+LIBS[wallet]="libbitcoin_wallet.a"
 
 # Declare allowed dependencies "X Y" where X is allowed to depend on Y. This
 # list is taken from doc/design/libraries.md.
@@ -187,7 +187,7 @@ fi
 # shellcheck disable=SC2046
 cmake --build "$BUILD_DIR" -j"$(nproc)" -t $(lib_targets)
 TEMP_DIR="$(mktemp -d)"
-cd "$BUILD_DIR/src"
+cd "$BUILD_DIR/lib"
 extract_symbols "$TEMP_DIR"
 if check_libraries "$TEMP_DIR"; then
     echo "Success! No unexpected dependencies were detected."

contrib/devtools/gen-manpages.py
@@ -9,12 +9,12 @@ import tempfile
 import argparse
 
 BINARIES = [
-'src/bitcoind',
-'src/bitcoin-cli',
-'src/bitcoin-tx',
-'src/bitcoin-wallet',
-'src/bitcoin-util',
-'src/qt/bitcoin-qt',
+'bin/bitcoind',
+'bin/bitcoin-cli',
+'bin/bitcoin-tx',
+'bin/bitcoin-wallet',
+'bin/bitcoin-util',
+'bin/bitcoin-qt',
 ]
 
 parser = argparse.ArgumentParser(

BitgesellOfficial/bitgesell

contrib/devtools/check-deps.sh
@@ -8,10 +8,10 @@ declare -A LIBS
 LIBS[cli]="libBGL_cli.a"
 LIBS[common]="libBGL_common.a"
 LIBS[consensus]="libBGL_consensus.a"
-LIBS[crypto]="crypto/libBGL_crypto.a"
+LIBS[crypto]="libBGL_crypto.a"
 LIBS[node]="libBGL_node.a"
-LIBS[util]="util/libBGL_util.a"
-LIBS[wallet]="wallet/libBGL_wallet.a"
+LIBS[util]="libBGL_util.a"
+LIBS[wallet]="libBGL_wallet.a"
 
 # Declare allowed dependencies "X Y" where X is allowed to depend on Y. This
 # list is taken from doc/design/libraries.md.
@@ -187,7 +187,7 @@ fi
 # shellcheck disable=SC2046
 cmake --build "$BUILD_DIR" -j"$(nproc)" -t $(lib_targets)
 TEMP_DIR="$(mktemp -d)"
-cd "$BUILD_DIR/src"
+cd "$BUILD_DIR/lib"
 extract_symbols "$TEMP_DIR"
 if check_libraries "$TEMP_DIR"; then
     echo "Success! No unexpected dependencies were detected."

contrib/devtools/gen-manpages.py
@@ -9,12 +9,12 @@ import tempfile
 import argparse
 
 BINARIES = [
-'src/BGLd',
-'src/BGL-cli',
-'src/BGL-tx',
-'src/BGL-wallet',
-'src/BGL-util',
-'src/qt/BGL-qt',
+'bin/BGLd',
+'bin/BGL-cli',
+'bin/BGL-tx',
+'bin/BGL-wallet',
+'bin/BGL-util',
+'bin/BGL-qt',
 ]
 
 parser = argparse.ArgumentParser(