ryanofsky/bitcoin

ci: Run multiprocess tests through wrapper executable

ci/test/00_setup_env_i686_multiprocess.sh
@@ -20,4 +20,4 @@ export BITCOIN_CONFIG="\
  -DCMAKE_CXX_FLAGS='-Wno-error=documentation' \
  -DAPPEND_CPPFLAGS='-DBOOST_MULTI_INDEX_ENABLE_SAFE_MODE' \
 "
-export BITCOIND=bitcoin-node  # Used in functional tests
+export BITCOIN_CMD="bitcoin -m" # Used in functional tests

BitgesellOfficial/bitgesell

ci/test/00_setup_env_i686_multiprocess.sh
@@ -20,4 +20,4 @@ export BGL_CONFIG="\
  -DCMAKE_CXX_COMPILER='clang++;-m32' \
  -DAPPEND_CPPFLAGS='-DBOOST_MULTI_INDEX_ENABLE_SAFE_MODE' \
 "
-export BGLD=BGL-node  # Used in functional tests
+export BGL_CMD="BGL -m" # Used in functional tests
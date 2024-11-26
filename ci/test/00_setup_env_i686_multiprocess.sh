3118d585ab564d9f64d543ecd21202b607135a2c
Ryan Ofsky <ryan@ofsky.org>
2024-11-26 11:58:16 -0500

    ci: Run multiprocess tests through wrapper executable

--- ci/test/00_setup_env_i686_multiprocess.sh
+++ ci/test/00_setup_env_i686_multiprocess.sh
@@ -20,4 +20,4 @@ export BGL_CONFIG="\
  -DCMAKE_CXX_COMPILER='clang++;-m32' \
  -DAPPEND_CPPFLAGS='-DBOOST_MULTI_INDEX_ENABLE_SAFE_MODE' \
 "
-export BGLD=BGL-node  # Used in functional tests
+export BGL_CMD="BGL -m" # Used in functional tests
