bitcoin-ghost/bitcoin-core-ladder-script

ci: Downgrade DEBUG=1 to -D_GLIBCXX_ASSERTIONS in centos task
to work around https://github.com/bitcoin/bitcoin/issues/32524#issuecomment-2900903169

ci/test/00_setup_env_native_centos.sh
@@ -10,6 +10,11 @@ export CONTAINER_NAME=ci_native_centos
 export CI_IMAGE_NAME_TAG="quay.io/centos/centos:stream10"
 export CI_BASE_PACKAGES="gcc-c++ glibc-devel libstdc++-devel ccache make ninja-build git python3 python3-pip which patch xz procps-ng ksh rsync coreutils bison e2fsprogs cmake"
 export PIP_PACKAGES="pyzmq"
-export DEP_OPTS="DEBUG=1"
 export GOAL="install"
-export BITCOIN_CONFIG="-DWITH_ZMQ=ON -DBUILD_GUI=ON -DREDUCE_EXPORTS=ON -DCMAKE_BUILD_TYPE=Debug"
+export BITCOIN_CONFIG="\
+ -DWITH_ZMQ=ON \
+ -DBUILD_GUI=ON \
+ -DREDUCE_EXPORTS=ON \
+ -DAPPEND_CPPFLAGS='-D_GLIBCXX_ASSERTIONS' \
+ -DCMAKE_BUILD_TYPE=Debug \
+"

BitgesellOfficial/bitgesell

ci/test/00_setup_env_native_centos.sh
@@ -10,6 +10,11 @@ export CONTAINER_NAME=ci_native_centos
 export CI_IMAGE_NAME_TAG="quay.io/centos/centos:stream10"
 export CI_BASE_PACKAGES="gcc-c++ glibc-devel libstdc++-devel ccache make ninja-build git python3 python3-pip which patch xz procps-ng ksh rsync coreutils bison e2fsprogs cmake"
 export PIP_PACKAGES="pyzmq"
-export DEP_OPTS="DEBUG=1"
 export GOAL="install"
-export BGL_CONFIG="-DWITH_ZMQ=ON -DBUILD_GUI=ON -DREDUCE_EXPORTS=ON -DCMAKE_BUILD_TYPE=Debug"
+export BGL_CONFIG="\
+ -DWITH_ZMQ=ON \
+ -DBUILD_GUI=ON \
+ -DREDUCE_EXPORTS=ON \
+ -DAPPEND_CPPFLAGS='-D_GLIBCXX_ASSERTIONS' \
+ -DCMAKE_BUILD_TYPE=Debug \
+"