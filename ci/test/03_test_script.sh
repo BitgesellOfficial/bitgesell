bitcoin/bitcoin

ci: enable -Werror=dev
Turn developer & deprecation warnings into errors.

ci/test/03_test_script.sh
@@ -118,7 +118,7 @@ BASE_BUILD_DIR=${BASE_BUILD_DIR:-$BASE_SCRATCH_DIR/build-$HOST}
 mkdir -p "${BASE_BUILD_DIR}"
 cd "${BASE_BUILD_DIR}"
 
-BITCOIN_CONFIG_ALL="$BITCOIN_CONFIG_ALL -DCMAKE_INSTALL_PREFIX=$BASE_OUTDIR"
+BITCOIN_CONFIG_ALL="$BITCOIN_CONFIG_ALL -DCMAKE_INSTALL_PREFIX=$BASE_OUTDIR -Werror=dev"
 
 if [[ "${RUN_TIDY}" == "true" ]]; then
   BITCOIN_CONFIG_ALL="$BITCOIN_CONFIG_ALL -DCMAKE_EXPORT_COMPILE_COMMANDS=ON"

BitgesellOfficial/bitgesell

ci/test/03_test_script.sh
@@ -116,7 +116,7 @@ PRINT_CCACHE_STATISTICS="ccache --version | head -n 1 && ccache --show-stats"
 # Folder where the build is done.
 BASE_BUILD_DIR=${BASE_BUILD_DIR:-$BASE_SCRATCH_DIR/build-$HOST}
 
-BGL_CONFIG_ALL="$BGL_CONFIG_ALL -DCMAKE_INSTALL_PREFIX=$BASE_OUTDIR"
+BGL_CONFIG_ALL="$BGL_CONFIG_ALL -DCMAKE_INSTALL_PREFIX=$BASE_OUTDIR -Werror=dev"
 
 if [[ "${RUN_TIDY}" == "true" ]]; then
   BGL_CONFIG_ALL="$BGL_CONFIG_ALL -DCMAKE_EXPORT_COMPILE_COMMANDS=ON"