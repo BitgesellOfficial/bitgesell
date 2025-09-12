bitcoin/bitcoin

txgraph: make number of acceptable iterations configurable (feature)

src/test/fuzz/txgraph.cpp
@@ -269,9 +269,11 @@ FUZZ_TARGET(txgraph)
     auto max_count = provider.ConsumeIntegralInRange<DepGraphIndex>(1, MAX_CLUSTER_COUNT_LIMIT);
     // And the maximum combined size of transactions per cluster.
     auto max_size = provider.ConsumeIntegralInRange<uint64_t>(1, 0x3fffff * MAX_CLUSTER_COUNT_LIMIT);
+    // And the number of iterations to consider a cluster acceptably linearized.
+    auto acceptable_iters = provider.ConsumeIntegralInRange<uint64_t>(0, 10000);
 
     // Construct a real graph, and a vector of simulated graphs (main, and possibly staging).
-    auto real = MakeTxGraph(max_count, max_size);
+    auto real = MakeTxGraph(max_count, max_size, acceptable_iters);
     std::vector<SimTxGraph> sims;
     sims.reserve(2);
     sims.emplace_back(max_count, max_size);

BitgesellOfficial/bitgesell

src/test/fuzz/txgraph.cpp
@@ -311,11 +311,11 @@ FUZZ_TARGET(txgraph)
      *  on the individual size of a transaction (but this restriction will be lifted in a future
      *  commit). */
     auto max_cluster_size = provider.ConsumeIntegralInRange<uint64_t>(1, 0x3fffff * MAX_CLUSTER_COUNT_LIMIT);
-    /** The maximum individual transaction size used in this test (not a TxGraph parameter). */
-    auto max_tx_size = std::min<uint64_t>(0x3fffff, max_cluster_size);
+    /** The number of iterations to consider a cluster acceptably linearized. */
+    auto acceptable_iters = provider.ConsumeIntegralInRange<uint64_t>(0, 10000);
 
     // Construct a real graph, and a vector of simulated graphs (main, and possibly staging).
-    auto real = MakeTxGraph(max_cluster_count, max_cluster_size);
+    auto real = MakeTxGraph(max_cluster_count, max_cluster_size, acceptable_iters);
     std::vector<SimTxGraph> sims;
     sims.reserve(2);
     sims.emplace_back(max_cluster_count, max_cluster_size);