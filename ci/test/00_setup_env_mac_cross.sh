7d8390a81a5c06d1dc6f15279b1be5069b6a2a1d
William Joy <william@vivelajoye.com>
2026-05-15 23:34:15 -0700

    ci: use BGL config variables

--- ci/test/00_setup_env_mac_cross.sh
+++ ci/test/00_setup_env_mac_cross.sh
@@ -17,4 +17,4 @@ export XCODE_BUILD_ID=15A240d
 export RUN_UNIT_TESTS=false
 export RUN_FUNCTIONAL_TESTS=false
 export GOAL="deploy"
-export BITCOIN_CONFIG="--with-gui --enable-reduce-exports"
+export BGL_CONFIG="--with-gui --enable-reduce-exports"
