b08ca4f8309d946cbd19ae8adaf1d688f4faf7cc
Orange Studio <orange@blllll.cn>
2026-05-16 14:50:54 +0800

    ci: guard Bitgesell default datadir

--- ci/test/03_test_script.sh
+++ ci/test/03_test_script.sh
@@ -82,9 +82,9 @@ fi
 
 # Make sure default datadir does not exist and is never read by creating a dummy file
 if [ "$CI_OS_NAME" == "macos" ]; then
-  echo > "${HOME}/Library/Application Support/Bitcoin"
+  echo > "${HOME}/Library/Application Support/BGL"
 else
-  echo > "${HOME}/.bitcoin"
+  echo > "${HOME}/.BGL"
 fi
 
 if [ -z "$NO_DEPENDS" ]; then
