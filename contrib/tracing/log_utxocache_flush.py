4dc764bfa348871e1ab2736646d46603ba468109
pinehill99 <pinehill99@users.noreply.github.com>
2026-05-29 05:50:02 +0900

    doc: align tracing examples with BGLd

--- contrib/tracing/log_utxocache_flush.py
+++ contrib/tracing/log_utxocache_flush.py
@@ -10,7 +10,7 @@ from bcc import BPF, USDT
 """Example logging BGL Core utxo set cache flushes utilizing
     the utxocache:flush tracepoint."""
 
-# USAGE:  ./contrib/tracing/log_utxocache_flush.py path/to/bitcoind
+# USAGE:  ./contrib/tracing/log_utxocache_flush.py path/to/BGLd
 
 # BCC: The C program to be compiled to an eBPF program (by BCC) and loaded into
 # a sandboxed Linux kernel VM.
--- contrib/tracing/mempool_monitor.py
+++ contrib/tracing/mempool_monitor.py
@@ -3,7 +3,7 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-""" Example logging Bitcoin Core mempool events using the mempool:added,
+""" Example logging BGL Core mempool events using the mempool:added,
     mempool:removed, mempool:replaced, and mempool:rejected tracepoints. """
 
 import curses
@@ -114,16 +114,16 @@ int trace_replaced(struct pt_regs *ctx) {
 """
 
 
-def main(bitcoind_path):
-    bitcoind_with_usdts = USDT(path=str(bitcoind_path))
+def main(BGLd_path):
+    BGLd_with_usdts = USDT(path=str(BGLd_path))
 
     # attaching the trace functions defined in the BPF program
     # to the tracepoints
-    bitcoind_with_usdts.enable_probe(probe="mempool:added", fn_name="trace_added")
-    bitcoind_with_usdts.enable_probe(probe="mempool:removed", fn_name="trace_removed")
-    bitcoind_with_usdts.enable_probe(probe="mempool:replaced", fn_name="trace_replaced")
-    bitcoind_with_usdts.enable_probe(probe="mempool:rejected", fn_name="trace_rejected")
-    bpf = BPF(text=PROGRAM, usdt_contexts=[bitcoind_with_usdts])
+    BGLd_with_usdts.enable_probe(probe="mempool:added", fn_name="trace_added")
+    BGLd_with_usdts.enable_probe(probe="mempool:removed", fn_name="trace_removed")
+    BGLd_with_usdts.enable_probe(probe="mempool:replaced", fn_name="trace_replaced")
+    BGLd_with_usdts.enable_probe(probe="mempool:rejected", fn_name="trace_rejected")
+    bpf = BPF(text=PROGRAM, usdt_contexts=[BGLd_with_usdts])
 
     events = []
 
@@ -365,7 +365,7 @@ class Dashboard:
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("USAGE: ", sys.argv[0], "path/to/bitcoind")
+        print("USAGE: ", sys.argv[0], "path/to/BGLd")
         exit(1)
 
     path = sys.argv[1]
