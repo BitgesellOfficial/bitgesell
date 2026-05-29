e8d7f7026857731191f7585f7e4a117fb27888fd
Bennett Alt <barnacle.agent@gmail.com>
2026-05-29 15:18:43 +0000

    fix: replace Bitcoin references with BGL in build scripts, CI, and contrib tools

--- contrib/devtools/BGL-tidy/example_logprintf.cpp
+++ contrib/devtools/BGL-tidy/example_logprintf.cpp
@@ -1,10 +1,10 @@
-// Copyright (c) 2023 Bitcoin Developers
+// Copyright (c) 2023 BGL Developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <string>
 
-// Test for bitcoin-unterminated-logprintf
+// Test for BGL-unterminated-logprintf
 
 enum LogFlags {
     NONE
