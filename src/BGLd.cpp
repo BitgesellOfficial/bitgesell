2fb3d7f250cdb7dd101b92b691b2abed46feefc3
Bennett Alt <barnacle.agent@gmail.com>
2026-05-29 13:57:20 +0000

    fix: replace legacy bitcoind references with BGLd in source comments and error messages
    Replace outdated 'bitcoind' and 'bitcoin' references in BGLd.cpp with
    correct BGL/BGLd naming to reflect the Bitgesell fork:
    - Error message: 'see bitcoind -h' -> 'see BGLd -h'
    - Comment: 'parameters/bitcoin.conf' -> 'parameters/BGL.conf'
    - Comment: 'qt/bitcoin.cpp' -> 'qt/BGL.cpp'
    - Comment: 'defaults to true for bitcoind' -> 'defaults to true for BGLd'
    - Comment: 'Connect bitcoind signal handlers' -> 'Connect BGLd signal handlers'
    
    Bounty: BGL PR bounty hunt (#32, #39)

--- src/BGLd.cpp
+++ src/BGLd.cpp
@@ -111,7 +111,7 @@ int fork_daemon(bool nochdir, bool noclose, TokenPipeEnd& endpoint)
 
 static bool ParseArgs(ArgsManager& args, int argc, char* argv[])
 {
-    // If Qt is used, parameters/bitcoin.conf are parsed in qt/bitcoin.cpp's main()
+    // If Qt is used, parameters/BGL.conf are parsed in qt/BGL.cpp's main()
     SetupServerArgs(args);
     std::string error;
     if (!args.ParseParameters(argc, argv, error)) {
@@ -125,7 +125,7 @@ static bool ParseArgs(ArgsManager& args, int argc, char* argv[])
     // Error out when loose non-argument tokens are encountered on command line
     for (int i = 1; i < argc; i++) {
         if (!IsSwitchChar(argv[i][0])) {
-            return InitError(Untranslated(strprintf("Command line contains unexpected token '%s', see bitcoind -h for a list of options.", argv[i])));
+            return InitError(Untranslated(strprintf("Command line contains unexpected token '%s', see BGLd -h for a list of options.", argv[i])));
         }
     }
     return true;
@@ -168,7 +168,7 @@ static bool AppInit(NodeContext& node)
     std::any context{&node};
     try
     {
-        // -server defaults to true for bitcoind but not for the GUI so do this here
+        // -server defaults to true for BGLd but not for the GUI so do this here
         args.SoftSetBoolArg("-server", true);
         // Set this early so that parameter interactions go to console
         InitLogging(args);
@@ -261,7 +261,7 @@ MAIN_FUNCTION
 
     SetupEnvironment();
 
-    // Connect bitcoind signal handlers
+    // Connect BGLd signal handlers
     noui_connect();
 
     util::ThreadSetInternalName("init");
